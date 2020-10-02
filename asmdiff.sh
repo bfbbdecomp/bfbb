5dca0a91b91a8a8d0b0dcd5ac092a988cc0976d1
seilweiss <32021834+seilweiss@users.noreply.github.com>
2020-10-02 13:38:36 -0500

    show correct addresses in asmdiff

--- asmdiff.sh
+++ asmdiff.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OBJDUMP="$DEVKITPPC/bin/powerpc-eabi-objdump -D -bbinary -EB -mpowerpc -M gekko"
+OBJDUMP="$DEVKITPPC/bin/powerpc-eabi-objdump -D -bbinary -EB -mpowerpc -M gekko --adjust-vma=0x80003200"
 $OBJDUMP baserom.dol > baserom.dump
 $OBJDUMP main.dol > main.dump
 diff -u --color=always baserom.dump main.dump
