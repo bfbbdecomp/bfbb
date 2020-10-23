a95a6b575a97a765ba7e164ebce33d38a4aa7c87
Matt <5638426+mattbruv@users.noreply.github.com>
2020-10-23 10:48:47 -0400

    zEntSimpleObj.s

--- src/Game/zEntSimpleObj.cpp
+++ src/Game/zEntSimpleObj.cpp
@@ -2,35 +2,50 @@
 
 #include <types.h>
 
-// zEntSimpleObj_MgrInit(zEntSimpleObj**,unsigned int)
+// func_80090E88
 #pragma GLOBAL_ASM("asm/Game/zEntSimpleObj.s", "zEntSimpleObj_MgrInit__FPP13zEntSimpleObjUi")
 
-// RwSphere::operator =(const RwSphere&)
+// func_8009147C
 #pragma GLOBAL_ASM("asm/Game/zEntSimpleObj.s", "__as__8RwSphereFRC8RwSphere")
 
-// zEntSimpleObj_MgrUpdateRender(RpWorld*,float)
+// func_800914A0
 #pragma GLOBAL_ASM("asm/Game/zEntSimpleObj.s", "zEntSimpleObj_MgrUpdateRender__FP7RpWorldf")
 
-// zEntSimpleObj_MgrCustomUpdate(zScene*,float)
+// func_80091744
 #pragma GLOBAL_ASM("asm/Game/zEntSimpleObj.s", "zEntSimpleObj_MgrCustomUpdate__FP6zScenef")
 
-// zEntSimpleObj_MgrCustomRender()
+// func_800917D0
 #pragma GLOBAL_ASM("asm/Game/zEntSimpleObj.s", "zEntSimpleObj_MgrCustomRender__Fv")
 
-// zEntSimpleObj_Render(xEnt*)
+// func_80091854
 #pragma GLOBAL_ASM("asm/Game/zEntSimpleObj.s", "zEntSimpleObj_Render__FP4xEnt")
 
-// zEntSimpleObj_Init(zEntSimpleObj*,xEntAsset*,bool)
+// func_800918A0
+#pragma GLOBAL_ASM("asm/Game/zEntSimpleObj.s", "zEntTrackPhysics_Init__FPvPv")
+
+// func_800918C4
+#pragma GLOBAL_ASM("asm/Game/zEntSimpleObj.s", "zEntSimpleObj_Init__FPvPv")
+
+// func_800918E8
 #pragma GLOBAL_ASM("asm/Game/zEntSimpleObj.s", "zEntSimpleObj_Init__FP13zEntSimpleObjP9xEntAssetb")
 
-// zEntSimpleObj_Setup(zEntSimpleObj*)
+// func_80091B04
+#pragma GLOBAL_ASM("asm/Game/zEntSimpleObj.s", "zEntSimpleObj_Move__FP4xEntP6xScenefP9xEntFrame")
+
+// func_80091B08
+#pragma GLOBAL_ASM("asm/Game/zEntSimpleObj.s", "zEntSimpleObj_Update__FP13zEntSimpleObjP6xScenef")
+
+// func_80091BE0
 #pragma GLOBAL_ASM("asm/Game/zEntSimpleObj.s", "zEntSimpleObj_Setup__FP13zEntSimpleObj")
 
-// zEntSimpleObj_Save(zEntSimpleObj*,xSerial*)
+// func_80091C00
 #pragma GLOBAL_ASM("asm/Game/zEntSimpleObj.s", "zEntSimpleObj_Save__FP13zEntSimpleObjP7xSerial")
 
-// zEntSimpleObj_Load(zEntSimpleObj*,xSerial*)
+// func_80091C20
 #pragma GLOBAL_ASM("asm/Game/zEntSimpleObj.s", "zEntSimpleObj_Load__FP13zEntSimpleObjP7xSerial")
 
-// zEntSimpleObj_Reset(zEntSimpleObj*,xScene*)
+// func_80091C40
 #pragma GLOBAL_ASM("asm/Game/zEntSimpleObj.s", "zEntSimpleObj_Reset__FP13zEntSimpleObjP6xScene")
+
+// func_80091CD8
+#pragma GLOBAL_ASM("asm/Game/zEntSimpleObj.s", "zEntSimpleObjEventCB__FP5xBaseP5xBaseUiPCfP5xBase")
