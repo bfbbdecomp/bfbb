3cb3d89b98a6d75c3b258f931d15ddd817212c36
seilweiss <32021834+seilweiss@users.noreply.github.com>
2020-10-22 22:45:51 -0500

    limit asmdiff output

--- asmdiff.sh
+++ asmdiff.sh
@@ -3,4 +3,4 @@
 OBJDUMP="$DEVKITPPC/bin/powerpc-eabi-objdump -D -bbinary -EB -mpowerpc -M gekko --adjust-vma=0x80003200"
 $OBJDUMP baserom.dol > baserom.dump
 $OBJDUMP main.dol > main.dump
-diff -u --color=always baserom.dump main.dump
+diff -u --color=always baserom.dump main.dump | head -n 2000
