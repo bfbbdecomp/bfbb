b8840a5574d65001224180e42ced97d6aa793022
Matt <5638426+mattbruv@users.noreply.github.com>
2020-10-23 10:46:25 -0400

    xHudFontMeter.s

--- src/Core/x/xHudFontMeter.cpp
+++ src/Core/x/xHudFontMeter.cpp
@@ -2,20 +2,32 @@
 
 #include <types.h>
 
-// xhud::font_meter_widget::load(xBase&,xDynAsset&,unsigned long)
+// func_8002F008
 #pragma GLOBAL_ASM("asm/Core/x/xHudFontMeter.s", "load__Q24xhud17font_meter_widgetFR5xBaseR9xDynAssetUl")
 
-// xhud::font_meter_widget::font_meter_widget(const xhud::font_meter_asset&)
+// func_8002F060
 #pragma GLOBAL_ASM("asm/Core/x/xHudFontMeter.s", "__ct__Q24xhud17font_meter_widgetFRCQ24xhud16font_meter_asset")
 
-// xhud::font_meter_widget::destruct()
+// func_8002F170
 #pragma GLOBAL_ASM("asm/Core/x/xHudFontMeter.s", "destruct__Q24xhud17font_meter_widgetFv")
 
-// xhud::font_meter_widget::type() const
+// func_8002F190
+#pragma GLOBAL_ASM("asm/Core/x/xHudFontMeter.s", "destroy__Q24xhud17font_meter_widgetFv")
+
+// func_8002F1B0
 #pragma GLOBAL_ASM("asm/Core/x/xHudFontMeter.s", "type__Q24xhud17font_meter_widgetCFv")
 
-// xhud::font_meter_asset::type_name()
+// func_8002F1F0
+#pragma GLOBAL_ASM("asm/Core/x/xHudFontMeter.s", "is__Q24xhud17font_meter_widgetCFUi")
+
+// func_8002F258
+#pragma GLOBAL_ASM("asm/Core/x/xHudFontMeter.s", "update__Q24xhud17font_meter_widgetFf")
+
+// func_8002F444
+#pragma GLOBAL_ASM("asm/Core/x/xHudFontMeter.s", "render__Q24xhud17font_meter_widgetFv")
+
+// func_8002F504
 #pragma GLOBAL_ASM("asm/Core/x/xHudFontMeter.s", "type_name__Q24xhud16font_meter_assetFv")
 
-// xfont::render(const char*,float,float) const
+// func_8002F510
 #pragma GLOBAL_ASM("asm/Core/x/xHudFontMeter.s", "render__5xfontCFPCcff")
