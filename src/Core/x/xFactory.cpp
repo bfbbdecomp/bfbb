cbe70d887bb08a234f0f37da6bab070b354da6db
Matt <5638426+mattbruv@users.noreply.github.com>
2020-10-23 10:45:10 -0400

    xFactory.s

--- src/Core/x/xFactory.cpp
+++ src/Core/x/xFactory.cpp
@@ -2,29 +2,35 @@
 
 #include <types.h>
 
-// xFactory::xFactory(int)
+// func_8010F2D0
 #pragma GLOBAL_ASM("asm/Core/x/xFactory.s", "__ct__8xFactoryFi")
 
-// xFactory::~xFactory()
+// func_8010F348
 #pragma GLOBAL_ASM("asm/Core/x/xFactory.s", "__dt__8xFactoryFv")
 
-// xFactory::RegItemType(XGOFTypeInfo*)
+// func_8010F3A8
 #pragma GLOBAL_ASM("asm/Core/x/xFactory.s", "RegItemType__8xFactoryFP12XGOFTypeInfo")
 
-// xFactory::RegItemType(int,xFactoryInst* (*)(int, RyzMemGrow*, void*),void (*)(xFactoryInst*))
+// func_8010F498
 #pragma GLOBAL_ASM("asm/Core/x/xFactory.s", "RegItemType__8xFactoryFiPFiP10RyzMemGrowPv_P12xFactoryInstPFP12xFactoryInst_v")
 
-// xFactory::GrowDataEnable(xBase*,int)
+// func_8010F500
 #pragma GLOBAL_ASM("asm/Core/x/xFactory.s", "GrowDataEnable__8xFactoryFP5xBasei")
 
-// xFactory::GrowDataDisable()
+// func_8010F538
 #pragma GLOBAL_ASM("asm/Core/x/xFactory.s", "GrowDataDisable__8xFactoryFv")
 
-// xFactory::CreateItem(int,void*,RyzMemGrow*)
+// func_8010F55C
 #pragma GLOBAL_ASM("asm/Core/x/xFactory.s", "CreateItem__8xFactoryFiPvP10RyzMemGrow")
 
-// xFactory::DestroyAll()
+// func_8010F6AC
 #pragma GLOBAL_ASM("asm/Core/x/xFactory.s", "DestroyAll__8xFactoryFv")
 
-// xFactory::DestroyItem(xFactoryInst*)
+// func_8010F6EC
 #pragma GLOBAL_ASM("asm/Core/x/xFactory.s", "DestroyItem__8xFactoryFP12xFactoryInst")
+
+// func_8010F7D4
+#pragma GLOBAL_ASM("asm/Core/x/xFactory.s", "OrdTest_infotype__FPCvPv")
+
+// func_8010F800
+#pragma GLOBAL_ASM("asm/Core/x/xFactory.s", "OrdComp_infotype__FPvPv")
