b6b0300c2cbd0916bd83316a01fd929bab788c10
Matt <5638426+mattbruv@users.noreply.github.com>
2020-10-23 10:44:15 -0400

    xAnim.s

--- src/Core/x/xAnim.cpp
+++ src/Core/x/xAnim.cpp
@@ -2,167 +2,167 @@
 
 #include <types.h>
 
-// _xSingleCompare(char,char)
+// func_800058E0
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "_xSingleCompare__Fcc")
 
-// _xSingleCompare(char,const char*)
+// func_80005950
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "_xSingleCompare__FcPCc")
 
-// _xCharIn(char,const char*)
+// func_800059B4
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "_xCharIn__FcPCc")
 
-// _xCheckAnimNameInner(const char*,const char*,int,char*,int*,int*)
+// func_800059E4
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "_xCheckAnimNameInner__FPCcPCciPcPiPi")
 
-// _xCheckAnimName(const char*,const char*,char*)
+// func_80005F4C
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "_xCheckAnimName__FPCcPCcPc")
 
-// xAnimInit()
+// func_80005FDC
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimInit__Fv")
 
-// xAnimTempTransitionInit(unsigned int)
+// func_80006010
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimTempTransitionInit__FUi")
 
-// xatan2(float,float)
+// func_80006070
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xatan2__Fff")
 
-// std::atan2f(float,float)
+// func_80006094
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "atan2f__3stdFff")
 
-// CalcRecipBlendMax(unsigned short*)
+// func_800060B8
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "CalcRecipBlendMax__FPUs")
 
-// StateHasTransition(xAnimState*,xAnimTransition*)
+// func_8000615C
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "StateHasTransition__FP10xAnimStateP15xAnimTransition")
 
-// DefaultHasTransition(xAnimState*,xAnimTransition*,unsigned int*)
+// func_8000618C
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "DefaultHasTransition__FP10xAnimStateP15xAnimTransitionPUi")
 
-// DefaultOverride(xAnimState*,xAnimTransition*)
+// func_800061F0
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "DefaultOverride__FP10xAnimStateP15xAnimTransition")
 
-// TransitionTimeInit(xAnimSingle*,xAnimTransition*)
+// func_8000623C
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "TransitionTimeInit__FP11xAnimSingleP15xAnimTransition")
 
-// xAnimFileNewBilinear(void**,const char*,unsigned int,xAnimFile**,unsigned int,unsigned int)
+// func_80006300
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimFileNewBilinear__FPPvPCcUiPP9xAnimFileUiUi")
 
-// xAnimFileNew(void*,const char*,unsigned int,xAnimFile**)
+// func_80006568
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimFileNew__FPvPCcUiPP9xAnimFile")
 
-// xAnimFileSetTime(xAnimFile*,float,float)
+// func_80006598
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimFileSetTime__FP9xAnimFileff")
 
-// xAnimFileEval(xAnimFile*,float,float*,unsigned int,xVec3*,xQuat*,float*)
+// func_80006658
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimFileEval__FP9xAnimFilefPfUiP5xVec3P5xQuatPf")
 
-// std::floorf(float)
+// func_80006B78
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "floorf__3stdFf")
 
-// xAnimStateNewEffect(xAnimState*,unsigned int,float,float,unsigned int (*)(unsigned int, xAnimActiveEffect*, xAnimSingle*, void*),unsigned int)
+// func_80006B9C
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimStateNewEffect__FP10xAnimStateUiffPFUiP17xAnimActiveEffectP11xAnimSinglePv_UiUi")
 
-// xAnimTableNew(const char*,xAnimTable**,unsigned int)
+// func_80006C70
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimTableNew__FPCcPP10xAnimTableUi")
 
-// xAnimDefaultBeforeEnter(xAnimPlay*,xAnimState*)
+// func_80006CFC
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimDefaultBeforeEnter__FP9xAnimPlayP10xAnimState")
 
-// xAnimTableNewState(xAnimTable*,const char*,unsigned int,unsigned int,float,float*,float*,float,unsigned short*,void*,void (*)(xAnimPlay*, xAnimState*),void (*)(xAnimState*, xAnimSingle*, void*),void (*)(xAnimPlay*, xQuat*, xVec3*, int))
+// func_80006D58
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimTableNewState__FP10xAnimTablePCcUiUifPfPffPUsPvPFP9xAnimPlayP10xAnimState_vPFP10xAnimStateP11xAnimSinglePv_vPFP9xAnimPlayP5xQuatP5xVec3i_v")
 
-// _xAnimTableAddTransitionHelper(xAnimState*,xAnimTransition*,unsigned int&,unsigned int&,xAnimState**)
+// func_80006E90
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "_xAnimTableAddTransitionHelper__FP10xAnimStateP15xAnimTransitionRUiRUiPP10xAnimState")
 
-// _xAnimTableAddTransition(xAnimTable*,xAnimTransition*,const char*,const char*)
+// func_80006F40
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "_xAnimTableAddTransition__FP10xAnimTableP15xAnimTransitionPCcPCc")
 
-// xAnimTableAddTransition(xAnimTable*,xAnimTransition*,const char*)
+// func_8000739C
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimTableAddTransition__FP10xAnimTableP15xAnimTransitionPCc")
 
-// xAnimTableNewTransition(xAnimTable*,const char*,const char*,unsigned int (*)(xAnimTransition*, xAnimSingle*, void*),unsigned int (*)(xAnimTransition*, xAnimSingle*, void*),unsigned int,unsigned int,float,float,unsigned short,unsigned short,float,unsigned short*)
+// func_800073C0
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimTableNewTransition__FP10xAnimTablePCcPCcPFP15xAnimTransitionP11xAnimSinglePv_UiPFP15xAnimTransitionP11xAnimSinglePv_UiUiUiffUsUsfPUs")
 
-// xAnimTableAddFile(xAnimTable*,xAnimFile*,const char*)
+// func_80007574
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimTableAddFile__FP10xAnimTableP9xAnimFilePCc")
 
-// xAnimTableAddFileID(xAnimTable*,xAnimFile*,unsigned int,unsigned int,unsigned int)
+// func_80007618
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimTableAddFileID__FP10xAnimTableP9xAnimFileUiUiUi")
 
-// xAnimTableGetStateID(xAnimTable*,unsigned int)
+// func_80007734
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimTableGetStateID__FP10xAnimTableUi")
 
-// xAnimTableGetState(xAnimTable*,const char*)
+// func_8000775C
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimTableGetState__FP10xAnimTablePCc")
 
-// EffectActiveInsert(xAnimSingle*,xAnimActiveEffect*)
+// func_80007798
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "EffectActiveInsert__FP11xAnimSingleP17xAnimActiveEffect")
 
-// xAnimActiveEffect::operator =(const xAnimActiveEffect&)
+// func_80007814
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "__as__17xAnimActiveEffectFRC17xAnimActiveEffect")
 
-// EffectActiveRemove(xAnimActiveEffect*,unsigned int,unsigned int)
+// func_80007828
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "EffectActiveRemove__FP17xAnimActiveEffectUiUi")
 
-// EffectPlaying(xAnimSingle*,xAnimEffect*)
+// func_800078A4
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "EffectPlaying__FP11xAnimSingleP11xAnimEffect")
 
-// EffectSingleStart(xAnimSingle*)
+// func_800078EC
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "EffectSingleStart__FP11xAnimSingle")
 
-// EffectSingleDuration(xAnimSingle*)
+// func_80007A4C
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "EffectSingleDuration__FP11xAnimSingle")
 
-// EffectSingleRun(xAnimSingle*)
+// func_80007B3C
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "EffectSingleRun__FP11xAnimSingle")
 
-// EffectSingleLoop(xAnimSingle*)
+// func_80007C68
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "EffectSingleLoop__FP11xAnimSingle")
 
-// EffectSingleStop(xAnimSingle*)
+// func_80007D44
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "EffectSingleStop__FP11xAnimSingle")
 
-// StopUpdate(xAnimSingle*)
+// func_80007E84
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "StopUpdate__FP11xAnimSingle")
 
-// LoopUpdate(xAnimSingle*)
+// func_80007EAC
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "LoopUpdate__FP11xAnimSingle")
 
-// xAnimPlaySetState(xAnimSingle*,xAnimState*,float)
+// func_80007F1C
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimPlaySetState__FP11xAnimSingleP10xAnimStatef")
 
-// SingleUpdate(xAnimSingle*,float)
+// func_8000804C
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "SingleUpdate__FP11xAnimSinglef")
 
-// SingleEval(xAnimSingle*,xVec3*,xQuat*)
+// func_80008728
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "SingleEval__FP11xAnimSingleP5xVec3P5xQuat")
 
-// xAnimPlaySetup(xAnimPlay*,void*,xAnimTable*,xModelInstance*)
+// func_8000880C
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimPlaySetup__FP9xAnimPlayPvP10xAnimTableP14xModelInstance")
 
-// xAnimPlayChooseTransition(xAnimPlay*)
+// func_800088EC
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimPlayChooseTransition__FP9xAnimPlay")
 
-// xAnimPlayStartTransition(xAnimPlay*,xAnimTransition*)
+// func_80008A44
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimPlayStartTransition__FP9xAnimPlayP15xAnimTransition")
 
-// xAnimPlayUpdate(xAnimPlay*,float)
+// func_80008C68
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimPlayUpdate__FP9xAnimPlayf")
 
-// xAnimPlayEval(xAnimPlay*)
+// func_80008D08
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimPlayEval__FP9xAnimPlay")
 
-// xAnimPoolCB(xMemPool*,void*)
+// func_80008FB4
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimPoolCB__FP8xMemPoolPv")
 
-// xAnimPoolInit(xMemPool*,unsigned int,unsigned int,unsigned int,unsigned int)
+// func_800090BC
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimPoolInit__FP8xMemPoolUiUiUiUi")
 
-// xAnimPoolAlloc(xMemPool*,void*,xAnimTable*,xModelInstance*)
+// func_800092D4
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimPoolAlloc__FP8xMemPoolPvP10xAnimTableP14xModelInstance")
 
-// xAnimPoolFree(xAnimPlay*)
+// func_80009330
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimPoolFree__FP9xAnimPlay")
 
-// xAnimFileRawTime(xAnimFile*,float)
+// func_800093B8
 #pragma GLOBAL_ASM("asm/Core/x/xAnim.s", "xAnimFileRawTime__FP9xAnimFilef")
