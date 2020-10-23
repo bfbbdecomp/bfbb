00f97c9dc5e1abea6c8fba107a3bd2b192c8315a
Matt <5638426+mattbruv@users.noreply.github.com>
2020-10-23 10:44:58 -0400

    xCutscene.s

--- src/Core/x/xCutscene.cpp
+++ src/Core/x/xCutscene.cpp
@@ -2,53 +2,62 @@
 
 #include <types.h>
 
-// xCutscene_Init(void*)
+// func_80015EA4
 #pragma GLOBAL_ASM("asm/Core/x/xCutscene.s", "xCutscene_Init__FPv")
 
-// xCutscene_Create(unsigned int)
+// func_80015F9C
 #pragma GLOBAL_ASM("asm/Core/x/xCutscene.s", "xCutscene_Create__FUi")
 
-// xCutscene_Destroy(xCutscene*)
+// func_800160E0
 #pragma GLOBAL_ASM("asm/Core/x/xCutscene.s", "xCutscene_Destroy__FP9xCutscene")
 
-// xCutscene_LoadStart(xCutscene*)
+// func_80016220
 #pragma GLOBAL_ASM("asm/Core/x/xCutscene.s", "xCutscene_LoadStart__FP9xCutscene")
 
-// xCutsceneConvertBreak(float,xCutsceneBreak*,unsigned int,int)
+// func_80016268
 #pragma GLOBAL_ASM("asm/Core/x/xCutscene.s", "xCutsceneConvertBreak__FfP14xCutsceneBreakUii")
 
-// xCutscene_Update(xCutscene*,float)
+// func_800162C8
 #pragma GLOBAL_ASM("asm/Core/x/xCutscene.s", "xCutscene_Update__FP9xCutscenef")
 
-// xCutscene_SetSpeed(xCutscene*,float)
+// func_800164C8
 #pragma GLOBAL_ASM("asm/Core/x/xCutscene.s", "xCutscene_SetSpeed__FP9xCutscenef")
 
-// xlog(float)
+// func_80016590
 #pragma GLOBAL_ASM("asm/Core/x/xCutscene.s", "xlog__Ff")
 
-// std::logf(float)
+// func_800165B0
 #pragma GLOBAL_ASM("asm/Core/x/xCutscene.s", "logf__3stdFf")
 
-// xCutscene_SetCamera(xCutscene*,xCamera*)
+// func_800165D4
 #pragma GLOBAL_ASM("asm/Core/x/xCutscene.s", "xCutscene_SetCamera__FP9xCutsceneP7xCamera")
 
-// xcsCalcAnimMatrices(RwMatrixTag*,RpAtomic*,xCutsceneAnimHdr*,float,unsigne d int)
+// func_80016818
 #pragma GLOBAL_ASM("asm/Core/x/xCutscene.s", "xcsCalcAnimMatrices__FP11RwMatrixTagP8RpAtomicP16xCutsceneAnimHdrfUi")
 
-// JDeltaEval(RpAtomic*,void*,void*,float)
+// func_80016A30
 #pragma GLOBAL_ASM("asm/Core/x/xCutscene.s", "JDeltaEval__FP8RpAtomicPvPvf")
 
-// xCutscene_Render(xCutscene*,xEnt**,int*,float*)
+// func_800172A8
+#pragma GLOBAL_ASM("asm/Core/x/xCutscene.s", "CutsceneShadowRender__FP19CutsceneShadowModel")
+
+// func_80017318
 #pragma GLOBAL_ASM("asm/Core/x/xCutscene.s", "xCutscene_Render__FP9xCutscenePP4xEntPiPf")
 
-// xCutscene_CurrentCutscene()
+// func_80017CA4
 #pragma GLOBAL_ASM("asm/Core/x/xCutscene.s", "xCutscene_CurrentCutscene__Fv")
 
-// std::atan(float)
+// func_80017CB0
 #pragma GLOBAL_ASM("asm/Core/x/xCutscene.s", "atan__3stdFf")
 
-// std::atanf(float)
+// func_80017CD0
 #pragma GLOBAL_ASM("asm/Core/x/xCutscene.s", "atanf__3stdFf")
 
-// xVec3Lerp(xVec3*,const xVec3*,const xVec3*,float)
+// func_80017CF4
 #pragma GLOBAL_ASM("asm/Core/x/xCutscene.s", "xVec3Lerp__FP5xVec3PC5xVec3PC5xVec3f")
+
+// func_80017D34
+#pragma GLOBAL_ASM("asm/Core/x/xCutscene.s", "CanRenderNow__9XCSNNoseyFv")
+
+// func_80017D38
+#pragma GLOBAL_ASM("asm/Core/x/xCutscene.s", "UpdatedAnimated__9XCSNNoseyFP8RpAtomicP11RwMatrixTagUiUi")
