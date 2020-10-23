33a4f207a138564a3889be4673103817a9bc5ae9
Matt <5638426+mattbruv@users.noreply.github.com>
2020-10-23 10:47:47 -0400

    xVolume.s

--- src/Core/x/xVolume.cpp
+++ src/Core/x/xVolume.cpp
@@ -2,17 +2,17 @@
 
 #include <types.h>
 
-// xVolume::Init(xVolumeAsset*)
+// func_80118934
 #pragma GLOBAL_ASM("asm/Core/x/xVolume.s", "Init__7xVolumeFP12xVolumeAsset")
 
-// xVolume::Reset()
+// func_80118994
 #pragma GLOBAL_ASM("asm/Core/x/xVolume.s", "Reset__7xVolumeFv")
 
-// xVolume::Save(xSerial*)
+// func_801189B8
 #pragma GLOBAL_ASM("asm/Core/x/xVolume.s", "Save__7xVolumeFP7xSerial")
 
-// xVolume::Load(xSerial*)
+// func_801189D8
 #pragma GLOBAL_ASM("asm/Core/x/xVolume.s", "Load__7xVolumeFP7xSerial")
 
-// xVolume::GetBound()
+// func_801189F8
 #pragma GLOBAL_ASM("asm/Core/x/xVolume.s", "GetBound__7xVolumeFv")
