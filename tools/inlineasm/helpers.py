eaaebf9b7b929ea1131ec75aaba56675ed16df20
Matt <5638426+mattbruv@users.noreply.github.com>
2020-10-23 08:35:42 -0400

    add funciton scoping to pragma processor

--- tools/inlineasm/helpers.py
+++ tools/inlineasm/helpers.py
@@ -2,26 +2,33 @@ import re
 
 pragmaRegex = r"(#pragma\sGLOBAL_ASM\((.*?)\))"
 
+
 def getPragmaMatches(fileText):
     matches = re.findall(pragmaRegex, fileText, flags=re.DOTALL)
     return matches
 
 # Arguments are processed outside of the regex to keep it simple
 # and support any changes we may make in the future
+
+
 def getPragmaArgs(argString):
     args = argString.split(",")
     args = map(lambda s: s.split("\"")[1], args)
     return list(args)
 
+
 labelRegex = r".+:"
+
 
 def getLabels(fileText):
     matches = re.findall(labelRegex, fileText)
     return matches
 
+
 def getAsmFunctions(fileText):
     matches = getLabels(fileText)
     return list(filter(lambda x: "lbl_" not in x, matches))
+
 
 def getAsmFunctionBlock(fileText, label):
     data = []
@@ -36,20 +43,25 @@ def getAsmFunctionBlock(fileText, label):
                 break
     return data
 
+
 def filterBlockCode(block):
     return list(filter(lambda x: "/*" in x, block))
+
 
 def codeLineToBytes(line):
     d = line.split()
     b = "0x" + "".join(d[3:7])
     return b
 
+
 def blockToBytes(block):
     code = filterBlockCode(block)
     return list(map(codeLineToBytes, code))
 
+
 def bytesToString(byteLine):
     return "opword" + " " + byteLine
+
 
 funcTemplate = """extern "C" {
 asm void {name}() {
@@ -57,14 +69,18 @@ nofralloc
 {data}
 }}"""
 
-def writeCode(source, funcName, codeBytes):
+
+def writeCode(source, funcName, codeBytes, isGlobal):
     source += "\n"
     t = funcTemplate.replace("{name}", funcName)
     bs = list(map(bytesToString, codeBytes))
     t = t.replace("{data}", "\n".join(bs))
+    if not isGlobal:
+        t = t.replace("asm void", "asm static void")
     source += t + "\n"
 
     return source
+
 
 def error(text):
     print("Error during #pragma processing:")
