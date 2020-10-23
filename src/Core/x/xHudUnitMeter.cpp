6adeac1c395b90726003b7b48f9290f01aebac10
Matt <5638426+mattbruv@users.noreply.github.com>
2020-10-23 10:46:31 -0400

    xHudUnitMeter.s

--- src/Core/x/xHudUnitMeter.cpp
+++ src/Core/x/xHudUnitMeter.cpp
@@ -2,26 +2,41 @@
 
 #include <types.h>
 
-// xhud::unit_meter_widget::load(xBase&,xDynAsset&,unsigned long)
+// func_8002FF80
 #pragma GLOBAL_ASM("asm/Core/x/xHudUnitMeter.s", "load__Q24xhud17unit_meter_widgetFR5xBaseR9xDynAssetUl")
 
-// xhud::unit_meter_widget::unit_meter_widget(const xhud::unit_meter_asset&)
+// func_8002FFD8
 #pragma GLOBAL_ASM("asm/Core/x/xHudUnitMeter.s", "__ct__Q24xhud17unit_meter_widgetFRCQ24xhud16unit_meter_asset")
 
-// xhud::unit_meter_widget::destruct()
+// func_800300EC
 #pragma GLOBAL_ASM("asm/Core/x/xHudUnitMeter.s", "destruct__Q24xhud17unit_meter_widgetFv")
 
-// xhud::unit_meter_widget::type() const
+// func_8003010C
+#pragma GLOBAL_ASM("asm/Core/x/xHudUnitMeter.s", "destroy__Q24xhud17unit_meter_widgetFv")
+
+// func_8003012C
 #pragma GLOBAL_ASM("asm/Core/x/xHudUnitMeter.s", "type__Q24xhud17unit_meter_widgetCFv")
 
-// xfmod(float,float)
+// func_8003016C
+#pragma GLOBAL_ASM("asm/Core/x/xHudUnitMeter.s", "is__Q24xhud17unit_meter_widgetCFUi")
+
+// func_800301D4
+#pragma GLOBAL_ASM("asm/Core/x/xHudUnitMeter.s", "setup__Q24xhud17unit_meter_widgetFv")
+
+// func_800301F4
+#pragma GLOBAL_ASM("asm/Core/x/xHudUnitMeter.s", "update__Q24xhud17unit_meter_widgetFf")
+
+// func_800303AC
 #pragma GLOBAL_ASM("asm/Core/x/xHudUnitMeter.s", "xfmod__Fff")
 
-// std::fmodf(float,float)
+// func_800303CC
 #pragma GLOBAL_ASM("asm/Core/x/xHudUnitMeter.s", "fmodf__3stdFff")
 
-// xDebugAddTweak(const char*,float*,float,float,const tweak_callback*,void*,unsigned int)
+// func_800303F0
+#pragma GLOBAL_ASM("asm/Core/x/xHudUnitMeter.s", "render__Q24xhud17unit_meter_widgetFv")
+
+// func_8003062C
 #pragma GLOBAL_ASM("asm/Core/x/xHudUnitMeter.s", "xDebugAddTweak__FPCcPfffPC14tweak_callbackPvUi")
 
-// xhud::unit_meter_asset::type_name()
+// func_80030630
 #pragma GLOBAL_ASM("asm/Core/x/xHudUnitMeter.s", "type_name__Q24xhud16unit_meter_assetFv")
