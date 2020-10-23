e0daed10ff63510a63042b2e4698dca5c6bf3d1b
seilweiss <32021834+seilweiss@users.noreply.github.com>
2020-10-23 18:52:58 -0500

    decomp most of zVolume.cpp

--- src/Core/x/xVec3.h
+++ src/Core/x/xVec3.h
@@ -8,6 +8,11 @@ struct xVec3
     float32 x;
     float32 y;
     float32 z;
+
+    xVec3& operator=(const xVec3&);
 };
+
+float32 xVec3Normalize(xVec3* o, const xVec3* v);
+float32 xVec3Dot(const xVec3* a, const xVec3* b);
 
 #endif
--- src/Game/zVolume.h
+++ src/Game/zVolume.h
@@ -1,6 +1,16 @@
 #ifndef ZVOLUME_H
 #define ZVOLUME_H
 
+#include "../Core/x/xVolume.h"
 
+struct zVolume : xVolume
+{
+};
+
+void zVolumeInit();
+void zVolumeSetup();
+zVolume* zVolumeGetVolume(uint16 n);
+void zVolume_OccludePrecalc(xVec3* camPos);
+int32 zVolumeEventCB(xBase*, xBase* to, uint32 toEvent, const float32*, xBase*);
 
 #endif
