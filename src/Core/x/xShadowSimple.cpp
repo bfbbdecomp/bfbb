69fb2745244482bc7c6899d6723cf53403cd7f87
Matt <5638426+mattbruv@users.noreply.github.com>
2020-10-23 10:47:29 -0400

    xShadowSimple.s

--- src/Core/x/xShadowSimple.cpp
+++ src/Core/x/xShadowSimple.cpp
@@ -2,37 +2,41 @@
 
 #include <types.h>
 
-// shadowRayCB(RpIntersection*,RpWorldSector*,RpCollisionTriangle*,float,void *)
+// func_80139218
 #pragma GLOBAL_ASM("asm/Core/x/xShadowSimple.s", "shadowRayCB__FP14RpIntersectionP13RpWorldSectorP19RpCollisionTrianglefPv")
 
-// xShadowSimple_SceneCollide(xShadowSimpleCache*,xVec3*,float)
+// func_801392FC
+#pragma GLOBAL_ASM("asm/Core/x/xShadowSimple.s", "shadowRayModelCB__FP14RpIntersectionP19RpCollisionTrianglefPv")
+
+// func_80139328
+#pragma GLOBAL_ASM("asm/Core/x/xShadowSimple.s", "shadowRayEntCB__FP4xEntPv")
+
+// func_80139674
 #pragma GLOBAL_ASM("asm/Core/x/xShadowSimple.s", "xShadowSimple_SceneCollide__FP18xShadowSimpleCacheP5xVec3f")
 
-// xShadowSimple_CalcCorners(xShadowSimpleCache*,xEnt*,float,float)
+// func_80139864
 #pragma GLOBAL_ASM("asm/Core/x/xShadowSimple.s", "xShadowSimple_CalcCorners__FP18xShadowSimpleCacheP4xEntff")
 
-// xShadowSimple_AddVerts(xShadowSimpleCache*)
+// func_80139A48
 #pragma GLOBAL_ASM("asm/Core/x/xShadowSimple.s", "xShadowSimple_AddVerts__FP18xShadowSimpleCache")
 
-// xShadowSimple_Init()
+// func_80139BB4
 #pragma GLOBAL_ASM("asm/Core/x/xShadowSimple.s", "xShadowSimple_Init__Fv")
 
-// xShadowSimple_CacheInit(xShadowSimpleCache*,xEnt*,unsigned char)
+// func_80139CFC
 #pragma GLOBAL_ASM("asm/Core/x/xShadowSimple.s", "xShadowSimple_CacheInit__FP18xShadowSimpleCacheP4xEntUc")
 
-// xShadowSimple_Add(xShadowSimpleCache*,xEnt*,float,float)
+// func_80139E5C
 #pragma GLOBAL_ASM("asm/Core/x/xShadowSimple.s", "xShadowSimple_Add__FP18xShadowSimpleCacheP4xEntff")
 
-// xShadowSimpleQueue::operator =(const xShadowSimpleQueue&)
+// func_8013A2F4
 #pragma GLOBAL_ASM("asm/Core/x/xShadowSimple.s", "__as__18xShadowSimpleQueueFRC18xShadowSimpleQueue")
 
-// xShadowSimple_Render()
+// func_8013A320
 #pragma GLOBAL_ASM("asm/Core/x/xShadowSimple.s", "xShadowSimple_Render__Fv")
 
-// xMat3x3RMulVec(xVec3*,const xMat3x3*,const xVec3*)
-namespace {
+// func_8013A538
 #pragma GLOBAL_ASM("asm/Core/x/xShadowSimple.s", "xMat3x3RMulVec__FP5xVec3PC7xMat3x3PC5xVec3")
-}
 
-// xQuickCullForLine(xQCData*,const xLine3*)
+// func_8013A59C
 #pragma GLOBAL_ASM("asm/Core/x/xShadowSimple.s", "xQuickCullForLine__FP7xQCDataPC6xLine3")
