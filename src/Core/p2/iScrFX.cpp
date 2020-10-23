5dd24bf2688bae89e93ca14db0b33a89f772ae1b
Matt <5638426+mattbruv@users.noreply.github.com>
2020-10-23 10:43:20 -0400

    iScrFX.s

--- src/Core/p2/iScrFX.cpp
+++ src/Core/p2/iScrFX.cpp
@@ -2,53 +2,53 @@
 
 #include <types.h>
 
-// iScrFxInit()
+// func_800CF34C
 #pragma GLOBAL_ASM("asm/Core/p2/iScrFX.s", "iScrFxInit__Fv")
 
-// iScrFxBegin()
+// func_800CF350
 #pragma GLOBAL_ASM("asm/Core/p2/iScrFX.s", "iScrFxBegin__Fv")
 
-// iScrFxEnd()
+// func_800CF3CC
 #pragma GLOBAL_ASM("asm/Core/p2/iScrFX.s", "iScrFxEnd__Fv")
 
-// iScrFxDrawBox(float,float,float,float,unsigned char,unsigned char,unsigned char,unsigned char)
+// func_800CF43C
 #pragma GLOBAL_ASM("asm/Core/p2/iScrFX.s", "iScrFxDrawBox__FffffUcUcUcUc")
 
-// iCameraMotionBlurActivate(unsigned int)
+// func_800CF544
 #pragma GLOBAL_ASM("asm/Core/p2/iScrFX.s", "iCameraMotionBlurActivate__FUi")
 
-// iCameraSetBlurriness(float)
+// func_800CF54C
 #pragma GLOBAL_ASM("asm/Core/p2/iScrFX.s", "iCameraSetBlurriness__Ff")
 
-// iScrFxCameraCreated(RwCamera*)
+// func_800CF5AC
 #pragma GLOBAL_ASM("asm/Core/p2/iScrFX.s", "iScrFxCameraCreated__FP8RwCamera")
 
-// iScrFxCameraEndScene(RwCamera*)
+// func_800CF60C
 #pragma GLOBAL_ASM("asm/Core/p2/iScrFX.s", "iScrFxCameraEndScene__FP8RwCamera")
 
-// iScrFxPostCameraEnd(RwCamera*)
+// func_800CF64C
 #pragma GLOBAL_ASM("asm/Core/p2/iScrFX.s", "iScrFxPostCameraEnd__FP8RwCamera")
 
-// iCameraOverlayRender(RwCamera*,RwRaster*,RwRGBA)
+// func_800CF66C
 #pragma GLOBAL_ASM("asm/Core/p2/iScrFX.s", "iCameraOverlayRender__FP8RwCameraP8RwRaster6RwRGBA")
 
-// FBMBlur_DebugIntervention(RwCamera*,RwRaster*)
+// func_800CF7F0
 #pragma GLOBAL_ASM("asm/Core/p2/iScrFX.s", "FBMBlur_DebugIntervention__FP8RwCameraP8RwRaster")
 
-// iScrFxMotionBlurOpen(RwCamera*)
+// func_800CF7F8
 #pragma GLOBAL_ASM("asm/Core/p2/iScrFX.s", "iScrFxMotionBlurOpen__FP8RwCamera")
 
-// iScrFxCameraDestroyed(RwCamera*)
+// func_800CF800
 #pragma GLOBAL_ASM("asm/Core/p2/iScrFX.s", "iScrFxCameraDestroyed__FP8RwCamera")
 
-// iScrFxMotionBlurRender(RwCamera*,unsigned int)
+// func_800CF854
 #pragma GLOBAL_ASM("asm/Core/p2/iScrFX.s", "iScrFxMotionBlurRender__FP8RwCameraUi")
 
-// GCMB_MakeFrameBufferCopy(const RwCamera*)
+// func_800CF8A4
 #pragma GLOBAL_ASM("asm/Core/p2/iScrFX.s", "GCMB_MakeFrameBufferCopy__FPC8RwCamera")
 
-// GCMB_KillFrameBufferCopy()
+// func_800CF8A8
 #pragma GLOBAL_ASM("asm/Core/p2/iScrFX.s", "GCMB_KillFrameBufferCopy__Fv")
 
-// GCMB_SiphonFrameBuffer(const RwCamera*)
+// func_800CF8E0
 #pragma GLOBAL_ASM("asm/Core/p2/iScrFX.s", "GCMB_SiphonFrameBuffer__FPC8RwCamera")
