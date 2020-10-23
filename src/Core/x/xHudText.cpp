ba20caeb3c34df2a5eca12cb2b36ce963682a74d
Matt <5638426+mattbruv@users.noreply.github.com>
2020-10-23 10:46:30 -0400

    xHudText.s

--- src/Core/x/xHudText.cpp
+++ src/Core/x/xHudText.cpp
@@ -2,17 +2,32 @@
 
 #include <types.h>
 
-// xhud::text_widget::load(xBase&,xDynAsset&,unsigned long)
+// func_80195428
 #pragma GLOBAL_ASM("asm/Core/x/xHudText.s", "load__Q24xhud11text_widgetFR5xBaseR9xDynAssetUl")
 
-// xhud::text_widget::text_widget(const xhud::text_asset&)
+// func_80195480
 #pragma GLOBAL_ASM("asm/Core/x/xHudText.s", "__ct__Q24xhud11text_widgetFRCQ24xhud10text_asset")
 
-// xhud::text_widget::destruct()
+// func_801954C4
 #pragma GLOBAL_ASM("asm/Core/x/xHudText.s", "destruct__Q24xhud11text_widgetFv")
 
-// xhud::text_widget::type() const
+// func_801954E4
+#pragma GLOBAL_ASM("asm/Core/x/xHudText.s", "setup__Q24xhud11text_widgetFv")
+
+// func_80195630
+#pragma GLOBAL_ASM("asm/Core/x/xHudText.s", "destroy__Q24xhud11text_widgetFv")
+
+// func_80195650
 #pragma GLOBAL_ASM("asm/Core/x/xHudText.s", "type__Q24xhud11text_widgetCFv")
 
-// xhud::text_asset::type_name()
+// func_80195690
+#pragma GLOBAL_ASM("asm/Core/x/xHudText.s", "is__Q24xhud11text_widgetCFUi")
+
+// func_801956F8
+#pragma GLOBAL_ASM("asm/Core/x/xHudText.s", "update__Q24xhud11text_widgetFf")
+
+// func_801957DC
+#pragma GLOBAL_ASM("asm/Core/x/xHudText.s", "render__Q24xhud11text_widgetFv")
+
+// func_80195900
 #pragma GLOBAL_ASM("asm/Core/x/xHudText.s", "type_name__Q24xhud10text_assetFv")
