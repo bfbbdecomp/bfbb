91ba4fa404512fdb87ec776e45f9e33f219e2d6a
Matt <5638426+mattbruv@users.noreply.github.com>
2020-10-23 10:45:07 -0400

    xEntDrive.s

--- src/Core/x/xEntDrive.cpp
+++ src/Core/x/xEntDrive.cpp
@@ -2,20 +2,20 @@
 
 #include <types.h>
 
-// xEntDriveInit(xEntDrive*,xEnt*)
+// func_8001BB54
 #pragma GLOBAL_ASM("asm/Core/x/xEntDrive.s", "xEntDriveInit__FP9xEntDriveP4xEnt")
 
-// xEntDriveMount(xEntDrive*,xEnt*,float,const xCollis*)
+// func_8001BBA4
 #pragma GLOBAL_ASM("asm/Core/x/xEntDrive.s", "xEntDriveMount__FP9xEntDriveP4xEntfPC7xCollis")
 
-// xEntDriveDismount(xEntDrive*,float)
+// func_8001BFDC
 #pragma GLOBAL_ASM("asm/Core/x/xEntDrive.s", "xEntDriveDismount__FP9xEntDrivef")
 
-// xEntDriveUpdate(xEntDrive*,xScene*,float,const xCollis*)
+// func_8001C0CC
 #pragma GLOBAL_ASM("asm/Core/x/xEntDrive.s", "xEntDriveUpdate__FP9xEntDriveP6xScenefPC7xCollis")
 
-// xCollis::tri_data::operator =(const xCollis::tri_data&)
+// func_8001C85C
 #pragma GLOBAL_ASM("asm/Core/x/xEntDrive.s", "__as__Q27xCollis8tri_dataFRCQ27xCollis8tri_data")
 
-// xVec3::operator =(float)
+// func_8001C878
 #pragma GLOBAL_ASM("asm/Core/x/xEntDrive.s", "__as__5xVec3Ff")
