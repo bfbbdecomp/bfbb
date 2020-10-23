8a8fa365a036f022a74f794f1d28729c9f7edbe6
Matt <5638426+mattbruv@users.noreply.github.com>
2020-10-23 10:47:58 -0400

    zCameraTweak.s

--- src/Game/zCameraTweak.cpp
+++ src/Game/zCameraTweak.cpp
@@ -2,41 +2,47 @@
 
 #include <types.h>
 
-// zCameraTweak_LookPreCalc(zCamTweakLook*,float,float,float)
+// func_8017EEA4
 #pragma GLOBAL_ASM("asm/Game/zCameraTweak.s", "zCameraTweak_LookPreCalc__FP13zCamTweakLookfff")
 
-// zCameraTweakGlobal_Init()
+// func_8017EF30
 #pragma GLOBAL_ASM("asm/Game/zCameraTweak.s", "zCameraTweakGlobal_Init__Fv")
 
-// zCameraTweakGlobal_Add(unsigned int,float,float,float,float)
+// func_8017EF80
 #pragma GLOBAL_ASM("asm/Game/zCameraTweak.s", "zCameraTweakGlobal_Add__FUiffff")
 
-// zCamTweak::operator =(const zCamTweak&)
+// func_8017F170
 #pragma GLOBAL_ASM("asm/Game/zCameraTweak.s", "__as__9zCamTweakFRC9zCamTweak")
 
-// zCameraTweakGlobal_Remove(unsigned int)
+// func_8017F19C
 #pragma GLOBAL_ASM("asm/Game/zCameraTweak.s", "zCameraTweakGlobal_Remove__FUi")
 
-// zCameraTweakGlobal_Reset()
+// func_8017F2D4
 #pragma GLOBAL_ASM("asm/Game/zCameraTweak.s", "zCameraTweakGlobal_Reset__Fv")
 
-// zCameraTweakGlobal_Update(float)
+// func_8017F328
 #pragma GLOBAL_ASM("asm/Game/zCameraTweak.s", "zCameraTweakGlobal_Update__Ff")
 
-// zCameraTweakGlobal_GetD()
+// func_8017F428
 #pragma GLOBAL_ASM("asm/Game/zCameraTweak.s", "zCameraTweakGlobal_GetD__Fv")
 
-// zCameraTweakGlobal_GetH()
+// func_8017F430
 #pragma GLOBAL_ASM("asm/Game/zCameraTweak.s", "zCameraTweakGlobal_GetH__Fv")
 
-// zCameraTweakGlobal_GetPitch()
+// func_8017F438
 #pragma GLOBAL_ASM("asm/Game/zCameraTweak.s", "zCameraTweakGlobal_GetPitch__Fv")
 
-// zCameraTweak_Init(zCameraTweak*,CameraTweak_asset*)
+// func_8017F440
+#pragma GLOBAL_ASM("asm/Game/zCameraTweak.s", "zCameraTweak_Init__FR5xBaseR9xDynAssetUl")
+
+// func_8017F460
 #pragma GLOBAL_ASM("asm/Game/zCameraTweak.s", "zCameraTweak_Init__FP12zCameraTweakP17CameraTweak_asset")
 
-// zCameraTweak_Save(zCameraTweak*,xSerial*)
+// func_8017F4C8
 #pragma GLOBAL_ASM("asm/Game/zCameraTweak.s", "zCameraTweak_Save__FP12zCameraTweakP7xSerial")
 
-// zCameraTweak_Load(zCameraTweak*,xSerial*)
+// func_8017F4E8
 #pragma GLOBAL_ASM("asm/Game/zCameraTweak.s", "zCameraTweak_Load__FP12zCameraTweakP7xSerial")
+
+// func_8017F508
+#pragma GLOBAL_ASM("asm/Game/zCameraTweak.s", "zCameraTweak_EventCB__FP5xBaseP5xBaseUiPCfP5xBase")
