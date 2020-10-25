3df7135647a1ae2fdf9f5cdba26d658ae0ddd245
Dark <dark@volatile.bz>
2020-10-25 00:32:27 -0400

    partially match iEnv

--- src/Core/p2/iEnv.cpp
+++ src/Core/p2/iEnv.cpp
@@ -2,29 +2,103 @@
 
 #include <types.h>
 
-// func_800C2EAC
-#pragma GLOBAL_ASM("asm/Core/p2/iEnv.s", "SetPipelineCB__FP8RpAtomicPv")
+static void* lbl_803CBAB0;
+static void* lbl_803CBAB4;
+static RwCamera* sPipeCamera;
+static iEnv* lastEnv;
 
-// func_800C2F10
-#pragma GLOBAL_ASM("asm/Core/p2/iEnv.s", "iEnvSetBSP__FP4iEnviP7RpWorld")
+const static float lbl_80260130[6] = { 1000.0f, 1000.0f, 1000.0f, -1000.0f, -1000.0f, -1000.0f };
+
+RpAtomic* SetPipelineCB(RpAtomic* param_1, void* param_2)
+{
+    if (RwCameraBeginUpdate(sPipeCamera) != NULL)
+    {
+        RpAtomicInstance(param_1);
+        RwCameraEndUpdate(sPipeCamera);
+    }
+    if (param_2 != NULL)
+    {
+        param_1->pipeline = (RxPipeline*)param_2;
+    }
+    return param_1;
+}
+
+void iEnvSetBSP(iEnv* env, int32 envDataType, RpWorld* bsp)
+{
+    if (envDataType == 0)
+    {
+        env->world = bsp;
+        return;
+    }
+    if (envDataType == 1)
+    {
+        env->collision = bsp;
+        return;
+    }
+    if (envDataType == 2)
+    {
+        env->fx = bsp;
+        return;
+    }
+    if (envDataType == 3)
+    {
+        env->camera = bsp;
+        return;
+    }
+}
 
 // func_800C2F50
 #pragma GLOBAL_ASM("asm/Core/p2/iEnv.s", "iEnvLoad__FP4iEnvPCvUii")
 
-// func_800C30B4
-#pragma GLOBAL_ASM("asm/Core/p2/iEnv.s", "iEnvFree__FP4iEnv")
+void iEnvFree(iEnv* param_1)
+{
+    _rwFrameSyncDirty();
+    RpWorldDestroy(param_1->world);
+    param_1->world = NULL;
+    if (param_1->fx != NULL)
+    {
+        RpWorldDestroy(param_1->fx);
+        param_1->fx = NULL;
+    }
+    if (param_1->collision != NULL)
+    {
+        RpWorldDestroy(param_1->collision);
+        param_1->collision = NULL;
+    }
+    return;
+}
 
-// func_800C3120
-#pragma GLOBAL_ASM("asm/Core/p2/iEnv.s", "iEnvDefaultLighting__FP4iEnv")
+void iEnvDefaultLighting(iEnv* param)
+{
+}
 
-// func_800C3124
-#pragma GLOBAL_ASM("asm/Core/p2/iEnv.s", "iEnvLightingBasics__FP4iEnvP9xEnvAsset")
+void iEnvLightingBasics(iEnv* param_1, xEnvAsset* param_2)
+{
+}
 
 // func_800C3128
 #pragma GLOBAL_ASM("asm/Core/p2/iEnv.s", "Jsp_ClumpRender__FP7RpClumpP12xJSPNodeInfo")
 
-// func_800C3234
+#ifdef NON_MATCHING
+// this cant cant possibly match until we add the type for xJSPHeader
+void iEnvRender(iEnv* param_1)
+{
+    RwRenderStateSet(rwRENDERSTATESRCBLEND, 5);
+    RwRenderStateSet(rwRENDERSTATEDESTBLEND, 6);
+    if (param_1->jsp == NULL)
+    {
+        RpWorldRender(param_1->world);
+    }
+    else
+    {
+        Jsp_ClumpRender(param_1->jsp->field_0xc, param_1->jsp->field_0x14);
+    }
+    lastEnv = param_1;
+    return;
+}
+#else
 #pragma GLOBAL_ASM("asm/Core/p2/iEnv.s", "iEnvRender__FP4iEnv")
+#endif
 
 // func_800C329C
 #pragma GLOBAL_ASM("asm/Core/p2/iEnv.s", "iEnvEndRenderFX__FP4iEnv")
--- src/Core/p2/iEnv.h
+++ src/Core/p2/iEnv.h
@@ -1,6 +1,5 @@
 #ifndef IENV_H
 #define IENV_H
-
 #include <rpworld.h>
 #include <rwcore.h>
 #include "../x/xJSP.h"
@@ -17,9 +16,15 @@ struct iEnv
     int32 memlvl;
 };
 
+// bringing in xEnv.h breaks the build entirely
+struct xEnvAsset;
+
 void iEnvFree(iEnv* param_1);
 void iEnvRender(iEnv* param_1);
 void iEnvDefaultLighting(iEnv* param_1);
 void iEnvLoad(iEnv* env, void const* data, uint32 dataSize, int32 dataType);
+void iEnvSetBSP(iEnv* param_1, int param_2, RpWorld* param_3);
+void iEnvLightingBasics(iEnv* param_1, xEnvAsset* param_2);
+RpAtomic* SetPipelineCB(RpAtomic* param_1, void* param_2);
 
 #endif
