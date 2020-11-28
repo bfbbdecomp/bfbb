45a94a6d47b0a06f0612eb45ee51c064fd30c92d
Mark Langen <stravant@gmail.com>
2020-11-27 16:31:54 -0800

    zConditional 95% complete
    * 100% match for all functions except zConditionalEventCB, which is
      a single instruction off, put it behind an #if 0.

--- src/Game/zVar.h
+++ src/Game/zVar.h
@@ -1,6 +1,16 @@
 #ifndef ZVAR_H
 #define ZVAR_H
 
+#include <types.h>
 
+struct zVarEntry
+{
+    uint32 entry;
+    uint32 varNameID;
+    int8* varName;
+    uint32 (*varCB)(void*);
+};
+
+void zVarInit(zVarEntry* table);
 
 #endif
