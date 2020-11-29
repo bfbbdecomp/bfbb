b650bbbe2165a5071aaf860119ed029e5bd33cfe
Mark Langen <stravant@gmail.com>
2020-11-29 13:13:00 -0800

    xMovePoint.cpp 60% complete (#42)
    * 100% match: xMovePointLoad, xMovePointSave, xMovePointReset,
      xMovePointSetup, xMovePointSplineDestroy.
    
    * 95% match: xMovePointGetNext, function is a 100% match save
      instruction ordering.
    
    * Did not attempt: xVec3Hdng and xMovePointSplineSetup, too much
      floating point math, need to do some easier float math functions
      first.

--- src/Core/x/xScene.h
+++ src/Core/x/xScene.h
@@ -6,40 +6,42 @@
 #include "xEnv.h"
 #include "xMemMgr.h"
 
-typedef char*(*xSceneBase2NameCallback)(xBase*);
-typedef char*(*xSceneID2NameCallback)(uint32);
-typedef xBase*(*xSceneResolvIDCallback)(uint32);
+typedef char* (*xSceneBase2NameCallback)(xBase*);
+typedef char* (*xSceneID2NameCallback)(uint32);
+typedef xBase* (*xSceneResolvIDCallback)(uint32);
 
 struct xScene
 {
-	uint32 sceneID;
-	uint16 flags;
-	uint16 num_ents;
-	uint16 num_trigs;
-	uint16 num_stats;
-	uint16 num_dyns;
-	uint16 num_npcs;
-	uint16 num_act_ents;
-	uint16 num_nact_ents;
-	float32 gravity;
-	float32 drag;
-	float32 friction;
-	uint16 num_ents_allocd;
-	uint16 num_trigs_allocd;
-	uint16 num_stats_allocd;
-	uint16 num_dyns_allocd;
-	uint16 num_npcs_allocd;
-	xEnt** trigs;
-	xEnt** stats;
-	xEnt** dyns;
-	xEnt** npcs;
-	xEnt** act_ents;
-	xEnt** nact_ents;
-	xEnv* env;
-	xMemPool mempool;
-	xSceneResolvIDCallback resolvID;
-	xSceneBase2NameCallback base2Name;
-	xSceneID2NameCallback id2Name;
+    uint32 sceneID;
+    uint16 flags;
+    uint16 num_ents;
+    uint16 num_trigs;
+    uint16 num_stats;
+    uint16 num_dyns;
+    uint16 num_npcs;
+    uint16 num_act_ents;
+    uint16 num_nact_ents;
+    float32 gravity;
+    float32 drag;
+    float32 friction;
+    uint16 num_ents_allocd;
+    uint16 num_trigs_allocd;
+    uint16 num_stats_allocd;
+    uint16 num_dyns_allocd;
+    uint16 num_npcs_allocd;
+    xEnt** trigs;
+    xEnt** stats;
+    xEnt** dyns;
+    xEnt** npcs;
+    xEnt** act_ents;
+    xEnt** nact_ents;
+    xEnv* env;
+    xMemPool mempool;
+    xSceneResolvIDCallback resolvID;
+    xSceneBase2NameCallback base2Name;
+    xSceneID2NameCallback id2Name;
 };
+
+xBase* xSceneResolvID(xScene* sc, uint32 id);
 
 #endif
