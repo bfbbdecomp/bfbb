7c7e53784b23a48e9c5b851009693514e58784cf
Mark Langen <stravant@gmail.com>
2020-12-19 14:56:23 -0800

    xMemMgr.cpp 5% progress
    * Hard-fought 100% match for xMemInitHeap.
    
    * Attempted several other functions but all the functions in this file
      are proving extremely difficult to 100% match thanks to using a ton
      of temporary variables in their bodies on top of the underlying code
      being complicated to begin with.

--- src/Core/x/xMemMgr.cpp
+++ src/Core/x/xMemMgr.cpp
@@ -1,24 +1,255 @@
 #include "xMemMgr.h"
 
 #include <types.h>
+#include <string.h>
+
+#include "../p2/iSystem.h"
+#include "../p2/iMemMgr.h"
+
+// Size: 0x10
+struct xHeapState_tag
+{
+    uint32 curr;
+    uint16 blk_ct;
+    uint16 pad;
+    uint32 used;
+    uint32 wasted;
+};
+
+struct xMemBlock_tag
+{
+    uint32 addr;
+    uint32 size;
+    int32 align;
+};
+
+struct xMemBlkInfo_tag
+{
+    xMemBlock_tag* header;
+    uint32 pre;
+    uint32 block;
+    uint32 post;
+    uint32 curr;
+    uint32 waste;
+    uint32 total;
+};
+
+struct xMemHeap_tag
+{
+    uint32 flags;
+    uint32 hard_base;
+    uint32 size;
+    int16 opp_heap[2];
+
+    // Offset: 0x10
+    xHeapState_tag state[12];
+
+    // Offset: 0xD0
+    uint16 state_idx;
+    uint16 max_blks;
+    xMemBlock_tag* blk;
+    xMemBlock_tag* lastblk;
+};
+
+struct xMemArea_tag
+{
+    uint32 addr;
+    uint32 size;
+    uint32 flags;
+};
+
+struct xMemInfo_tag
+{
+    xMemArea_tag system;
+
+    // Offset: 0xC
+    xMemArea_tag stack;
+
+    // Offset: 0x18
+    xMemArea_tag DRAM;
+
+    // Offset: 0x24
+    xMemArea_tag SRAM;
+};
+
+extern xMemInfo_tag gMemInfo;
+extern xMemHeap_tag gxHeap[3];
+
+void xMemInitHeap(xMemHeap_tag* heap, uint32 base, uint32 size, uint32 flags);
 
 // func_80033554
-#pragma GLOBAL_ASM("asm/Core/x/xMemMgr.s", "xMemDebug_SoakLog__FPCc")
+void xMemDebug_SoakLog(const int8*)
+{
+}
 
 // func_80033558
+#ifndef NON_MATCHING
 #pragma GLOBAL_ASM("asm/Core/x/xMemMgr.s", "xMemInit__Fv")
+#else
+// The instructions putting 0/1/2 into registers happen in the wrong order.
+void xMemInit()
+{
+    iMemInit();
+    xMemInitHeap(&gxHeap[0], gMemInfo.DRAM.addr, gMemInfo.DRAM.size, 0x8826);
+    xMemInitHeap(&gxHeap[1], gMemInfo.DRAM.addr + gMemInfo.DRAM.size, gMemInfo.DRAM.size, 0x8925);
+    xMemInitHeap(&gxHeap[2], gMemInfo.DRAM.addr + gMemInfo.DRAM.size, gMemInfo.DRAM.size, 0x892A);
+    gxHeap[0].opp_heap[0] = 1;
+    gxHeap[0].opp_heap[1] = 2;
+    gxHeap[1].opp_heap[0] = 0;
+    gxHeap[1].opp_heap[1] = 2;
+    gxHeap[2].opp_heap[0] = 0;
+    gxHeap[2].opp_heap[1] = 1;
+    gActiveHeap = 0;
+}
+#endif
 
 // func_8003361C
-#pragma GLOBAL_ASM("asm/Core/x/xMemMgr.s", "xMemExit__Fv")
+void xMemExit()
+{
+    iMemExit();
+}
 
 // func_8003363C
-#pragma GLOBAL_ASM("asm/Core/x/xMemMgr.s", "xMemInitHeap__FP12xMemHeap_tagUiUiUi")
+void xMemInitHeap(xMemHeap_tag* heap, uint32 base, uint32 size, uint32 flags)
+{
+    uint32 old_base = base;
+    int32 align = 1 << ((flags >> 9) & 0x1F);
+    if (flags & 0x100)
+    {
+        base = base & -align;
+        size -= (old_base - base);
+    }
+    else
+    {
+        base = (old_base + align - 1) & -align;
+        size -= (base - old_base);
+    }
+
+    heap->state_idx = 0;
+    heap->hard_base = base;
+    heap->size = size;
+    heap->flags = flags;
+    heap->lastblk = NULL;
+    xHeapState_tag* sp = &heap->state[heap->state_idx];
+    sp->curr = base;
+    sp->blk_ct = 0;
+
+    if (flags & 0x10000)
+    {
+        heap->max_blks = (1 << ((flags >> 14) & 0x1));
+        heap->blk = (xMemBlock_tag*)malloc(heap->max_blks * sizeof(xMemBlock_tag));
+        memset(heap->blk, 0, heap->max_blks * sizeof(xMemBlock_tag));
+    }
+    else
+    {
+        heap->max_blks = -1;
+        heap->blk = 0;
+    }
+    heap->opp_heap[0] = -1;
+    heap->opp_heap[1] = -1;
+}
 
 // func_80033734
-#pragma GLOBAL_ASM("asm/Core/x/xMemMgr.s", "xMemGetBlockInfo__FP12xMemHeap_tagUiiP15xMemBlkInfo_tag")
+#if 1
+#pragma GLOBAL_ASM("asm/Core/x/xMemMgr.s",                                                         \
+                   "xMemGetBlockInfo__FP12xMemHeap_tagUiiP15xMemBlkInfo_tag")
+#else
+// Not particularly close. This function is a nightmare, it has so many
+// variables to untangle.
+void xMemGetBlockInfo(xMemHeap_tag* heap, uint32 size, int32 align, xMemBlkInfo_tag* info)
+{
+    int32 hdr = 1;
+    xHeapState_tag* sp = &heap->state[heap->state_idx];
+    if (heap->flags & 0x100)
+    {
+        hdr = -1;
+    }
+
+    int32 remainder = 0xc & -(heap->flags & 0x10000);
+    int32 pre;
+    int32 post;
+    int32 block;
+    int32 r8;
+    if (heap->flags & 0x100)
+    {
+        block = -remainder;
+        pre = -(remainder + size + ((sp->curr - remainder - size) & (align - 1)));
+        post = pre + size;
+        r8 = -pre;
+    }
+    else
+    {
+        block = 0;
+        if (r8 = (align - 1) & (sp->curr + remainder))
+        {
+            r8 = align;
+        }
+        pre = (align + remainder) - r8;
+        post = pre + size;
+        r8 = post;
+    }
+
+    int32 total = (r8 + 3) & 0xFFFFFFFE;
+    if (heap->flags & 0x10000)
+    {
+        info->header = &heap->blk[sp->curr];
+    }
+    else
+    {
+        info->header = (xMemBlock_tag*)(sp->curr + block);
+    }
+    info->pre = sp->curr + pre;
+    info->block = sp->curr + pre;
+    info->post = sp->curr + post;
+    info->curr = sp->curr + hdr * total;
+    info->waste = total - (remainder + size);
+    info->total = total;
+}
+#endif
 
 // func_80033864
+#if 1
 #pragma GLOBAL_ASM("asm/Core/x/xMemMgr.s", "xMemGrowAlloc__FUiUi")
+#else
+// Not particularly close. Not 100% confident that I understand what's going on,
+// in particular the bit that goes:
+// (heap->lastblk->size + size) - heap->lastblk->size;
+// It really seems like the assembly code is adding and then immediately
+// subtracting again, and I don't see why it would want to do that.
+#undef xMemGrowAlloc
+void* xMemGrowAlloc(uint32 heapID, uint32 size)
+{
+    size = (size + 3) & 0xFFFFFFFC;
+    xMemHeap_tag* heap = &gxHeap[heapID];
+    xHeapState_tag* sp = &heap->state[heap->state_idx];
+    // pad size to multiple of 4 bytes
+    xMemBlock_tag* hdr = heap->lastblk;
+    uint32 anotherVar = (heap->lastblk->size + size) - heap->lastblk->size;
+    if (sp->used + anotherVar > heap->size)
+    {
+        return 0;
+    }
+    else
+    {
+        void* memptr;
+        if (heap->flags & 0x100)
+        {
+            memptr = (void*)(hdr->addr - size);
+            hdr->addr = (uint32)memptr;
+            sp->curr -= anotherVar;
+        }
+        else
+        {
+            sp->curr += anotherVar;
+            memptr = (void*)(hdr->addr + heap->lastblk->size);
+        }
+        sp->used += anotherVar;
+        hdr->size += size;
+        memset(memptr, 0, size);
+        return memptr;
+    }
+}
+#endif
 
 // func_80033940
 #pragma GLOBAL_ASM("asm/Core/x/xMemMgr.s", "xMemAlloc__FUiUii")
