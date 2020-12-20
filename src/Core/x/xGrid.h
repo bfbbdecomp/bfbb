d86a373560c58a6fafde9932c9b5c1e3d8f58cf4
Mark Langen <stravant@gmail.com>
2020-12-20 05:47:51 -0800

    xGrid.cpp 50% complete (#56)
    * 100% match for xGridIterFirstCell, xGridGetCell, xGridUpdate,
      xGridRemove, xGridAdd, xGridAddToCell, xGridEmpty, xGridKill, and
      xGridBoundInit.
    
    * xGridInit and xGridGetCell NON_MATCHING on some typical floating point
      instructions.
    
    * Did not attempt xGridAdd main variant, way too many floating point
      instructions.

--- src/Core/x/xGrid.h
+++ src/Core/x/xGrid.h
@@ -3,17 +3,63 @@
 
 #include <types.h>
 
+#include "xMath3.h"
+
+struct xEnt;
+
 struct xGridBound
 {
     void* data;
     uint16 gx;
     uint16 gz;
+
+    // Offset: 0x8
     uint8 ingrid;
     uint8 oversize;
     uint8 deleted;
     uint8 gpad;
+
+    // Offset: 0xC
     xGridBound** head;
     xGridBound* next;
 };
+
+struct xGrid
+{
+    uint8 ingrid_id;
+    uint8 pad[3];
+    uint16 nx;
+    uint16 nz;
+
+    // Offset: 0x8
+    float32 minx;
+    float32 minz;
+    float32 maxx;
+    float32 maxz;
+
+    // Offset: 0x18
+    float32 csizex;
+    float32 csizez;
+    float32 inv_csizex;
+    float32 inv_csizez;
+
+    // Offset: 0x28
+    float32 maxr;
+    xGridBound** cells;
+    xGridBound* other;
+};
+
+struct xGridIterator
+{
+    xGridBound** listhead;
+    xGridBound* curcell;
+    uint32 delfound;
+};
+
+void xGridEmpty(xGrid* grid);
+int32 xGridRemove(xGridBound* gridb);
+xGridBound** xGridGetCell(xGrid* grid, const xEnt* ent, int32& grx, int32& grz);
+void xGridGetCell(xGrid* grid, float32 posx, float32 posy, float32 posz, int32& grx, int32& grz);
+void xGridIterFirstCell(xGrid* grid, int32 grx, int32 grz, xGridIterator& iter);
 
 #endif
