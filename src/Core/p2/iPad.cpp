945c826f41d6384ee999ce63f486c4c2679654b8
Matt <5638426+mattbruv@users.noreply.github.com>
2020-12-24 00:13:54 -0500

    start ipad

--- src/Core/p2/iPad.cpp
+++ src/Core/p2/iPad.cpp
@@ -1,9 +1,13 @@
+#include <types.h>
+#include <dolphin.h>
+
 #include "iPad.h"
 
-#include <types.h>
-
-// func_800CA920
-#pragma GLOBAL_ASM("asm/Core/p2/iPad.s", "iPadInit__Fv")
+int32 iPadInit()
+{
+    PADInit();
+    return 1;
+}
 
 // func_800CA944
 #pragma GLOBAL_ASM("asm/Core/p2/iPad.s", "iPadEnable__FP8_tagxPads")
@@ -14,20 +18,30 @@
 // func_800CA9F4
 #pragma GLOBAL_ASM("asm/Core/p2/iPad.s", "iPadUpdate__FP8_tagxPadPUi")
 
-// func_800CAE38
-#pragma GLOBAL_ASM("asm/Core/p2/iPad.s", "iPadConvFromGCN__FUiUiUi")
+int32 iPadConvFromGCN(uint32 a, uint32 b, uint32 c)
+{
+    // TODO: this can probably be simplified,
+    // basically a copy of ghidra's output
+    return c & (int32)(-(a & b) | a & b) >> 0x1f;
+}
 
-// func_800CAE50
-#pragma GLOBAL_ASM("asm/Core/p2/iPad.s", "iPadRumbleFx__FP8_tagxPadP11_tagxRumblef")
+void iPadRumbleFx(_tagxPad* p, _tagxRumble* r, float32 time_passed)
+{
+}
 
-// func_800CAE54
-#pragma GLOBAL_ASM("asm/Core/p2/iPad.s", "iPadStopRumble__FP8_tagxPad")
+void iPadStopRumble(_tagxPad* pad)
+{
+    PADControlMotor(pad->port, 0);
+}
 
 // func_800CAE7C
 #pragma GLOBAL_ASM("asm/Core/p2/iPad.s", "iPadStopRumble__Fv")
 
-// func_800CAEC0
-#pragma GLOBAL_ASM("asm/Core/p2/iPad.s", "iPadStartRumble__FP8_tagxPadP11_tagxRumble")
+void iPadStartRumble(_tagxPad* pad, _tagxRumble* rumble)
+{
+    PADControlMotor(pad->port, 1);
+}
 
-// func_800CAEE8
-#pragma GLOBAL_ASM("asm/Core/p2/iPad.s", "iPadKill__Fv")
+void iPadKill()
+{
+}
