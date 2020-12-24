f8cb687f50b6b2e35d73a2539bdb9d8445038eaf
mattbruv <5638426+mattbruv@users.noreply.github.com>
2020-12-24 00:48:32 -0500

    Ipad (#61)
    * start ipad
    
    * fix xpad include issue
    
    * ipad
    
    * xpad fix

--- src/Core/p2/iPad.cpp
+++ src/Core/p2/iPad.cpp
@@ -1,12 +1,28 @@
+#include <types.h>
+#include <dolphin.h>
+
 #include "iPad.h"
 
-#include <types.h>
-
-// func_800CA920
-#pragma GLOBAL_ASM("asm/Core/p2/iPad.s", "iPadInit__Fv")
+int32 iPadInit()
+{
+    PADInit();
+    return 1;
+}
 
 // func_800CA944
+#if 1
 #pragma GLOBAL_ASM("asm/Core/p2/iPad.s", "iPadEnable__FP8_tagxPads")
+#else
+_tagxPad* iPadEnable(_tagxPad* pad, int16 port)
+{
+    pad->port = port;
+    pad->slot = 0;
+    pad->state = ePad_Enabled;
+    // *(undefined4*)(gTrcPad + (int)*(short*)(pad + 0x54) * 0xc + 8) = 2;
+    // *(uint*)(pad + 0x40) = *(uint*)(pad + 0x40) | 3;
+    // *(uint*)(pad + 0x40) = *(uint*)(pad + 0x40) | 4;
+}
+#endif
 
 // func_800CA98C
 #pragma GLOBAL_ASM("asm/Core/p2/iPad.s", "iPadConvStick__Ff")
@@ -14,20 +30,30 @@
 // func_800CA9F4
 #pragma GLOBAL_ASM("asm/Core/p2/iPad.s", "iPadUpdate__FP8_tagxPadPUi")
 
-// func_800CAE38
-#pragma GLOBAL_ASM("asm/Core/p2/iPad.s", "iPadConvFromGCN__FUiUiUi")
+int32 iPadConvFromGCN(uint32 a, uint32 b, uint32 c)
+{
+    // TODO: this can probably be simplified,
+    // basically a copy of ghidra's output
+    return c & (int32)(-(a & b) | a & b) >> 0x1f;
+}
 
-// func_800CAE50
-#pragma GLOBAL_ASM("asm/Core/p2/iPad.s", "iPadRumbleFx__FP8_tagxPadP11_tagxRumblef")
+void iPadRumbleFx(_tagxPad* p, _tagxRumble* r, float32 time_passed)
+{
+}
 
-// func_800CAE54
-#pragma GLOBAL_ASM("asm/Core/p2/iPad.s", "iPadStopRumble__FP8_tagxPad")
+void iPadStopRumble(_tagxPad* pad)
+{
+    PADControlMotor(pad->port, 0);
+}
 
 // func_800CAE7C
 #pragma GLOBAL_ASM("asm/Core/p2/iPad.s", "iPadStopRumble__Fv")
 
-// func_800CAEC0
-#pragma GLOBAL_ASM("asm/Core/p2/iPad.s", "iPadStartRumble__FP8_tagxPadP11_tagxRumble")
+void iPadStartRumble(_tagxPad* pad, _tagxRumble* rumble)
+{
+    PADControlMotor(pad->port, 1);
+}
 
-// func_800CAEE8
-#pragma GLOBAL_ASM("asm/Core/p2/iPad.s", "iPadKill__Fv")
+void iPadKill()
+{
+}
