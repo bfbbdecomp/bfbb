6307c224637227b05e4b2640d48187391577cffc
earthcrafterman <banddstudios@gmail.com>
2020-12-26 15:05:26 -0500

    Formatted zNPCCommon.h

--- src/Game/zNPCTypeCommon.h
+++ src/Game/zNPCTypeCommon.h
@@ -147,96 +147,96 @@ enum en_LASSO_STATUS
 
 enum en_mdlvert
 {
-	NPC_MDLVERT_ATTACKBASE,
-	NPC_MDLVERT_ATTACK,
-	NPC_MDLVERT_ATTACK1,
-	NPC_MDLVERT_ATTACK2,
-	NPC_MDLVERT_ATTACK3,
-	NPC_MDLVERT_ATTACK4,
-	NPC_MDLVERT_LOSEYEBALL,
-	NPC_MDLVERT_DMGSMOKE_A,
-	NPC_MDLVERT_DMGSMOKE_B,
-	NPC_MDLVERT_DMGSMOKE_C,
-	NPC_MDLVERT_DMGFLAME_A,
-	NPC_MDLVERT_DMGFLAME_B,
-	NPC_MDLVERT_DMGFLAME_C,
-	NPC_MDLVERT_PROPEL,
-	NPC_MDLVERT_EXHAUST,
-	NPC_MDLVERT_GEN01,
-	NPC_MDLVERT_GEN02,
-	NPC_MDLVERT_GEN03,
-	NPC_MDLVERT_GEN04,
-	NPC_MDLVERT_GEN05,
-	NPC_MDLVERT_NOMORE,
-	NPC_MDLVERT_FORCEINT = 0x7fffffff
+    NPC_MDLVERT_ATTACKBASE,
+    NPC_MDLVERT_ATTACK,
+    NPC_MDLVERT_ATTACK1,
+    NPC_MDLVERT_ATTACK2,
+    NPC_MDLVERT_ATTACK3,
+    NPC_MDLVERT_ATTACK4,
+    NPC_MDLVERT_LOSEYEBALL,
+    NPC_MDLVERT_DMGSMOKE_A,
+    NPC_MDLVERT_DMGSMOKE_B,
+    NPC_MDLVERT_DMGSMOKE_C,
+    NPC_MDLVERT_DMGFLAME_A,
+    NPC_MDLVERT_DMGFLAME_B,
+    NPC_MDLVERT_DMGFLAME_C,
+    NPC_MDLVERT_PROPEL,
+    NPC_MDLVERT_EXHAUST,
+    NPC_MDLVERT_GEN01,
+    NPC_MDLVERT_GEN02,
+    NPC_MDLVERT_GEN03,
+    NPC_MDLVERT_GEN04,
+    NPC_MDLVERT_GEN05,
+    NPC_MDLVERT_NOMORE,
+    NPC_MDLVERT_FORCEINT = 0x7fffffff
 };
 
 enum en_LASSO_EVENT
 {
-	LASS_EVNT_BEGIN,
-	LASS_EVNT_ENDED,
-	LASS_EVNT_GRABSTART,
-	LASS_EVNT_GRABEND,
-	LASS_EVNT_YANK,
-	LASS_EVNT_ABORT,
-	LASS_EVNT_NOMORE,
-	LASS_EVNT_FORCEINT = 0x7fffffff
+    LASS_EVNT_BEGIN,
+    LASS_EVNT_ENDED,
+    LASS_EVNT_GRABSTART,
+    LASS_EVNT_GRABEND,
+    LASS_EVNT_YANK,
+    LASS_EVNT_ABORT,
+    LASS_EVNT_NOMORE,
+    LASS_EVNT_FORCEINT = 0x7fffffff
 };
 
 enum en_NPC_MSG_ID
 {
-	NPC_MID_NONE,
-	NPC_MID_SYSEVENT,
-	NPC_MID_RESPAWN,
-	NPC_MID_DAMAGE,
-	NPC_MID_EXPLOSION,
-	NPC_MID_BUNNYHOP,
-	NPC_MID_DTRON_NPCEMIT,
-	NPC_MID_DTRON_NPCAVAIL,
-	NPC_MID_DTRON_ISDEAD,
-	NPC_MID_CHAT_REQUEST,
-	NPC_MID_CHAT_DECLINE,
-	NPC_MID_CHAT_ACCEPT,
-	NPC_MID_CHAT_DONE,
-	NPC_MID_CHAT_ABORT,
-	NPC_MID_TALKSTART,
-	NPC_MID_TALKON,
-	NPC_MID_TALKOFF,
-	NPC_MID_SAWPLYR,
-	NPC_MID_NEEDMEDIC,
-	NPC_MID_UNDERATTACK,
-	NPC_MID_NPCDIED,
-	NPC_MID_PLYRSPATULA,
-	NPC_MID_BECOMESCARED,
-	NPC_MID_NOLONGERSCARED,
-	NPC_MID_CELEBRATE,
-	NPC_MID_STUN,
-	NPC_MID_SCRIPTBEGIN,
-	NPC_MID_SCRIPTEND,
-	NPC_MID_SCRIPTHALT,
-	NPC_MID_DEV_ANIMSPIN,
-	NPC_MID_DEV_ANIMCYCLE,
-	NPC_MID_DEV_HEROMODE,
-	NPC_MID_DEV_DONE,
-	NPC_MID_NOMORE,
-	NPC_MID_FORCE = 0x7fffffff
+    NPC_MID_NONE,
+    NPC_MID_SYSEVENT,
+    NPC_MID_RESPAWN,
+    NPC_MID_DAMAGE,
+    NPC_MID_EXPLOSION,
+    NPC_MID_BUNNYHOP,
+    NPC_MID_DTRON_NPCEMIT,
+    NPC_MID_DTRON_NPCAVAIL,
+    NPC_MID_DTRON_ISDEAD,
+    NPC_MID_CHAT_REQUEST,
+    NPC_MID_CHAT_DECLINE,
+    NPC_MID_CHAT_ACCEPT,
+    NPC_MID_CHAT_DONE,
+    NPC_MID_CHAT_ABORT,
+    NPC_MID_TALKSTART,
+    NPC_MID_TALKON,
+    NPC_MID_TALKOFF,
+    NPC_MID_SAWPLYR,
+    NPC_MID_NEEDMEDIC,
+    NPC_MID_UNDERATTACK,
+    NPC_MID_NPCDIED,
+    NPC_MID_PLYRSPATULA,
+    NPC_MID_BECOMESCARED,
+    NPC_MID_NOLONGERSCARED,
+    NPC_MID_CELEBRATE,
+    NPC_MID_STUN,
+    NPC_MID_SCRIPTBEGIN,
+    NPC_MID_SCRIPTEND,
+    NPC_MID_SCRIPTHALT,
+    NPC_MID_DEV_ANIMSPIN,
+    NPC_MID_DEV_ANIMCYCLE,
+    NPC_MID_DEV_HEROMODE,
+    NPC_MID_DEV_DONE,
+    NPC_MID_NOMORE,
+    NPC_MID_FORCE = 0x7fffffff
 };
 
 enum en_NPC_MSG_DATA
 {
-	NPC_MDAT_BLANK,
-	NPC_MDAT_SYSEVENT,
-	NPC_MDAT_BLAST,
-	NPC_MDAT_CHAT,
-	NPC_MDAT_SPAWN,
-	NPC_MDAT_TARGET,
-	NPC_MDAT_DAMAGE,
-	NPC_MDAT_STUN,
-	NPC_MDAT_SCRIPT,
-	NPC_MDAT_MOUNT,
-	NPC_MDAT_AREANOTIFY,
-	NPC_MDAT_NOMORE,
-	NPC_MDAT_FORCE = 0x7fffffff
+    NPC_MDAT_BLANK,
+    NPC_MDAT_SYSEVENT,
+    NPC_MDAT_BLAST,
+    NPC_MDAT_CHAT,
+    NPC_MDAT_SPAWN,
+    NPC_MDAT_TARGET,
+    NPC_MDAT_DAMAGE,
+    NPC_MDAT_STUN,
+    NPC_MDAT_SCRIPT,
+    NPC_MDAT_MOUNT,
+    NPC_MDAT_AREANOTIFY,
+    NPC_MDAT_NOMORE,
+    NPC_MDAT_FORCE = 0x7fffffff
 };
 
 struct zNPCLassoInfo
@@ -282,10 +282,10 @@ struct zNPCCommon : xNPCBasic
 
     void Process(xScene* xscn, float32 dt);
     void Destroy();
-    
+
     // vTable
     virtual int32 NPCMessage(NPCMsg* mail);
-	virtual void RenderExtra();
+    virtual void RenderExtra();
     virtual void RenderExtraPostParticles();
     virtual void ParseINI();
     virtual void ParseLinks();
@@ -303,95 +303,95 @@ struct zNPCCommon : xNPCBasic
 
 struct NPCSysEvent
 {
-	int32 doLinkEvents;
-	int32 handled;
-	xBase* from;
-	xBase* to;
-	uint32 toEvent;
-	float32 toParam[4];
-	xBase* toParamWidget;
+    int32 doLinkEvents;
+    int32 handled;
+    xBase* from;
+    xBase* to;
+    uint32 toEvent;
+    float32 toParam[4];
+    xBase* toParamWidget;
 };
 
 struct NPCBlastInfo
 {
-	xVec3 pos_blast;
-	float32 rad_blast;
-	float32 spd_expand;
+    xVec3 pos_blast;
+    float32 rad_blast;
+    float32 spd_expand;
 };
 
 struct NPCChatInfo
 {
-	xVec3 pos_chat;
-	float32 tym_chat;
+    xVec3 pos_chat;
+    float32 tym_chat;
 };
 
 struct NPCSpawnInfo
 {
-	xVec3 pos_spawn;
-	zMovePoint* nav_firstMovepoint;
-	zMovePoint* nav_spawnReference;
-	int32 spawnSuccess;
+    xVec3 pos_spawn;
+    zMovePoint* nav_firstMovepoint;
+    zMovePoint* nav_spawnReference;
+    int32 spawnSuccess;
 };
 
 struct NPCTargetInfo
 {
-	xBase* bas_tgt;
-	xVec3 pos_tgt;
+    xBase* bas_tgt;
+    xVec3 pos_tgt;
 };
 
 struct NPCDamageInfo
 {
-	en_NPC_DAMAGE_TYPE dmg_type;
-	xBase* dmg_from;
-	xVec3 vec_dmghit;
+    en_NPC_DAMAGE_TYPE dmg_type;
+    xBase* dmg_from;
+    xVec3 vec_dmghit;
 };
 
 struct NPCStunInfo
 {
-	float32 tym_stuntime;
-	en_NPC_CARRY_STATE carrystate;
-	int32 allowStun;
+    float32 tym_stuntime;
+    en_NPC_CARRY_STATE carrystate;
+    int32 allowStun;
 };
 
 struct NPCScriptInfo
 {
-	uint32 aid_playanim;
+    uint32 aid_playanim;
 };
 
 struct NPCMountInfo
 {
-	xEnt* ent_toMount;
-	xCollis* col_forMount;
+    xEnt* ent_toMount;
+    xCollis* col_forMount;
 };
 
 struct NPCAreaInfo
 {
-	zNPCCommon* npc_origin;
-	xVec3 pos_origin;
+    zNPCCommon* npc_origin;
+    xVec3 pos_origin;
 };
 
 struct NPCMsg
 {
-	en_NPC_MSG_ID msgid;
-	uint32 sendto;
-	uint32 from;
-	en_NPC_MSG_DATA infotype;
-	union
-	{
-		NPCSysEvent sysevent;
-		NPCBlastInfo blastarea;
-		NPCChatInfo chatter;
-		NPCSpawnInfo spawning;
-		NPCTargetInfo target;
-		NPCDamageInfo dmgdata;
-		NPCStunInfo stundata;
-		NPCScriptInfo scriptdata;
-		NPCMountInfo mountdata;
-		NPCAreaInfo areadata;
-	};
-	void* attached;
-	NPCMsg* next;
-	float32 tmr_delay;
+    en_NPC_MSG_ID msgid;
+    uint32 sendto;
+    uint32 from;
+    en_NPC_MSG_DATA infotype;
+    union
+    {
+        NPCSysEvent sysevent;
+        NPCBlastInfo blastarea;
+        NPCChatInfo chatter;
+        NPCSpawnInfo spawning;
+        NPCTargetInfo target;
+        NPCDamageInfo dmgdata;
+        NPCStunInfo stundata;
+        NPCScriptInfo scriptdata;
+        NPCMountInfo mountdata;
+        NPCAreaInfo areadata;
+    };
+    void* attached;
+    NPCMsg* next;
+    float32 tmr_delay;
 };
 
 void ZNPC_Common_Startup();
