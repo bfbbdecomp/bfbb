2f18f7d374b9307aaed94d7c83114110dba20cec
Matt <5638426+mattbruv@users.noreply.github.com>
2020-12-30 08:26:47 -0500

    stub zAnimListInit

--- src/Game/zAnimList.cpp
+++ src/Game/zAnimList.cpp
@@ -3,6 +3,7 @@
 #include "../Core/x/xAnim.h"
 
 #include "zAnimList.h"
+#include "zAssetTypes.h"
 
 extern int32 nals;
 extern uint32* aids; // anim IDs (not AIDS, you fool)
@@ -15,7 +16,23 @@ uint32 AlwaysConditional(xAnimTransition*, xAnimSingle*, void*)
 }
 
 // func_8004E7E8
+#if 1
 #pragma GLOBAL_ASM("asm/Game/zAnimList.s", "zAnimListInit__Fv")
+#else
+void zAnimListInit()
+{
+    int32 i;
+    uint32 size;
+    zAnimListAsset* zala;
+    st_PKR_ASSET_TOCINFO ainfo;
+    xAnimTable* atbl;
+    void* buf;
+    xAnimFile* afile;
+    int32 idle_exists;
+    int32 j;
+    xAnimFile* afile;
+}
+#endif
 
 void zAnimListExit()
 {
