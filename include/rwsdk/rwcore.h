5c36e034985228d633daa2c9139852f465003baf
mattbruv <5638426+mattbruv@users.noreply.github.com>
2021-01-01 19:52:57 -0500

    Fix build error (#80)
    * accidentally broke the build because renderware doesn't include our custom types
    
    * fix

--- include/rwsdk/rwcore.h
+++ include/rwsdk/rwcore.h
@@ -19,8 +19,8 @@ struct _RxObjSpace3DVertex
     RwV3d objVertex;
     RxColorUnion c;
     RwV3d objNormal;
-    float32 u;
-    float32 v;
+    RwReal u;
+    RwReal v;
 };
 
 typedef RxObjSpace3DVertex RxObjSpace3DLitVertex;
