8174b84f8d33d865bfd2d9789ed2e9002a207bbc
Seil Weiss <32021834+pslehisl@users.noreply.github.com>
2021-01-05 23:10:48 -0600

    decomp

--- src/Core/x/xstransvc.h
+++ src/Core/x/xstransvc.h
@@ -1,11 +1,13 @@
 #ifndef XSTRANSVC_H
 #define XSTRANSVC_H
 
-#include <types.h>
+#include "xpkrsvc.h"
 
 void* xSTFindAsset(uint32 aid, uint32* size);
 int32 xSTAssetCountByType(uint32 type);
 void* xSTFindAssetByType(uint32 type, int32 idx, uint32* size);
 char* xSTAssetName(uint32 aid);
+int32 xSTGetAssetInfo(uint32 aid, st_PKR_ASSET_TOCINFO* tocainfo);
+char* xST_xAssetID_HIPFullPath(uint32 aid);
 
 #endif
