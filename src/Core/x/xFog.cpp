0650886377947a1c521ae0c060caffbd761d2ee2
Gota7 <gotaseven@gmail.com>
2021-01-06 21:20:58 -0500

    Fully Decompile xFog.cpp
    But removing the ASM file doesn't work because of the float :{

--- src/Core/x/xFog.cpp
+++ src/Core/x/xFog.cpp
@@ -1,27 +1,84 @@
 #include "xFog.h"
+#include "xEvent.h"
+
+#include "iCamera.h"
 
 #include <types.h>
 
-// func_8001FDBC
-#pragma GLOBAL_ASM("asm/Core/x/xFog.s", "xFogClearFog__Fv")
+extern float32 lbl_803CCC50; // 0.0f.
 
-// func_8001FDE4
-#pragma GLOBAL_ASM("asm/Core/x/xFog.s", "xFogInit__FPvPv")
+void xFogClearFog()
+{
+    iCameraSetFogParams(0, lbl_803CCC50);
+}
 
-// func_8001FE04
-#pragma GLOBAL_ASM("asm/Core/x/xFog.s", "xFogInit__FP5xBaseP9xFogAsset")
+void xFogInit(void* b, void* tasset)
+{
+    xFogInit((xBase*)b, (xFogAsset*)tasset);
+}
 
-// func_8001FE70
-#pragma GLOBAL_ASM("asm/Core/x/xFog.s", "xFogReset__FP5_xFog")
+void xFogInit(xBase* ent, xFogAsset* tasset)
+{
+    xBaseInit(ent, (xBaseAsset*)tasset);
+    ent->eventFunc = xFogEventCB;
+    ((_xFog*)ent)->tasset = tasset;
+    if (ent->linkCount != 0)
+    {
+        ent->link = (xLinkAsset*)(((_xFog*)ent)->tasset + 1);
+    }
+    else
+    {
+        ent->link = NULL;
+    }
+}
 
-// func_8001FE94
-#pragma GLOBAL_ASM("asm/Core/x/xFog.s", "xFogSave__FP5_xFogP7xSerial")
+void xFogReset(_xFog* ent)
+{
+    xBaseReset((xBase*)ent, (xBaseAsset*)ent->tasset);
+}
 
-// func_8001FEB4
-#pragma GLOBAL_ASM("asm/Core/x/xFog.s", "xFogLoad__FP5_xFogP7xSerial")
+void xFogSave(_xFog* ent, xSerial* s)
+{
+    xBaseSave((xBase*)ent, s);
+}
 
-// func_8001FED4
-#pragma GLOBAL_ASM("asm/Core/x/xFog.s", "xFogEventCB__FP5xBaseP5xBaseUiPCfP5xBase")
+void xFogLoad(_xFog* ent, xSerial* s)
+{
+    xBaseLoad((xBase*)ent, s);
+}
 
-// func_8001FFAC
-#pragma GLOBAL_ASM("asm/Core/x/xFog.s", "xFogUpdate__FP5xBaseP6xScenef")
+int32 xFogEventCB(xBase* to, xBase* from, uint32 toEvent, const float32* toParam, xBase* b3)
+{
+    switch (toEvent)
+    {
+    case eEventOn:
+        iFogParams fog;
+        fog.type = rwFOGTYPELINEAR;
+        xFogAsset* fromFog = ((_xFog*)from)->tasset;
+        fog.start = fromFog->fogStart;
+        fog.stop = fromFog->fogStop;
+        fog.density = fromFog->fogDensity;
+        fog.fogcolor.red = fromFog->fogColor[0];
+        fog.fogcolor.green = fromFog->fogColor[1];
+        fog.fogcolor.blue = fromFog->fogColor[2];
+        fog.fogcolor.alpha = fromFog->fogColor[3];
+        fog.bgcolor.red = fromFog->bkgndColor[0];
+        fog.bgcolor.green = fromFog->bkgndColor[1];
+        fog.bgcolor.blue = fromFog->bkgndColor[2];
+        fog.bgcolor.alpha = fromFog->bkgndColor[3];
+        fog.table = NULL;
+        iCameraSetFogParams(&fog, fromFog->transitionTime);
+        break;
+    case eEventOff:
+        iCameraSetFogParams(NULL, lbl_803CCC50);
+        break;
+    case eEventReset:
+        xFogReset((_xFog*)from);
+        break;
+    }
+    return eEventEnable;
+}
+
+void xFogUpdate(xBase* ent, xScene* sc, float32 dt)
+{
+}
--- src/Core/x/xFog.h
+++ src/Core/x/xFog.h
@@ -1,6 +1,35 @@
 #ifndef XFOG_H
 #define XFOG_H
 
+#include "xEnt.h"
+#include "xBase.h"
 
+#include <types.h>
+
+struct xFogAsset : xBaseAsset
+{
+    uint8 bkgndColor[4];
+    uint8 fogColor[4];
+    float32 fogDensity;
+    float32 fogStart;
+    float32 fogStop;
+    float32 transitionTime;
+    uint8 fogType;
+    uint8 padFog[3];
+};
+
+struct _xFog : xBase
+{
+    xFogAsset* tasset;
+};
+
+void xFogClearFog();
+void xFogInit(void* b, void* tasset);
+void xFogInit(xBase* ent, xFogAsset* tasset);
+void xFogReset(_xFog* ent);
+void xFogSave(_xFog* ent, xSerial* s);
+void xFogLoad(_xFog* ent, xSerial* s);
+int32 xFogEventCB(xBase* to, xBase* from, uint32 toEvent, const float32* toParam, xBase* b3);
+void xFogUpdate(xBase* ent, xScene* sc, float32 dt);
 
 #endif
