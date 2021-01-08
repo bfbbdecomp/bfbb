8b1a8fc42a655233f500b1b27e92731d0b565acb
Gota7 <gotaseven@gmail.com>
2021-01-07 23:52:47 -0500

    Partial Progress On xSurface.cpp
    Progress.

--- src/Core/x/xSurface.h
+++ src/Core/x/xSurface.h
@@ -19,8 +19,19 @@ struct xSurface : xBase
     uint8 state;
     uint8 pad[3];
     void* moprops;
+
+    void operator=(const xSurface& ent);
+    void operator=(const xBase& ent);
 };
 
 #define XSURFACE_TYPE_1 1
+
+void xSurfaceInit(uint16 num_surfs);
+void xSurfaceExit();
+void xSurfaceSave(xSurface* ent, xSerial* s);
+void xSurfaceLoad(xSurface* ent, xSerial* s);
+void xSurfaceReset();
+uint16 xSurfaceGetNumSurfaces();
+xSurface* xSurfaceGetByIdx(uint16 n);
 
 #endif
