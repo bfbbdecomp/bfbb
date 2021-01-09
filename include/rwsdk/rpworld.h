9877b7a3118eef5dc47b7b1023f38560f93984f3
Seil Weiss <32021834+pslehisl@users.noreply.github.com>
2021-01-09 02:23:45 -0600

    zEnv.cpp 100% complete (#103)

--- include/rwsdk/rpworld.h
+++ include/rwsdk/rpworld.h
@@ -287,34 +287,18 @@ struct RpAtomic
 
 typedef RpAtomic* (*RpAtomicCallBack)(RpAtomic* atomic, void* data);
 
-enum RpWorldRenderOrder
-{
-    rpWORLDRENDERNARENDERORDER = 0,
-    rpWORLDRENDERFRONT2BACK,
-    rpWORLDRENDERBACK2FRONT,
-    rpWORLDRENDERORDERFORCEENUMSIZEINT = RWFORCEENUMSIZEINT
-};
-
-typedef struct RpWorldSector;
-typedef RpWorldSector* (*RpWorldSectorCallBack)(RpWorldSector* worldSector, void* data);
-
-struct RpSector
-{
-    RwInt32 type;
-};
-
-struct RpPolygon
-{
-    RwUInt16 matIndex;
-    RwUInt16 vertIndex[3];
-};
-
 struct RpVertexNormal
 {
     RwInt8 x;
     RwInt8 y;
     RwInt8 z;
     RwUInt8 pad;
+};
+
+struct RpPolygon
+{
+    RwUInt16 matIndex;
+    RwUInt16 vertIndex[3];
 };
 
 struct RpWorldSector
@@ -337,6 +321,84 @@ struct RpWorldSector
     RwUInt16 numVertices;
     RwUInt16 numPolygons;
     RwUInt16 pad;
+};
+
+typedef RpWorldSector* (*RpWorldSectorCallBack)(RpWorldSector* worldSector, void* data);
+
+struct RpSector
+{
+    RwInt32 type;
+};
+
+#define rpWORLD 7
+
+enum RpWorldPrivateFlag
+{
+    rpWORLDSINGLEMALLOC = 0x01,
+    rpWORLDPRIVATEFLAGFORCEENUMSIZEINT = RWFORCEENUMSIZEINT
+};
+
+enum RpWorldFlag
+{
+    rpWORLDTRISTRIP = 0x01,
+    rpWORLDPOSITIONS = 0x02,
+    rpWORLDTEXTURED = 0x04,
+    rpWORLDPRELIT = 0x08,
+    rpWORLDNORMALS = 0x10,
+    rpWORLDLIGHT = 0x20,
+    rpWORLDMODULATEMATERIALCOLOR = 0x40,
+    rpWORLDTEXTURED2 = 0x80,
+    rpWORLDNATIVE = 0x01000000,
+    rpWORLDNATIVEINSTANCE = 0x02000000,
+    rpWORLDFLAGSMASK = 0x000000FF,
+    rpWORLDNATIVEFLAGSMASK = 0x0F000000,
+    rpWORLDSECTORSOVERLAP = 0x40000000,
+    rpWORLDFLAGFORCEENUMSIZEINT = RWFORCEENUMSIZEINT
+};
+
+#define RpWorldGetBBoxMacro(_world) (&((_world)->boundingBox))
+
+#define _rpWorldGetOriginMacro(_world) (&((_world)->worldOrigin))
+
+#define RpWorldGetNumMaterialsMacro(_world) ((_world)->matList.numMaterials)
+
+#define RpWorldGetMaterialMacro(_world, _num) (((_world)->matList.materials)[(_num)])
+
+#define RpWorldGetNumClumpsMacro(_world) ((_world)->numClumpsInWorld)
+
+#define RpWorldSetRenderOrderMacro(_world, _renderOrder)                                           \
+    (((_world)->renderOrder = _renderOrder), (_world))
+
+#define RpWorldGetRenderOrderMacro(_world) ((_world)->renderOrder)
+
+#define RpWorldSetFlagsMacro(_world, _flags) (((_world)->flags = (_flags)), (_world))
+
+#define RpWorldGetFlagsMacro(_world) ((_world)->flags)
+
+#define RpWorldGetBBox(_world) RpWorldGetBBoxMacro(_world)
+
+#define _rpWorldGetOrigin(_world) _rpWorldGetOriginMacro(_world)
+
+#define RpWorldGetNumMaterials(_world) RpWorldGetNumMaterialsMacro(_world)
+
+#define RpWorldGetMaterial(_world, _num) RpWorldGetMaterialMacro(_world, _num)
+
+#define RpWorldGetNumClumps(_world) RpWorldGetNumClumpsMacro(_world)
+
+#define RpWorldSetRenderOrder(_world, _renderOrder) RpWorldSetRenderOrderMacro(_world, _renderOrder)
+
+#define RpWorldGetRenderOrder(_world) RpWorldGetRenderOrderMacro(_world)
+
+#define RpWorldSetFlags(_world, _flags) RpWorldSetFlagsMacro(_world, _flags)
+
+#define RpWorldGetFlags(_world) RpWorldGetFlagsMacro(_world)
+
+enum RpWorldRenderOrder
+{
+    rpWORLDRENDERNARENDERORDER = 0,
+    rpWORLDRENDERFRONT2BACK,
+    rpWORLDRENDERBACK2FRONT,
+    rpWORLDRENDERORDERFORCEENUMSIZEINT = RWFORCEENUMSIZEINT
 };
 
 typedef RpWorldSector* (*RpWorldSectorCallBackRender)(RpWorldSector* worldSector);
