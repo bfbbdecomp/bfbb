3f509a3c9963ace07ed2d60caa6494827425187a
Seil Weiss <32021834+pslehisl@users.noreply.github.com>
2021-01-10 17:43:04 -0600

    Move all Dolphin defs into one include file (#109)

--- include/dolphin/dolphin.h
+++ include/dolphin/dolphin.h
@@ -1,23 +1,147 @@
 #ifndef __DOLPHIN_H__
 #define __DOLPHIN_H__
 
-#include "dolphin/types.h"
-#include "dolphin/gx.h"
-#include "dolphin/os.h"
-#include "dolphin/dvd.h"
+typedef signed char s8;
+typedef signed short s16;
+typedef signed long s32;
+typedef signed long long s64;
+typedef unsigned char u8;
+typedef unsigned short u16;
+typedef unsigned long u32;
+typedef unsigned long long u64;
 
+typedef volatile u8 vu8;
+typedef volatile u16 vu16;
+typedef volatile u32 vu32;
+typedef volatile u64 vu64;
+typedef volatile s8 vs8;
+typedef volatile s16 vs16;
+typedef volatile s32 vs32;
+typedef volatile s64 vs64;
+
+typedef float f32;
+typedef double f64;
+typedef volatile f32 vf32;
+typedef volatile f64 vf64;
+
+typedef char* Ptr;
+
+#ifndef BOOL
+typedef int BOOL;
+#endif
+
+#ifndef TRUE
+#define TRUE 1
+#endif
+
+#ifndef FALSE
+#define FALSE 0
+#endif
+
+#ifndef NULL
+#define NULL 0
+#endif
+
+#ifdef __cplusplus
 extern "C" {
-typedef int32 OSHeapHandle;
-void OSPanic(int8* param_1, uint32 param_2, int8* param_3);
-void* OSAllocFromHeap(void* handle_t, uint32 size_t);
-void OSFreeToHeap(void* handle_t, void* param_2);
+#endif
+
+typedef u8 GXBool;
+
+#define GX_TRUE ((GXBool)1)
+#define GX_FALSE ((GXBool)0)
+#define GX_ENABLE ((GXBool)1)
+#define GX_DISABLE ((GXBool)0)
+
+typedef struct DVDDiskID DVDDiskID;
+
+struct DVDDiskID
+{
+    char gameName[4];
+    char company[2];
+    u8 diskNumber;
+    u8 gameVersion;
+    u8 streaming;
+    u8 streamingBufSize;
+    u8 padding[22];
+};
+
+typedef struct DVDCommandBlock DVDCommandBlock;
+
+typedef void (*DVDCBCallback)(s32 result, DVDCommandBlock* block);
+
+struct DVDCommandBlock
+{
+    DVDCommandBlock* next;
+    DVDCommandBlock* prev;
+    u32 command;
+    s32 state;
+    u32 offset;
+    u32 length;
+    void* addr;
+    u32 currTransferSize;
+    u32 transferredSize;
+    DVDDiskID* id;
+    DVDCBCallback callback;
+    void* userData;
+};
+
+typedef struct DVDFileInfo DVDFileInfo;
+
+typedef void (*DVDCallback)(s32 result, DVDFileInfo* fileInfo);
+
+struct DVDFileInfo
+{
+    DVDCommandBlock cb;
+    u32 startAddr;
+    u32 length;
+    DVDCallback callback;
+};
+
+typedef int OSHeapHandle;
+
+#define OSRoundUp32B(x) (((u32)(x) + 32 - 1) & ~(32 - 1))
+#define OSRoundDown32B(x) (((u32)(x)) & ~(32 - 1))
+
+typedef s64 OSTime;
+typedef u32 OSTick;
+
+typedef struct OSCalendarTime
+{
+    int sec; // seconds after the minute [0, 61]
+    int min; // minutes after the hour [0, 59]
+    int hour; // hours since midnight [0, 23]
+    int mday; // day of the month [1, 31]
+    int mon; // month since January [0, 11]
+    int year; // years since 0000
+    int wday; // days since Sunday [0, 6]
+    int yday; // days since January 1 [0, 365]
+    int msec; // milliseconds after the second [0,999]
+    int usec; // microseconds after the millisecond [0,999]
+} OSCalendarTime;
+
+extern volatile OSHeapHandle __OSCurrHeap;
+
+#define OSAlloc(size) OSAllocFromHeap(__OSCurrHeap, (size))
+#define OSFree(ptr) OSFreeToHeap(__OSCurrHeap, (ptr))
+
+void GXSetColorUpdate(GXBool update_enable);
+void GXSetAlphaUpdate(GXBool update_enable);
+void OSPanic(const char* file, int line, const char* msg, ...);
+void* OSAllocFromHeap(OSHeapHandle heap, u32 size);
+void OSFreeToHeap(OSHeapHandle heap, void* ptr);
 OSHeapHandle OSCreateHeap(void* start, void* end);
 OSHeapHandle OSSetCurrentHeap(OSHeapHandle heap);
-void* OSInitAlloc(void* arenaStart, void* arenaEnd, int32 maxHeaps);
+void* OSInitAlloc(void* arenaStart, void* arenaEnd, int maxHeaps);
 void* OSGetArenaHi();
 void* OSGetArenaLo();
-void PADInit();
-void PADControlMotor(int32 chan, uint32 command);
+OSTime OSGetTime();
+void OSTicksToCalendarTime(OSTime ticks, OSCalendarTime* td);
+BOOL PADInit();
+void PADControlMotor(int chan, u32 command);
+
+#ifdef __cplusplus
 }
+#endif
 
 #endif
