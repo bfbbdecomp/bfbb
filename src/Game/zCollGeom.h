f5ad80c78e4cf08efead89dd9ceb2e8619bf740b
Seil Weiss <32021834+pslehisl@users.noreply.github.com>
2021-01-10 22:34:10 -0600

    zCollGeom.cpp 50% complete

--- src/Game/zCollGeom.h
+++ src/Game/zCollGeom.h
@@ -3,6 +3,19 @@
 
 #include "../Core/x/xEnt.h"
 
+#include <rwcore.h>
+#include <rpworld.h>
+
+struct zCollGeomTable
+{
+    RpAtomic* baseModel;
+    RpAtomic* colModel[1];
+    RpAtomic* camcolModel;
+};
+
 uint32 zCollGeom_EntSetup(xEnt* ent);
+void zCollGeom_Init();
+void zCollGeom_CamEnable(xEnt* ent);
+void zCollGeom_CamDisable(xEnt* ent);
 
 #endif
