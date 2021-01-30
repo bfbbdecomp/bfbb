c135427288d3721343074b5cc4b93d6d638fa6fd
earthcrafterman <banddstudios@gmail.com>
2021-01-29 21:26:49 -0500

    more zNPCCommon work

--- src/Core/p2/iModel.h
+++ src/Core/p2/iModel.h
@@ -2,6 +2,7 @@
 #define IMODEL_H
 
 #include "../x/xMath3.h"
+#include "../x/xModel.h"
 
 #include <rwcore.h>
 #include <rpworld.h>
@@ -11,6 +12,7 @@ RpAtomic* iModelFile_RWMultiAtomic(RpAtomic* model);
 void iModelSetMaterialTexture(RpAtomic* model, void* texture);
 void iModelResetMaterial(RpAtomic* model);
 int32 iModelCullPlusShadow(RpAtomic* model, RwMatrix* mat, xVec3* shadowVec, int32* shadowOutside);
+void iModelTagEval(RpAtomic* model, const xModelTag* tag, RwMatrixTag* mat, xVec3* dest);
 RpAtomic* iModelFileNew(void* buffer, uint32 size);
 void iModelRender(RpAtomic* model, RwMatrix* mat);
 
