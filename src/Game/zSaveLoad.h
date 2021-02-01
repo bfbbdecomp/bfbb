c78f8fb430c32e38a5715aa5015e6a0a13f3d162
Steven Casper <sebastiancasper3@gmail.com>
2021-01-31 23:24:04 -0500

    zSaveLoad.cpp progress (#127)
    * zSaveLoad.cpp progress
    
    * remove callbacks and format
    
    * extern zSaveLoad_strings

--- src/Game/zSaveLoad.h
+++ src/Game/zSaveLoad.h
@@ -12,7 +12,23 @@ struct zSaveLoadGame
     int8 thumbIconIndex;
 };
 
+struct zSaveLoadUI
+{
+    uint32 entry;
+    uint32 nameID;
+    int8* name;
+};
+
+extern zSaveLoadUI zSaveLoadUITable[62];
 extern zSaveLoadGame zSaveLoadGameTable[3];
+
+void zSaveLoad_Tick();
+void zSaveLoadInit();
+void zSaveLoadGameTableInit(zSaveLoadGame* saveTable);
+void zSaveLoadUITableInit(zSaveLoadUI* saveTable);
+void zSaveLoad_UIEvent(int32 p1, uint32 p2);
+int32 zSaveLoadCardCount();
+int32 zSaveLoad_CardPromptGameSlotEmpty();
 
 uint32 zSaveLoad_LoadLoop();
 uint32 zSaveLoad_SaveLoop();
@@ -24,5 +40,14 @@ int32 zSaveLoad_getgame();
 int32 zSaveLoad_getMCneeded();
 int32 zSaveLoad_getMCavailable();
 uint32 zSaveLoad_slotIsEmpty(uint32 i);
+
+int32 zSaveLoad_CardCheckSlotEmpty_hasGame(int32 num, int32 game);
+int32 zSaveLoad_CardCheckValid(int32 cardNumber, int32 mode);
+int32 zSaveLoad_CardCheckSpace(int32 cardNumber, int32 mode);
+int32 zSaveLoad_CardCheckGames(int32 cardNumber, int32 mode);
+
+int32 zSaveLoad_CardCheckGameSlot(int32 a, int32 b, int32 c);
+int32 zSaveLoad_CardCheckSlotEmpty(int32 a, int32 b);
+int32 zSaveLoad_CardCheckSlotOverwrite(int32 a, int32 b);
 
 #endif
