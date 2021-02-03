bb8591b90f68de09759dd488ba7678ea5f1a71c1
Steven Casper <sebastiancasper3@gmail.com>
2021-02-02 19:17:25 -0500

    iSnd.cpp progress

--- src/Core/p2/iSnd.cpp
+++ src/Core/p2/iSnd.cpp
@@ -2,11 +2,31 @@
 
 #include <types.h>
 
+#include "dolphin/dolphin.h"
+#include "../src/Core/p2/iMix.h"
+#include "../src/Core/x/xCutscene.h"
+#include "../src/Core/x/xSnd.h"
+#include "../src/Core/x/xMath.h"
+
+extern vinfo voices[58];
+extern int8 soundInited;
+extern int32 SoundFlags;
+extern float32 _1262;
+extern float32 _1263;
+extern volatile int32 fc;
+
 // func_800CF92C
 #pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "dv_callback__FPv")
 
 // func_800CFA40
-#pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "arq_callback__FUl")
+void arq_callback(long32)
+{
+    if (!soundInited)
+    {
+        return;
+    }
+    SoundFlags = 0;
+}
 
 // func_800CFA58
 #pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "dump_flags__FUi")
@@ -24,13 +44,23 @@
 #pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iSndInit__Fv")
 
 // func_800D07D8
-#pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iSndExit__Fv")
+void iSndExit()
+{
+    soundInited = 0;
+    AXQuit();
+}
 
 // func_800D0800
-#pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iSndSetEnvironmentalEffect__F13isound_effect")
+//not sure where this type is from.
+void iSndSetEnvironmentalEffect(isound_effect)
+{
+    return;
+}
 
 // func_800D0804
-#pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iSndInitSceneLoaded__Fv")
+void iSndInitSceneLoaded()
+{
+}
 
 // func_800D0808
 #pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iSndIsPlaying__FUi")
@@ -50,8 +80,26 @@
 // func_800D0EFC
 #pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iSndStop__FUi")
 
+#ifndef NON_MATCHING
 // func_800D1060
 #pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iVolFromX__Ff")
+#else
+uint32 iVolFromX(float32 param1)
+{
+    float f = MAX(param1, _1263);
+
+    int32 i = _1262 * xlog(f);
+    i = (i & i >> 0x1f);
+    if (i < -0x388)
+    {
+        return -0x388;
+    }
+    else
+    {
+        return (i & i >> 0x1f);
+    }
+}
+#endif
 
 // func_800D10C8
 #pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iSndCalcVol__FP13xSndVoiceInfoP5vinfo")
@@ -60,13 +108,37 @@
 #pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iSndCalcVol3d__FP13xSndVoiceInfoP5vinfo")
 
 // func_800D12D8
-#pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iSndVolUpdate__FP13xSndVoiceInfoP5vinfo")
+void iSndVolUpdate(xSndVoiceInfo* info, vinfo* vinfo)
+{
+    MIXUnMute(vinfo->voice);
+    xSndInternalUpdateVoicePos(info);
+    if ((info->flags & 8) != 0)
+    {
+        iSndCalcVol3d(info, vinfo);
+    }
+    else
+    {
+        iSndCalcVol(info, vinfo);
+    }
+}
 
 // func_800D1344
 #pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iSndUpdateStreams__Fv")
 
 // func_800D14E8
-#pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iSndUpdateSounds__Fv")
+void iSndUpdateSounds()
+{
+    if (soundInited)
+    {
+        for (int i = 0; i < 58; i++)
+        {
+            if (voices[i].voice != NULL)
+            {
+                iSndVolUpdate(&gSnd.voice[i + 6], &voices[i]);
+            }
+        }
+    }
+}
 
 // func_800D1570
 #pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iSndUpdate__Fv")
@@ -96,28 +168,80 @@
 #pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iSndSetPitch__FUif")
 
 // func_800D2560
-#pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iSndStartStereo__FUiUif")
+void iSndStartStereo()
+{
+}
 
 // func_800D2564
-#pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iSndStereo__FUi")
+void iSndStereo(uint32 i)
+{
+    if (i == 0)
+    {
+        OSSetSoundMode(0);
+        gSnd.stereo = 0;
+    }
+    else
+    {
+        OSSetSoundMode(1);
+        gSnd.stereo = 1;
+    }
+}
 
+#ifndef NON_MATCHING
 // func_800D25B4
 #pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iSndWaitForDeadSounds__Fv")
+#else
+void iSndWaitForDeadSounds()
+{
+    fc = 0;
+    int i = 0;
+    // Can't get 0x8c to get stored in r31
+    while ((i = fc) < 0x8c) //for (int i = 0; (i = fc) < 0x8c; )
+    {
+        i = fc;
+        while (fc < i + 0xe)
+            ;
+        iSndUpdate();
+    }
+}
+#endif
 
 // func_800D260C
-#pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iSndSuspendCD__FUi")
+// #pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iSndSuspendCD__FUi")
+void iSndSuspendCD(uint32)
+{
+}
 
 // func_800D2610
 #pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iSndSceneExit__Fv")
 
 // func_800D27E4
-#pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iSndMessWithEA__FP9sDSPADPCM")
+void iSndMessWithEA(sDSPADPCM* param1) //sDSPADPCM*
+{
+    if (param1 != NULL)
+    {
+        param1->buffer[5] = SampleToNybbleAddress(param1->buffer[0] - 1);
+    }
+}
 
+#if 1
 // func_800D2820
 #pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "SampleToNybbleAddress__FUi")
+#else
+uint32 SampleToNybbleAddress(uint32 sample)
+{
+    uint32 i = sample * 0x124924925U >> 0x20;
+    i = i & 0xfffffff0;
+    i = i + sample % 0xe + 2;
+    return i;
+}
+#endif
 
 // func_800D2854
-#pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "sndloadcb__FP9tag_xFile")
+void sndloadcb(tag_xFile* tag)
+{
+    SoundFlags = 0;
+}
 
 // func_800D2860
 #pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iSndLoadSounds__FPv")
@@ -126,10 +250,19 @@
 #pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iSndDIEDIEDIE__Fv")
 
 // func_800D2EB8
-#pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iSndSetExternalCallback__FPFUi_v")
+void iSndSetExternalCallback(uint32 (*func_ptr)())
+{
+}
 
 // func_800D2EBC
-#pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iSndMyAXFree__FPP6_AXVPB")
+void iSndMyAXFree(_AXVPB** param1)
+{
+    if (param1 != NULL && *param1 != NULL)
+    {
+        AXFreeVoice(*param1);
+        *param1 = NULL;
+    }
+}
 
 // func_800D2F00
 #pragma GLOBAL_ASM("asm/Core/p2/iSnd.s", "iSndSuspend__Fv")
