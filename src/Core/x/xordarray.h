4c7a8478ea7720018a0e6db73dfae256da98e203
Seil Weiss <32021834+pslehisl@users.noreply.github.com>
2021-02-06 14:55:34 -0600

    xFactory 100% complete (#151)

--- src/Core/x/xordarray.h
+++ src/Core/x/xordarray.h
@@ -11,13 +11,16 @@ struct st_XORDEREDARRAY
     int32 warnlvl;
 };
 
+typedef int32 (*XOrdCompareCallback)(void* vkey, void* vitem);
+typedef int32 (*XOrdTestCallback)(const void* vkey, void* vitem);
+
 void XOrdInit(st_XORDEREDARRAY* array, int32 size, int32 tempAlloc);
 void XOrdReset(st_XORDEREDARRAY* array);
 void XOrdDone(st_XORDEREDARRAY* array, int32 wasTempAlloc);
 void XOrdAppend(st_XORDEREDARRAY* array, void* elt);
-void XOrdInsert(st_XORDEREDARRAY* array, void* elt, int32 (*compare)(void*, void*));
+void XOrdInsert(st_XORDEREDARRAY* array, void* elt, XOrdCompareCallback compare);
 void* XOrdRemove(st_XORDEREDARRAY* array, void* elt, int32 index);
-int32 XOrdLookup(st_XORDEREDARRAY* array, void* key, int32 (*test)(void*, void*));
+int32 XOrdLookup(st_XORDEREDARRAY* array, const void* key, XOrdTestCallback test);
 void XOrdSort(st_XORDEREDARRAY* array, int32 (*test)(void*, void*));
 
 #endif
