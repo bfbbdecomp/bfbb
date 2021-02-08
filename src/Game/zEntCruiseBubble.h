7d806f0bb810fa7b25ec21dee87b3b0d6ad3512f
Adrian Richter <adrichter97@gmail.com>
2021-02-08 18:59:12 +0100

    add offset hints

--- src/Game/zEntCruiseBubble.h
+++ src/Game/zEntCruiseBubble.h
@@ -10,16 +10,26 @@ namespace cruise_bubble
 
 struct sound_config
 {
+    // offset 0x0
     int8* name; // probably char*
+    // offset 0x4
     float32 volume;
+    // offset 0x8
     float32 radius_inner;
+    // offset 0xc
     float32 radius_outer;
+    // offset 0x10
     uint8 streamed; // might be bool, not sure
     uint8 looping; // might be bool, not sure
+    // offset 0x14
     _tagSDRumbleType rumble;
+    // offset 0x18
     int32 first;
+    // offset 0x1c
     int32 last;
+    // offset 0x20
     uint32 id;
+    // offset 0x24
     uint32 handle;
 };
 
