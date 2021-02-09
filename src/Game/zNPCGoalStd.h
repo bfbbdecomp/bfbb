e33ba7f431eee29cee88aa63202a1c696eccfda2
Seil Weiss <32021834+pslehisl@users.noreply.github.com>
2021-02-08 19:36:45 -0600

    replace NPC_GOAL_ID macros with en_NPCGOALS (#164)

--- src/Game/zNPCGoalStd.h
+++ src/Game/zNPCGoalStd.h
@@ -3,18 +3,6 @@
 
 #include "zNPCGoalCommon.h"
 
-#define NPC_GOAL_ID_IDLE NPC_GOAL_ID('N', 0)
-#define NPC_GOAL_ID_WANDER NPC_GOAL_ID('N', 1)
-#define NPC_GOAL_ID_PATROL NPC_GOAL_ID('N', 2)
-#define NPC_GOAL_ID_FIDGET NPC_GOAL_ID('N', 3)
-#define NPC_GOAL_ID_WAITING NPC_GOAL_ID('N', 4)
-#define NPC_GOAL_ID_DEAD NPC_GOAL_ID('N', 5)
-#define NPC_GOAL_ID_NOMANLAND NPC_GOAL_ID('N', 6)
-#define NPC_GOAL_ID_LIMBO NPC_GOAL_ID('N', 7)
-#define NPC_GOAL_ID_DEVANIMCYCLE NPC_GOAL_ID('X', 0)
-#define NPC_GOAL_ID_DEVANIMSPIN NPC_GOAL_ID('X', 1)
-#define NPC_GOAL_ID_DEVANIMHERO NPC_GOAL_ID('X', 2)
-
 struct zNPCGoalDead : zNPCGoalCommon
 {
     int32 flg_deadinfo;
--- src/Game/zNPCGoalVillager.h
+++ src/Game/zNPCGoalVillager.h
@@ -3,18 +3,6 @@
 
 #include "zNPCGoalCommon.h"
 
-#define NPC_GOAL_ID_CHEER NPC_GOAL_ID('S', 8)
-#define NPC_GOAL_ID_HURT NPC_GOAL_ID('S', 9)
-#define NPC_GOAL_ID_PLAYERNEAR NPC_GOAL_ID('S', 10)
-#define NPC_GOAL_ID_TALK NPC_GOAL_ID('S', 11)
-#define NPC_GOAL_ID_CHATTER NPC_GOAL_ID('S', 12)
-#define NPC_GOAL_ID_SPEAK NPC_GOAL_ID('S', 13)
-#define NPC_GOAL_ID_BALLOON NPC_GOAL_ID('S', 14)
-#define NPC_GOAL_ID_BOYRIDE NPC_GOAL_ID('S', 15)
-#define NPC_GOAL_ID_BOYFALL NPC_GOAL_ID('S', 16)
-#define NPC_GOAL_ID_BOYWEEP NPC_GOAL_ID('S', 17)
-#define NPC_GOAL_ID_BOYSWIM NPC_GOAL_ID('S', 18)
-
 class zNPCGoalPlayerNear
 {
 public:
