4dd6d1ea57d437cda9bdf2a38fbab637a80cf593
Matt <5638426+mattbruv@users.noreply.github.com>
2021-02-12 22:02:18 -0500

    document sandy cheeks boss

--- src/Game/zNPCTypeBossSandy.h
+++ src/Game/zNPCTypeBossSandy.h
@@ -98,6 +98,66 @@ struct zNPCBSandy : zNPCBoss
 
     zNPCBSandy(int32 myType);
 
+    /*
+    Sandy Full V-Table data:
+
+    0 0x0 00000000 NULL
+    1 0x4 00000000 NULL
+    2 0x8 8013dca0 zNPCBSandy::Init(xEntAsset *)
+    3 0xc 800f4738 xNPCBasic::PostInit(void)
+    4 0x10 8013e418 zNPCBSandy::Setup(void)
+    5 0x14 800eed68 xNPCBasic::PostSetup(void)
+    6 0x18 8013e830 zNPCBSandy::Reset(void)
+    7 0x1c 8014075c zNPCBSandy::Process(xScene *, float)
+    8 0x20 800f0164 zNPCCommon::BUpdate(xVec3 *)
+    9 0x24 801419d0 zNPCBSandy::NewTime(xScene *, float)
+    10 0x28 800f0004 zNPCCommon::Move(xScene *, float, xEntFrame *)
+    11 0x2c 800f03d0 zNPCCommon::SysEvent(xBase *, xBase *, unsigned int, float const *, xBase *, int *)
+    12 0x30 8013f9bc zNPCBSandy::Render(void)
+    13 0x34 800b51f4 xNPCBasic::Save( const(xSerial *))
+    14 0x38 800b51f8 xNPCBasic::Load(xSerial *)
+    15 0x3c 800f1770 zNPCCommon::CollideReview(void)
+    16 0x40 80136554 zNPCBoss::ColChkFlags( const(void))
+    17 0x44 8013655c zNPCBoss::ColPenFlags( const(void))
+    18 0x48 80136564 zNPCBoss::ColChkByFlags( const(void))
+    19 0x4c 8013656c zNPCBoss::ColPenByFlags( const(void))
+    20 0x50 80136574 zNPCBoss::PhysicsFlags( const(void))
+    21 0x54 800efa10 zNPCCommon::Destroy(void)
+    22 0x58 800efd30 zNPCCommon::NPCMessage(NPCMsg *)
+    23 0x5c 800eed6c zNPCCommon::RenderExtra(void)
+    24 0x60 800eed70 zNPCCommon::RenderExtraPostParticles(void)
+    25 0x64 8013f1ac zNPCBSandy::ParseINI(void)
+    26 0x68 800f1394 zNPCCommon::ParseLinks(void)
+    27 0x6c 800f1718 zNPCCommon::ParseProps(void)
+    28 0x70 8013e5ec zNPCBSandy::SelfSetup(void)
+    29 0x74 800f19ac zNPCCommon::SelfDestroy(void)
+    30 0x78 80060028 zNPCCommon::IsHealthy(void)
+    31 0x7c 800ee2c0 zNPCCommon::IsAlive(void)
+    32 0x80 8014184c zNPCBSandy::Damage(en_NPC_DAMAGE_TYPE, xBase *, xVec3 const *)
+    33 0x84 800efc34 zNPCCommon::Respawn(xVec3 const *, zMovePoint *, zMovePoint *)
+    34 0x88 800f46b0 zNPCCommon::DuploOwner(zNPCCommon *)
+    35 0x8c 800ec87c zNPCCommon::DuploNotice(en_SM_NOTICES, void *)
+    36 0x90 80082bd8 zNPCCommon::CanRope(void)
+    37 0x94 800f3f24 zNPCCommon::LassoNotify(en_LASSO_EVENT)
+    38 0x98 80082bd0 zNPCCommon::SetCarryState(en_NPC_CARRY_STATE)
+    39 0x9c 800ee2c8 zNPCCommon::Stun(float)
+    40 0xa0 800f46b8 zNPCCommon::SpeakBegin(void)
+    41 0xa4 800f46bc zNPCCommon::SpeakEnd(void)
+    42 0xa8 800f46c0 zNPCCommon::SpeakStart(unsigned int, unsigned int, int)
+    43 0xac 800f46c4 zNPCCommon::SpeakStop(void)
+    44 0xb0 8013f1f8 zNPCBSandy::AnimPick(int, en_NPC_GOAL_SPOT, xGoal *)
+    45 0xb4 800f2448 zNPCCommon::GetParm(en_npcparm, void *)
+    46 0xb8 800f25d0 zNPCCommon::GetParmDefault(en_npcparm, void *)
+    47 0xbc 800f46c8 zNPCCommon::GenShadCacheRad(void)
+    48 0xc0 800f4664 zNPCCommon::PRIV_GetDriverData(void)
+    49 0xc4 800f46a8 zNPCCommon::PRIV_GetLassoData(void)
+    50 0xc8 800f3b68 zNPCCommon::LassoSetup(void)
+    51 0xcc 8013657c zNPCBoss::AttackTimeLeft(void)
+    52 0xd0 80136584 zNPCBoss::HoldUpDude(void)
+    53 0xd4 80136588 zNPCBoss::ThanksImDone(void)
+    54 0xd8 00000000 NULL
+    */
+
     virtual void Init(xEntAsset* asset); // 8013dca0 entry=2
     virtual void Setup(); // 8013e418 entry=4
     virtual void Reset(); // 8013e830 entry=6
