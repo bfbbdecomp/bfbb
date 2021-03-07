9a90298a8f90cb6c3e65adebd4e90019c962c2b9
Steven Casper <sebastiancasper3@gmail.com>
2021-03-07 07:09:04 -0500

    xstransvc.cpp progress

--- src/Core/x/xpkrsvc.h
+++ src/Core/x/xpkrsvc.h
@@ -27,4 +27,51 @@ struct st_PKR_ASSET_TOCINFO
     void* mempos;
 };
 
+enum en_LAYER_TYPE
+{
+    PKR_LTYPE_DEFAULT,
+    PKR_LTYPE_TEXTURE,
+    PKR_LTYPE_BSP,
+    PKR_LTYPE_MODEL,
+    PKR_LTYPE_ANIMATION,
+    PKR_LTYPE_VRAM,
+    PKR_LTYPE_SRAM,
+    PKR_LTYPE_SNDTOC,
+    PKR_LTYPE_CUTSCENE,
+    PKR_LTYPE_CUTSCENETOC,
+    PKR_LTYPE_JSPINFO,
+    PKR_LTYPE_NOMORE,
+    PKR_LTYPE_ALL = 0xffffffff
+};
+
+struct st_PACKER_READ_DATA
+{
+};
+
+struct st_PACKER_READ_FUNCS
+{
+    uint32 api_ver;
+    st_PACKER_READ_DATA* (*Init)(void*, const int8*, uint32, int32*, st_PACKER_ASSETTYPE*);
+    void (*Done)(st_PACKER_READ_DATA*);
+    int32 (*LoadLayer)(st_PACKER_READ_DATA*, en_LAYER_TYPE);
+    uint32 (*GetAssetSize)(st_PACKER_READ_DATA*, uint32);
+    void* (*LoadAsset)(st_PACKER_READ_DATA*, uint32, int8*, void*);
+    void* (*AssetByType)(st_PACKER_READ_DATA*, uint32, int32, uint32*);
+    int32 (*AssetCount)(st_PACKER_READ_DATA*, uint32);
+    int32 (*IsAssetReady)(st_PACKER_READ_DATA*, uint32);
+    int32 (*SetActive)(st_PACKER_READ_DATA*, en_LAYER_TYPE);
+    int8* (*AssetName)(st_PACKER_READ_DATA*, uint32);
+    uint32 (*GetBaseSector)(st_PACKER_READ_DATA*);
+    int32 (*GetAssetInfo)(st_PACKER_READ_DATA*, uint32, st_PKR_ASSET_TOCINFO*);
+    int32 (*GetAssetInfoByType)(st_PACKER_READ_DATA*, uint32, int32, st_PKR_ASSET_TOCINFO*);
+    int32 (*PkgHasAsset)(st_PACKER_READ_DATA*, uint32);
+    uint32 (*PkgTimeStamp)(st_PACKER_READ_DATA*);
+    void (*PkgDisconnect)(st_PACKER_READ_DATA*);
+};
+
+st_PACKER_READ_FUNCS* PKRGetReadFuncs(int32 apiver);
+int32 PKRStartup();
+int32 PKRLoadStep(int32 i);
+uint32 PKRAssetIDFromInst(void* asset_inst);
+
 #endif
