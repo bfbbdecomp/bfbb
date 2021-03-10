3cd480dfb209240b5f7107170d75770555269a32
Joey Ballentine <joeyjballentine@gmail.com>
2021-03-10 02:36:17 -0600

    zLOD

--- src/Game/zLOD.cpp
+++ src/Game/zLOD.cpp
@@ -3,6 +3,7 @@
 #include <types.h>
 #include <../src/Core/x/xEnt.h>
 #include <../src/Core/x/xBound.h>
+#include "zBase.h"
 #include "zEntDestructObj.h"
 
 extern uint32 sTableCount;
@@ -16,16 +17,17 @@ extern float32 lbl_803CDC48;
 extern float32 lbl_803CDC4C;
 
 // func_800A1D18
-#if 1
+#if 0
 #pragma GLOBAL_ASM("asm/Game/zLOD.s", "AddToLODList__FP14xModelInstance")
 #else
+// This was almost completely matching until I switched sTableList to sTableList[i]
 void AddToLODList(xModelInstance* model)
 {
+    // This was necessary for it to match back before I made the switch mentioned above
     for (int i = 0; i < sManagerCount; i++)
     {
         if (sManagerList[i].model == model)
         {
-            // THIS MATCHES???? WTF
             break;
         }
     }
@@ -58,11 +60,6 @@ void AddToLODList(xModelInstance* model)
                     minst->FadeStart = minst->FadeEnd - lbl_803CDC48;
                     minst = minst->Next;
                 }
-                // Missing: Loads sTableList into r0, loads sManagerCount into r3,
-                // adds r3 into r0
-                // stores r0 into sManagerCount
-
-                // Adds: Storing sTableList into r4
 
                 sManagerList[sManagerCount].numextra = numextra;
                 sManagerList[sManagerCount].lod = &sTableList[i];
@@ -79,9 +76,6 @@ void AddToLODList(xModelInstance* model)
 #endif
 
 // func_800A1EEC
-#if 0
-#pragma GLOBAL_ASM("asm/Game/zLOD.s", "AddToLODList__FP4xEntP6xScenePv")
-#else
 xEnt* AddToLODList(xEnt* ent, xScene* scene, void* v)
 {
     if (!ent->model)
@@ -90,21 +84,22 @@ xEnt* AddToLODList(xEnt* ent, xScene* scene, void* v)
     }
 
     AddToLODList(ent->model);
-    if (ent->baseType == '\033')
+
+    if (ent->baseType == eBaseTypeDestructObj)
     {
-        if (ent[1].camcollModel != NULL)
+        zEntDestructObj* destruct = (zEntDestructObj*)ent;
+        if (destruct->hit_model != NULL)
         {
-            AddToLODList(ent[1].camcollModel);
+            AddToLODList(destruct->hit_model);
         }
-        if (ent[1].lightKit != NULL)
+        if (destruct->destroy_model != NULL)
         {
-            AddToLODList((xModelInstance*)ent[1].lightKit);
+            AddToLODList(destruct->destroy_model);
         }
     }
 
     return ent;
 }
-#endif
 
 // func_800A1F5C
 #pragma GLOBAL_ASM("asm/Game/zLOD.s", "zLOD_Setup__Fv")
@@ -123,13 +118,16 @@ zLODTable* zLOD_Get(xEnt* ent)
         return 0;
     }
 
-    for (int i = 0; i < sTableCount; i++)
+    if (sTableCount != 0)
     {
-        if (sTableList[i].baseBucket != NULL)
+        for (int i = 0; i < sTableCount; i++)
         {
-            if ((*sTableList[i].baseBucket)->OriginalData == ent->model->Data)
+            if (sTableList[i].baseBucket != NULL)
             {
-                return &sTableList[i];
+                if ((*sTableList[i].baseBucket)->OriginalData == ent->model->Data)
+                {
+                    return &sTableList[i];
+                }
             }
         }
     }
