76441ad235a1ed94cc4c16e63815af601cc9f6d5
Seil Weiss <32021834+pslehisl@users.noreply.github.com>
2021-03-27 05:52:05 -0500

    Decomp most of zDiscoFloor

--- src/Core/p2/iModel.h
+++ src/Core/p2/iModel.h
@@ -8,11 +8,13 @@
 #include <rpworld.h>
 
 int32 iModelCull(RpAtomic* model, RwMatrixTag* mat);
+int32 iModelSphereCull(xSphere* sphere);
 RpAtomic* iModelFile_RWMultiAtomic(RpAtomic* model);
 void iModelSetMaterialTexture(RpAtomic* model, void* texture);
 void iModelResetMaterial(RpAtomic* model);
 int32 iModelCullPlusShadow(RpAtomic* model, RwMatrix* mat, xVec3* shadowVec, int32* shadowOutside);
 void iModelTagEval(RpAtomic* model, const xModelTag* tag, RwMatrixTag* mat, xVec3* dest);
+void iModelSetMaterialAlpha(RpAtomic* model, uint8 alpha);
 RpAtomic* iModelFileNew(void* buffer, uint32 size);
 void iModelRender(RpAtomic* model, RwMatrix* mat);
 
