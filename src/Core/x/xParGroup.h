e475ca09965d612305f52ba695ff2c942a47e2e0
Seil Weiss <32021834+pslehisl@users.noreply.github.com>
2021-03-29 13:21:22 -0500

    Decomp almost all of xParGroup (#223)

--- src/Core/x/xParGroup.h
+++ src/Core/x/xParGroup.h
@@ -2,7 +2,8 @@
 #define XPARGROUP_H
 
 #include "xPar.h"
-#include "xParCmd.h"
+
+struct xParCmdTex;
 
 struct xParGroup
 {
@@ -22,5 +23,26 @@ struct xParGroup
     void (*draw)(void*, xParGroup*);
     xParCmdTex* m_cmdTex;
 };
+
+#define XPARGROUP_UNK1 0x1
+#define XPARGROUP_ALLOCPARS 0x2
+#define XPARGROUP_NOAGING 0x4
+#define XPARGROUP_NOBACK2LIFE 0x8
+#define XPARGROUP_UNK10 0x10
+
+void xParGroupInit(xParGroup* ps);
+void xParGroupSetAging(xParGroup* ps, int32 age);
+void xParGroupSetBack2Life(xParGroup* ps, int32 b2l);
+void xParGroupSetVisibility(xParGroup* ps, int32 vis);
+void xParGroupSetPriority(xParGroup* ps, uint8 val);
+void xParGroupRegister(xParGroup* ps);
+void xParGroupUnregister(xParGroup* ps);
+void xParGroupSetActive(xParGroup* ps, uint32 isActive);
+void xParGroupKillAllParticles(xParGroup* ps);
+void xParGroupAnimate(xParGroup* ps, float32 dt);
+void xParGroupAddParP(xParGroup* ps, xPar* p);
+xPar* xParGroupAddPar(xParGroup* ps);
+void xParGroupKillPar(xParGroup* ps, xPar* p);
+void xParGroupAddParToDeadList(xParGroup* ps, xPar* p);
 
 #endif
