8c704238462c749f7065d54b79e558c6174930e0
Steven Casper <sebastiancasper3@gmail.com>
2024-06-22 00:13:14 -0400

    enable linker warnings

--- configure.py
+++ configure.py
@@ -151,7 +151,6 @@ config.asflags = [
 config.ldflags = [
     "-fp hardware",
     "-nodefaults",
-    "-warn off",
     # "-listclosure", # Uncomment for Wii linkers
 ]
 # Use for any additional files that should cause a re-configure when modified
