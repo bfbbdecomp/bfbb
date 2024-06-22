ade6b31f2ca810ce551402375fccb739d9126899
tgsm <doodrabbit@hotmail.com>
2024-06-22 08:05:12 -0500

    Finish matching iTime

--- src/SB/Core/gc/iTime.cpp
+++ src/SB/Core/gc/iTime.cpp
@@ -1,11 +1,11 @@
 #include "iTime.h"
+#include "iSystem.h"
 
 #include <types.h>
 #include <dolphin.h>
 
-extern iTime sStartupTime;
-extern float32 sGameTime;
-extern float32 lbl_803CE1D8;
+static iTime sStartupTime;
+static float32 sGameTime;
 
 void iTimeInit()
 {
@@ -21,14 +21,10 @@ iTime iTimeGet()
     return OSGetTime() - sStartupTime;
 }
 
-#if 0
-// I can't seem to figure out the float operations going on here. It looks the setup is fairly right though?
 float32 iTimeDiffSec(iTime time)
 {
-    return (float32)time / (176 - lbl_803CE1D8);
+    return (float32)time / (GET_BUS_FREQUENCY() / 4);
 }
-
-#endif
 
 float32 iTimeDiffSec(iTime t0, iTime t1)
 {
