1868a944a289b411c88d6883009d6734ba5d5562
Ian <ian.goofygoober@gmail.com>
2024-07-18 17:31:25 -0500

    Finish iPad Equivalent
    - Complete iPadEnable
    - Complete iPadConvStick
    - Complete iPadUpdate equivalent
    - Complete iPadStopRumble equivalent
    - Add PADStatus to dolphin.h
    - Mark iPad as equivalent

--- src/SB/Core/gc/iPad.h
+++ src/SB/Core/gc/iPad.h
@@ -13,6 +13,10 @@ struct _tagxRumble;
 
 S32 iPadInit();
 _tagxPad* iPadEnable(_tagxPad* pad, S16 port);
+S32 iPadConvStick(float value);
+S32 iPadUpdate(_tagxPad* pad, U32* on);
+S32 iPadConvFromGCN(U32 a, U32 b, U32 c);
+void iPadRumbleFx(_tagxPad* p, _tagxRumble* r, F32 time_passed);
 void iPadStopRumble(_tagxPad* pad);
 void iPadStopRumble();
 void iPadStartRumble(_tagxPad* pad, _tagxRumble* rumble);
