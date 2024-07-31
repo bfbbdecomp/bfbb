5baba51f282f430205ddfd459460a22e006f3487
escape209 <agtescape@gmail.com>
2024-07-31 16:26:07 +0100

    zMain things

--- src/SB/Game/zMain.cpp
+++ src/SB/Game/zMain.cpp
@@ -1,7 +1,30 @@
+#include "zGlobals.h"
 #include "zMain.h"
 
+#include <string.h>
 #include <types.h>
 
 void iEnvStartup()
 {
 }
+
+void zMainOutputMgrSetup()
+{
+    iTime tim = iTimeGet();
+    iTimeDiffSec(tim);
+    iTimeGet();
+}
+
+void zMainInitGlobals()
+{
+    memset(&globals, 0, 0x1fc8);
+    globals.sceneFirst = 1;
+    iTime tim = iTimeGet();
+    iTimeDiffSec(tim);
+    iTimeGet();
+}
+
+void zMainMemLvlChkCB()
+{
+    zSceneMemLvlChkCB();
+} 
