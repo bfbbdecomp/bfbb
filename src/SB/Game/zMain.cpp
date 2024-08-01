6cdd771b6d73274395c12e0d4b428e2468c70b63
escape209 <agtescape@gmail.com>
2024-08-01 06:26:03 +0100

    zMain things (#337)
    * zMain things
    
    * Match zLedgeAdjust
    
    * Update src/SB/Game/zMain.cpp
    
    Co-authored-by: Thomas <doodrabbit@hotmail.com>
    
    ---------
    
    Co-authored-by: Thomas <doodrabbit@hotmail.com>

--- src/SB/Game/zMain.cpp
+++ src/SB/Game/zMain.cpp
@@ -1,7 +1,36 @@
+#include "zEntPlayer.h"
+#include "zGlobals.h"
 #include "zMain.h"
 
+#include <string.h>
 #include <types.h>
 
 void iEnvStartup()
 {
+}
+
+void zMainOutputMgrSetup()
+{
+    iTime tim = iTimeGet();
+    iTimeDiffSec(tim);
+    iTimeGet();
+}
+
+void zMainInitGlobals()
+{
+    memset(&globals, 0, sizeof(zGlobals));
+    globals.sceneFirst = 1;
+    iTime tim = iTimeGet();
+    iTimeDiffSec(tim);
+    iTimeGet();
+}
+
+void zMainMemLvlChkCB()
+{
+    zSceneMemLvlChkCB();
+}
+
+void zLedgeAdjust(zLedgeGrabParams* params)
+{
+    params->animGrab *= (1.0f/30);
 }
