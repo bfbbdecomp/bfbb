fa576ff84d0dc461312f34b623e0a1caaf311c0e
TotallyNotMichael-GH <iamtotallynotmichael@gmail.com>
2025-02-02 18:11:41 -0500

    Even more functions out of the way
    Thanks to Square for helping with
    ZapperStop!

--- src/SB/Game/zEntSimpleObj.cpp
+++ src/SB/Game/zEntSimpleObj.cpp
@@ -5,3 +5,18 @@
 void zEntSimpleObj_Move(xEnt*, xScene*, F32, xEntFrame*)
 {
 }
+
+void zEntSimpleObj_Setup(zEntSimpleObj *arg0) 
+{
+    zEntSetup((zEnt *) arg0);
+}
+
+void zEntSimpleObj_Save(zEntSimpleObj *arg0, xSerial *arg1) 
+{
+    zEntSave((zEnt *) arg0, arg1);
+}
+
+void zEntSimpleObj_Load(zEntSimpleObj *arg0, xSerial *arg1) 
+{
+    zEntLoad((zEnt *) arg0, arg1);
+}
--- src/SB/Game/zNPCGoalAmbient.cpp
+++ src/SB/Game/zNPCGoalAmbient.cpp
@@ -26,3 +26,33 @@ S32 zNPCGoalJellyAttack::Enter(F32 arg0, void* arg1)
     zNPCGoalJellyAttack::ZapperStart();
     return zNPCGoalPushAnim::Enter(arg0, arg1);
 }
+
+S32 zNPCGoalJellyAttack::Exit(F32 arg0, void* arg1)
+{
+    zNPCGoalJellyAttack::ZapperStop();
+    return zNPCGoalPushAnim::Exit(arg0, arg1);
+}
+
+S32 zNPCGoalJellyAttack::Process(en_trantype* arg0, F32 arg1, void* arg2, xScene* arg3)
+{
+    zNPCGoalJellyAttack::ZapperUpdate();
+    return zNPCGoalPushAnim::Process(arg0, arg1, arg2, arg3);
+}
+
+S32 zNPCGoalJellyAttack::ZapperStop()
+{
+    s32 var_r29;
+    zLightning *temp_r3;
+    zNPCGoalJellyAttack *var_r30;
+    zNPC_SNDStop((_tageNPCSnd) 4);
+    var_r30 = this;
+    var_r29 = 0;
+    for (S32 i = 0; i< 3; ++i) 
+    {
+        if(zap_lytnin[i] != NULL)
+        {
+            zLightningKill(zap_lytnin[i]);
+        }
+        zap_lytnin[i] = NULL;
+    }
+}
