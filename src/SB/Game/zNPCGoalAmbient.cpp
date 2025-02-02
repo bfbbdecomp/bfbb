84864747d4a387c726b39e5aaf52c97894cf3d92
TotallyNotMichael-GH <iamtotallynotmichael@gmail.com>
2025-02-01 19:31:01 -0500

    Match zNPCGoalJellyAttack::Enter(float, void*)
    My first Function in god knows how long.

--- src/SB/Game/zNPCGoalAmbient.cpp
+++ src/SB/Game/zNPCGoalAmbient.cpp
@@ -2,8 +2,8 @@
 
 #include "xMath3.h"
 #include "xVec3.h"
-
 #include "zNPCGoalAmbient.h"
+#include "zNPCSndTable.h"
 
 void xMat3x3RMulVec(xVec3* o, const xMat3x3* m, const xVec3* v)
 {
@@ -14,4 +14,15 @@ void xMat3x3RMulVec(xVec3* o, const xMat3x3* m, const xVec3* v)
     o->x = x;
     o->y = y;
     o->z = z;
+}
+
+S32 zNPCGoalJellyAttack::Enter(F32 arg0, void* arg1)
+{
+    zNPCJelly* temp_r31 = (zNPCJelly*)psyche->clt_owner;
+
+    temp_r31->SndPlayRandom(NPC_STYP_ENCOUNTER);
+    temp_r31->VelStop();
+    lastAnimTime = 0;
+    zNPCGoalJellyAttack::ZapperStart();
+    return zNPCGoalPushAnim::Enter(arg0, arg1);
 }
