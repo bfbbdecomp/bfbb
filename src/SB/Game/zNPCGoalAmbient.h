63d8bfb13e978fd79a73120e1aba5c79abb49b52
TotallyNotMichael-GH <iamtotallynotmichael@gmail.com>
2025-02-01 19:47:48 -0500

    Match zNPCGoalJellyAttack::Enter(float, void*) (#393)
    My first Function in god knows how long.

--- src/SB/Game/zNPCGoalAmbient.h
+++ src/SB/Game/zNPCGoalAmbient.h
@@ -1,7 +1,12 @@
 #ifndef ZNPCGOALAMBIENT_H
 #define ZNPCGOALAMBIENT_H
 
+#include "zNPCGoalAmbient.h"
 #include "zNPCGoalCommon.h"
+#include "zNPCGoalStd.h"
+#include "zNPCSndTable.h"
+#include "zNPCTypeAmbient.h"
+
 
 xFactoryInst* GOALCreate_Ambient(S32 who, RyzMemGrow* grow, void*);
 
@@ -13,6 +18,13 @@ struct zNPCGoalJellyBirth : zNPCGoalCommon
     void BirthInfoSet(const xVec3* pos_birth, F32 tym_fall);
     S32 Process(en_trantype* trantyp, F32 dt, void* ctxt, xScene* xscn);
     S32 Enter(F32 dt, void* updCtxt);
+};
+
+struct zNPCGoalJellyAttack : zNPCGoalPushAnim
+{
+    U32 lastAnimTime;
+    S32 Enter(F32 dt, void* updCtxt);
+    S32 ZapperStart();
 };
 
 #endif
