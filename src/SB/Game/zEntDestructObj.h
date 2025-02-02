62849253aaf311260b4716956ac300bcccdc8139
Josh Sanchez <joshuaesanch@gmail.com>
2025-02-01 19:07:55 -0600

    Partial match zEntDestructObj up to 97%+

--- src/SB/Game/zEntDestructObj.h
+++ src/SB/Game/zEntDestructObj.h
@@ -2,6 +2,20 @@
 #define ZENTDESTRUCTOBJ_H
 
 #include "zEnt.h"
+#include "xSFX.h"
+#include "zParEmitter.h"
+#include "zGlobals.h"
+#include "zRumble.h"
+#include "xVec3.h"
+#include "xString.h"
+#include "xstransvc.h"
+#include "zShrapnel.h"
+#include "zCollGeom.h"
+#include "iCollide.h"
+
+#define DOBJ_STATE_INIT 0
+#define DOBJ_STATE_RESET 1
+#define DOBJ_STATE_DESTROYED 2
 
 struct zEntDestructObjAsset
 {
@@ -9,50 +23,63 @@ struct zEntDestructObjAsset
     U32 initAnimState;
     U32 health;
     U32 spawnItemID;
-    U32 dflags;
+    U32 dflags; // 0x10
     U8 collType;
     U8 fxType;
     U8 pad[2];
     F32 blast_radius;
     F32 blast_strength;
-    U32 shrapnelID_destroy;
-    U32 shrapnelID_hit;
-    U32 sfx_destroy;
-    U32 sfx_hit;
-    U32 hitModel;
-    U32 destroyModel;
+    U32 shrapnelID_destroy; // 0x20
+    U32 shrapnelID_hit; // 0x24
+    U32 sfx_destroy; // 0x28
+    U32 sfx_hit; // 0x2C
+    U32 hitModelId; // 0x30
+    U32 destroyModelId; // 0x34
 };
-
-struct zParEmitter;
-struct zShrapnelAsset;
-struct xSFXAsset;
 
 struct zEntDestructObj : zEnt
 {
-    zEntDestructObjAsset* dasset;
+    zEntDestructObjAsset* dasset; // Offset 0xD4
     U32 state;
     U32 healthCnt;
-    F32 fx_timer;
+    F32 fx_timer; // Offset 0xE0
     zParEmitter* fx_emitter;
     F32 respawn_timer;
     U32 throw_target;
-    zShrapnelAsset* shrapnel_destroy;
+    zShrapnelAsset* shrapnel_destroy; // Offset 0xF0
     zShrapnelAsset* shrapnel_hit;
     xModelInstance* base_model;
     xModelInstance* hit_model;
-    xModelInstance* destroy_model;
+    xModelInstance* destroy_model; // Offset 0x100
     void (*destroy_notify)(zEntDestructObj&, void*);
     void* notify_context;
-    xSFXAsset* sfx_destroy;
-    xSFXAsset* sfx_hit;
+    xSFXAsset* sfx_destroy; // 0x10C
+    xSFXAsset* sfx_hit; // 0x110
 };
+
+static zParEmitter* sEmitDust;
+static zParEmitter* sEmitFire;
+static zParEmitter* sEmitSmoke;
+static zParEmitter* sEmitWeb;
+static zParEmitter* sEmitXplo;
+static zParEmitter* sShrapDefault;
 
 void zEntDestructObj_FindFX();
 void zEntDestructObj_Init(void* ent, void* asset);
+void zEntDestructObj_Init(zEntDestructObj* ent, xEntAsset* asset);
+
+// TODO: update unk with callback signatures from zEnt
+void zEntDestructObj_Move(zEntDestructObj* ent, xScene* scene, F32 unk, xEntFrame* frame);
+void zEntDestructObj_Update(zEntDestructObj* ent, xScene* scene, F32 unk);
+void zEntDestructObj_Hit(zEntDestructObj* ent, U32 mask);
+U32 zEntDestructObj_GetHit(zEntDestructObj* ent, U32 mask);
 void zEntDestructObj_Save(zEntDestructObj* ent, xSerial* s);
 void zEntDestructObj_Load(zEntDestructObj* ent, xSerial* s);
 void zEntDestructObj_Setup(zEntDestructObj* ent);
+void zEntDestructObj_Reset(zEntDestructObj* ent, xScene* scene);
 U32 zEntDestructObj_isDestroyed(zEntDestructObj* ent);
-void zEntDestructObj_Hit(zEntDestructObj* ent, U32 mask);
+void zEntDestructObj_DestroyFX(zEntDestructObj* ent);
+S32 zEntDestructObjEventCB(xBase* from, xBase* to, U32 toEvent, const F32* toParam,
+                           xBase* baseUnk3);
 
 #endif
