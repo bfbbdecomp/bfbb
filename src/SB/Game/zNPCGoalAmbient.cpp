4f2f2a375ccdb6ac6433edd91f9cd91cdcf96da5
TotallyNotMichael-GH <iamtotallynotmichael@gmail.com>
2025-02-02 18:35:36 -0500

    Partial matches for more Jellyfish functions (#395)
    * Match zNPCGoalJellyAttack::Enter(float, void*)
    
    My first Function in god knows how long.
    
    * Even more functions out of the way
    
    Thanks to Square for helping with
    ZapperStop!
    
    * Even more functions out of the way
    
    Thanks to Square for helping with
    ZapperStop!
    
    * Thank you, Square

--- src/SB/Game/zNPCGoalAmbient.cpp
+++ src/SB/Game/zNPCGoalAmbient.cpp
@@ -26,3 +26,28 @@ S32 zNPCGoalJellyAttack::Enter(F32 arg0, void* arg1)
     zNPCGoalJellyAttack::ZapperStart();
     return zNPCGoalPushAnim::Enter(arg0, arg1);
 }
+
+S32 zNPCGoalJellyAttack::Exit(F32 arg0, void* arg1)
+{
+    zNPCGoalJellyAttack::ZapperStop();
+    return zNPCGoalPushAnim::Exit(arg0, arg1);
+}
+
+S32 zNPCGoalJellyAttack::Process(en_trantype* arg0, F32 arg1, void* arg2, xScene* arg3)
+{
+    zNPCGoalJellyAttack::ZapperUpdate();
+    return zNPCGoalPushAnim::Process(arg0, arg1, arg2, arg3);
+}
+
+S32 zNPCGoalJellyAttack::ZapperStop()
+{
+    zNPC_SNDStop((_tageNPCSnd) 4);
+    for (S32 i = 0; i< 3; ++i) 
+    {
+        if(zap_lytnin[i] != NULL)
+        {
+            zLightningKill(zap_lytnin[i]);
+        }
+        zap_lytnin[i] = NULL;
+    }
+}
