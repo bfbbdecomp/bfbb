90369a38554ca8e7652d9247d5bbcad201a42e31
escape209 <agtescape@gmail.com>
2025-02-04 18:04:17 +0000

    A few matches (zGrid, zNPC)

--- src/SB/Game/zGrid.cpp
+++ src/SB/Game/zGrid.cpp
@@ -1,3 +1,20 @@
 #include "zGrid.h"
+#include "xGrid.h"
+#include "xString.h"
 
 #include <types.h>
+
+xGrid colls_grid;
+xGrid colls_oso_grid;
+xGrid npcs_grid;
+static int zGridInitted;
+
+void zGridExit(zScene* s)
+{
+    xGridKill(&colls_grid);
+    xGridKill(&colls_oso_grid);
+    xGridKill(&npcs_grid);
+
+    gGridIterActive = NULL;
+    zGridInitted = NULL;
+}
