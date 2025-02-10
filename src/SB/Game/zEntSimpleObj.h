81c5b37e63752285b36716a67626bf679b917cda
Josh Sanchez <joshuaesanch@gmail.com>
2025-02-10 14:39:44 -0600

    zEntSimpleObj full match on most functions

--- src/SB/Game/zEntSimpleObj.h
+++ src/SB/Game/zEntSimpleObj.h
@@ -7,6 +7,16 @@
 #include "xstransvc.h"
 #include "iAnim.h"
 #include "iModel.h"
+#include "zFX.h"
+#include "xEnt.h"
+#include "zCollGeom.h"
+#include "zGlobals.h"
+#include "xSkyDome.h"
+#include "zGoo.h"
+#include "zShrapnel.h"
+#include "xMath.h"
+#include "zGame.h"
+#include "zLOD.h"
 
 struct xSimpleObjAsset
 {
@@ -29,16 +39,19 @@ struct zScene;
 extern void iModelAnimMatrices(RpAtomic*, xQuat*, xVec3*, RwMatrixTag*);
 
 void zEntSimpleObj_MgrInit(zEntSimpleObj** entList, U32 entCount);
-void zEntSimpleObj_MgrUpdateRender(RpWorld*, F32 dt);
+void zEntSimpleObj_MgrUpdateRender(RpWorld* world, F32 dt);
 void zEntSimpleObj_MgrCustomUpdate(zScene* s, F32 dt);
 void zEntSimpleObj_MgrCustomRender();
 void zEntTrackPhysics_Init(void* ent, void* asset);
 void zEntSimpleObj_Init(void* ent, void* asset);
+void zEntSimpleObj_Render(xEnt* ent);
+void zEntSimpleObj_Init(zEntSimpleObj* ent, xEntAsset* asset, bool arg2);
+void zEntSimpleObj_Move(xEnt*, xScene*, F32, xEntFrame*);
 void zEntSimpleObj_Setup(zEntSimpleObj* ent);
 void zEntSimpleObj_Update(zEntSimpleObj* ent, xScene* scene, F32 dt);
 void zEntSimpleObj_Save(zEntSimpleObj* ent, xSerial* s);
 void zEntSimpleObj_Load(zEntSimpleObj* ent, xSerial* s);
 void zEntSimpleObj_Reset(zEntSimpleObj* ent, xScene* scene);
-void zEntSimpleObjEventCB(xBase* from, xBase* to, U32 toEvent, const F32* toParam, xBase*);
+s32 zEntSimpleObjEventCB(xBase* from, xBase* to, U32 toEvent, const F32* toParam, xBase* base3);
 
 #endif
