16179b3bac12fdec6f937022ef76e10ff98d79eb
Josh Sanchez <joshuaesanch@gmail.com>
2025-03-15 01:45:40 -0500

    xColor: Initialize static color data

--- src/SB/Core/x/xColor.h
+++ src/SB/Core/x/xColor.h
@@ -3,21 +3,6 @@
 
 #include "iColor.h"
 
-extern iColor_tag g_RED;
-extern iColor_tag g_GREEN;
-extern iColor_tag g_BLUE;
-extern iColor_tag g_CYAN;
-extern iColor_tag g_YELLOW;
-extern iColor_tag g_WHITE;
-extern iColor_tag g_GRAY50;
-extern iColor_tag g_NEON_RED;
-extern iColor_tag g_NEON_GREEN;
-extern iColor_tag g_NEON_BLUE;
-extern iColor_tag g_PIMP_GOLD;
-extern iColor_tag g_ORANGE;
-extern iColor_tag g_LAVENDER;
-extern iColor_tag g_PINK;
-
 iColor_tag xColorFromRGBA(U8 r, U8 g, U8 b, U8 a);
 
 #endif
