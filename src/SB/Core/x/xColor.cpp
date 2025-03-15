ca5649fc6b169a0d76bca3bc6d365c65f166f849
Josh Sanchez <joshuaesanch@gmail.com>
2025-03-15 01:47:51 -0500

    Clang format pass on xColor

--- src/SB/Core/x/xColor.cpp
+++ src/SB/Core/x/xColor.cpp
@@ -1,16 +1,16 @@
 #include "xColor.h"
 
-static iColor_tag g_RED = {255, 0, 0, 255};
-static iColor_tag g_GREEN = {0, 255, 0, 255};
-static iColor_tag g_BLUE = {0, 0, 255, 255};
-static iColor_tag g_CYAN = {0, 255, 255, 255};
-static iColor_tag g_YELLOW = {255, 255, 0, 255};
-static iColor_tag g_WHITE = {255, 255, 255, 255};
-static iColor_tag g_GRAY50 = {128, 128, 128, 255};
-static iColor_tag g_NEON_RED = {255, 32, 0, 255};
-static iColor_tag g_NEON_GREEN = {32, 255, 0, 255};
-static iColor_tag g_NEON_BLUE = {32, 32, 255, 255};
-static iColor_tag g_PIMP_GOLD = {215, 220, 19, 255};
-static iColor_tag g_ORANGE = {255, 165, 0, 255};
-static iColor_tag g_LAVENDER = {200, 105, 229, 255};
-static iColor_tag g_PINK = {255, 128, 255, 255};
+static iColor_tag g_RED = { 255, 0, 0, 255 };
+static iColor_tag g_GREEN = { 0, 255, 0, 255 };
+static iColor_tag g_BLUE = { 0, 0, 255, 255 };
+static iColor_tag g_CYAN = { 0, 255, 255, 255 };
+static iColor_tag g_YELLOW = { 255, 255, 0, 255 };
+static iColor_tag g_WHITE = { 255, 255, 255, 255 };
+static iColor_tag g_GRAY50 = { 128, 128, 128, 255 };
+static iColor_tag g_NEON_RED = { 255, 32, 0, 255 };
+static iColor_tag g_NEON_GREEN = { 32, 255, 0, 255 };
+static iColor_tag g_NEON_BLUE = { 32, 32, 255, 255 };
+static iColor_tag g_PIMP_GOLD = { 215, 220, 19, 255 };
+static iColor_tag g_ORANGE = { 255, 165, 0, 255 };
+static iColor_tag g_LAVENDER = { 200, 105, 229, 255 };
+static iColor_tag g_PINK = { 255, 128, 255, 255 };
