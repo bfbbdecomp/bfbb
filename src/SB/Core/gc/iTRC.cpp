6ac85e1469e3f622468c69dc03831643317167a9
Katlyn <175969931+RejectSanity@users.noreply.github.com>
2025-03-19 15:38:32 -0400

    Filled out iTRC file and added function iTRCDisk::Init (#451)
    * zNPCTypeDutchman: added Death::Enter function
    
    * Implemented suggestions from PR review #445
    
    * Fixed float to F32 dt & added updCtxt
    
    * Added Damage::Exit function and Declared GXEnd in iTRC
    
    * Filled out iTRC file and added function iTRCDisk::Init

--- src/SB/Core/gc/iTRC.cpp
+++ src/SB/Core/gc/iTRC.cpp
@@ -1,8 +1,161 @@
 #include "iTRC.h"
 
 #include <types.h>
+#include <string.h>
+
+bool ROMFont::Init()
+{
+
+}
+
+void ROMFont::InitGX()
+{
+
+}
+
+void ROMFont::InitVI()
+{
+
+}
+
+void ROMFont::RenderBegin()
+{
+
+}
+
+void ROMFont::RenderEnd()
+{
+
+}
+
+void ROMFont::SwapBuffers()
+{
+
+}
+
+void ROMFont::DrawCell(int, int, int, int)
+{
+
+}
 
 void GXEnd()
+{
+
+}
+
+void GXPosition3s16()
+{
+
+}
+
+void ROMFont::LoadSheet(void*)
+{
+
+}
+
+void GXSetTexCoordGen()
+{
+
+}
+
+void ROMFont::DrawString(int, int, char*)
+{
+
+}
+
+void ROMFont::GetWidth(char*)
+{
+
+}
+
+void ROMFont::DrawTextBox(int, int, int, int, char*)
+{
+
+}
+
+
+
+int ResetButton::EnableReset() 
+{  
+   return ResetButton::mResetEnabled = 1;
+}
+
+int ResetButton::DisableReset() 
+{  
+   return ResetButton::mResetEnabled = 0;
+}
+
+void ResetButton::SetSndKillFunction(void (*Func)())
+{
+  
+}
+
+void ResetButton::CheckResetButton()
+{
+  
+}
+
+bool iTRCDisk::Init(void)
+{
+  bool RFInit = ROMFont::Init();
+  return RFInit;
+}
+
+void iTRCDisk::SetErrorMessage(const char*)
+{
+  
+}
+
+void iTRCDisk::ResetMessage()
+{
+}
+
+void iTRCDisk::SetPadStopRumblingFunction(void (*)(void))
+{
+
+}
+
+void iTRCDisk::SetSndSuspendFunction(void (*)(void))
+{
+
+}
+
+void iTRCDisk::SetSndResumeFunction(void (*)(void))
+{
+
+}
+
+void iTRCDisk::SetSndKillFunction(void (*)(void))
+{
+
+}
+
+void iTRCDisk::SetMovieSuspendFunction(void (*)(void))
+{
+
+}
+
+void iTRCDisk::SetMovieResumeFunction(void (*)(void))
+{
+
+}
+
+bool iTRCDisk::IsDiskIDed()
+{
+
+}
+
+void iTRCDisk::DisplayErrorMessage()
+{
+  
+}
+
+void iTRCDisk::SetDVDState()
+{
+
+}
+
+bool iTRCDisk::CheckDVDAndResetState()
 {
 
 }
--- src/SB/Core/gc/iTRC.h
+++ src/SB/Core/gc/iTRC.h
@@ -10,20 +10,48 @@ struct _tagiTRCPadInfo
     _tagPadInit pad_init;
 };
 
+namespace ROMFont
+{
+    bool Init();
+    void InitGX();
+    void InitVI();
+    void RenderBegin();
+    void RenderEnd();
+    void SwapBuffers();
+    void DrawCell(int, int, int, int);
+    void LoadSheet(void*);
+    void DrawString(int, int, char*);
+    void GetWidth(char*);
+    void DrawTextBox(int, int, int, int, char*);
+}
+
 // Yes, this is a namespace, not a class.
 namespace iTRCDisk
 {
+    void SetPadStopRumblingFunction(void (*)(void));
+    void SetSndSuspendFunction(void (*)(void));
+    void SetSndResumeFunction(void (*)(void));
+    void SetSndKillFunction(void (*)(void));
+    void SetMovieSuspendFunction(void (*)(void));
+    void SetMovieResumeFunction(void (*)(void));
     bool CheckDVDAndResetState();
-    void Init();
+    bool Init();
+    void ResetMessage();
+    void DisplayErrorMessage();
+    void SetDVDState();
+    void SetErrorMessage(const char*);
     bool IsDiskIDed();
+    char mMessage();
 } // namespace iTRCDisk
 
 namespace ResetButton
 {
-    void EnableReset();
-    void DisableReset();
+    int EnableReset();
+    int mResetEnabled;
+    int DisableReset();
     void SetSndKillFunction(void (*func)());
     void CheckResetButton();
+
 } // namespace ResetButton
 
 #endif
