db3b80a049832828f2d2c96653ff631e2a04b80c
Red <53205920+Redberd36@users.noreply.github.com>
2025-04-13 16:42:31 -0400

    zNPCFXCinematic -> NPARMgmt::KillAll() 100% (#516)
    * Fix zNPCFXCinematic implementation
    
    * All NPCCone functions 100%
    
    * forgot one change.
    
    * Moved Static S32 init to the top of file in zNPCFXCinematic.cpp
    
    * Removed commented code in zNPCFXCinematic.cpp
    
    * Removed commented code at the bottom of zNPCFXCinematic.cpp
    
    * Removed commented code at the top of zNPCFXCinematic.h
    
    * Added parameter names for UVSliceSet, UVBaseSet, ColorSet
    
    * Removed the blank git file

--- src/SB/Game/zNPCFXCinematic.cpp
+++ src/SB/Game/zNPCFXCinematic.cpp
@@ -6,8 +6,18 @@
 #include <types.h>
 #include "xCutscene.h"
 
+static S32 init = 0;
+
 void zNPCFXShutdown()
 {
+}
+
+void zNPCFXCinematic_Init()
+{
+    if (!init)
+    {
+        init = 1;
+    }
 }
 
 void clamp_bone_index(NCINEntry*, RpAtomic*)
@@ -19,7 +29,31 @@ void NPCCone::TextureSet(RwRaster* raster)
     rast_cone = raster;
 }
 
+void NPCCone::UVSliceSet(F32 u, F32 v)
+{
+    this->uv_tip[2] = u;
+    this->uv_slice[1] = v; 
+}
+
+void NPCCone::UVBaseSet(F32 u, F32 v)
+{
+    this->uv_tip[0] = u;
+    this->uv_tip[1] = v;
+}
+
+void NPCCone::ColorSet(RwRGBA top, RwRGBA bot)
+{
+    this->rgba_top = top;
+    this->rgba_bot = bot;
+}
+
 void NPCCone::RadiusSet(F32 conefloat)
 {
     rad_cone = conefloat;
 }
+
+void NPARMgmt::KillAll()
+{
+    this->cnt_active = 0;
+}
+
