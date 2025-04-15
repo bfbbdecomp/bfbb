5413ee82f2e92fa6d8ae1767859cc2e5576ae2aa
Red <53205920+Redberd36@users.noreply.github.com>
2025-04-15 10:57:23 -0400

    40% - 92% for some NCIN_Par functions in zNPCFXCinematic (#518)
    * Fix zNPCFXCinematic implementation
    
    * All NPCCone functions 100%
    
    * forgot one change.
    
    * Moved Static S32 init to the top of file in zNPCFXCinematic.cpp
    
    * Removed commented code in zNPCFXCinematic.cpp
    
    * Removed commented code at the bottom of zNPCFXCinematic.cpp
    
    * Removed commented code at the top of zNPCFXCinematic.h
    
    * Added parameter names for UVSliceSet, UVBaseSet, ColorSet
    
    * Removed the blank git file
    
    * 40% - 92% for some NCIN_Par functions in zNPCFXCinematic
    
    * Pointer name fixes for NCIN_Par functions in zNPCFXCinematic + one 100% zTalkBox function
    
    * Deleted static variables top of script

--- src/SB/Game/zTalkBox.cpp
+++ src/SB/Game/zTalkBox.cpp
@@ -1,3 +1,19 @@
 #include "zTalkBox.h"
 
 #include <types.h>
+
+namespace
+{
+    struct SharedTalkboxState
+    {
+        void* padding[2]; // FIXME: variables not verified
+        ztalkbox* active;
+    };
+
+    SharedTalkboxState ztalkbox_shared;
+}
+
+ztalkbox* ztalkbox::get_active()
+{
+    return ztalkbox_shared.active;
+}
