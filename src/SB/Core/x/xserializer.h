47e02ba3569e12a77a0001bac18fc89f57b0c22f
Kevin <136357605+energydrink02@users.noreply.github.com>
2025-05-25 19:14:31 +0200

    xserializer progress

--- src/SB/Core/x/xserializer.h
+++ src/SB/Core/x/xserializer.h
@@ -3,6 +3,7 @@
 
 #include <types.h>
 #include "xsavegame.h"
+#include "xordarray.h"
 
 struct st_SERIAL_CLIENTINFO
 {
@@ -16,6 +17,16 @@ struct st_SERIAL_PERCID_SIZE
 {
     U32 idtag;
     S32 needsize;
+};
+
+struct st_XSERIAL_DATA_PRIV
+{
+    S32 flg_info;
+    S32* bitbuf;
+    S32 buf_bytcnt;
+    st_SERIAL_CLIENTINFO* cltbuf;
+    st_SERIAL_CLIENTINFO* cltnext;
+    st_XORDEREDARRAY cltlist;
 };
 
 struct xSerial
@@ -32,7 +43,10 @@ struct xSerial
     ~xSerial();
 
     void setClient(U32 idtag);
+    void prepare(U32 idtag);
 
+    void wrbit(S32 is_on);
+    S32 Write(char* data, S32 elesize, S32 n);
     S32 Write_b1(S32 bits);
     S32 Write_b7(U32 bits);
     S32 Write(U8 data);
@@ -40,6 +54,8 @@ struct xSerial
     S32 Write(S32 data);
     S32 Write(U32 data);
     S32 Write(F32 data);
+    S32 rdbit();
+    S32 Read(char* buf, S32 elesize, S32 n);
     S32 Read_b1(S32* bits);
     S32 Read_b7(U32* bits);
     S32 Read(U8* buf);
@@ -51,9 +67,10 @@ struct xSerial
 };
 
 S32 xSerialStartup(S32, st_SERIAL_PERCID_SIZE*);
-void xSerialShutdown();
+S32 xSerialShutdown();
 void xSerialTraverse(S32 (*func)(U32 clientID, xSerial* xser));
 void xSerialWipeMainBuffer();
+void xSER_init_buffers(S32 count, st_SERIAL_PERCID_SIZE* sizeinfo);
 S32 xSerial_svgame_register(st_XSAVEGAME_DATA* sgctxt, en_SAVEGAME_MODE mode);
 
 #endif
