708c85b5804b230b934435b7e7b21ba9e3adfe34
Colin Miller <therealnucka@gmail.com>
2025-05-30 20:13:57 -0400

    Small work in iModel

--- src/SB/Core/gc/iModel.cpp
+++ src/SB/Core/gc/iModel.cpp
@@ -2,6 +2,9 @@
 
 #include <types.h>
 
+RpLight* sEmptyDirectionalLight;
+RpLight* sEmptyAmbientLight;
+
 RwFrame* GetChildFrameHierarchy(RwFrame* frame, void* data)
 {
     RpHAnimHierarchy* hierarchy = RpHAnimFrameGetHierarchy(frame);
@@ -23,6 +26,26 @@ void* GetHierarchy(RpAtomic* frame)
     unk_0[0] = 0;
     GetChildFrameHierarchy((RwFrame*)frame, unk_0);
     return unk_0[0];
+}
+
+void iModelInit()
+{
+    RwFrame* frame;
+    RwRGBAReal black;
+    void* tempVoid = 0;
+
+    for (S32 i = 0; i < 4; i++)
+    {
+        frame = (RwFrame*)RpLightCreate(i);
+        //black = frame;
+        RpLightSetColor(sEmptyDirectionalLight, &black);
+        frame = RwFrameCreate();
+        _rwObjectHasFrameSetFrame(sEmptyDirectionalLight, frame);
+
+        sEmptyAmbientLight = RpLightCreate(2);
+
+        RpLightSetColor(sEmptyAmbientLight, &black);
+    }
 }
 
 RpAtomic* iModelFileNew(void* buffer, U32 size)
@@ -49,13 +72,57 @@ void iModelUnload(RpAtomic* userdata)
     if ((RpClump*)userdata != 0)
     {
         RwFrameGetRoot((RwFrame*)userdata);
-        RwFrameDestroyHierarchy(frame);
         userdata = 0;
+    }
+    else
+    {
+        RwFrameDestroyHierarchy(frame);
     }
     if (clump != 0)
     {
         RpClumpDestroy(clump);
     }
+}
+
+RpAtomic* NextAtomicCallback(RpAtomic* atomic, void* data)
+{
+    // What?
+
+    RpAtomic** nextModel;
+
+    if (data != atomic)
+    {
+        atomic = 0;
+        return atomic;
+    }
+    if (data != 0)
+    {
+        return atomic;
+    }
+
+    atomic = (RpAtomic*)data;
+
+    return atomic;
+}
+
+RpAtomic* iModelFile_RWMultiAtomic(RpAtomic* model)
+{
+    RpClump* clump;
+    RpAtomic* nextModel = 0;
+    RpAtomic* modelArray[2];
+
+    if (model == 0)
+    {
+        modelArray[0] = 0;
+    }
+    else
+    {
+        //modelArray[0] = (S32)model;
+        modelArray[0] = nextModel;
+        RpClumpForAllAtomics(model->clump, (RpAtomicCallBack)NextAtomicCallback(nextModel, NULL),
+                             modelArray);
+    }
+    return model;
 }
 
 void iModelCacheAtomic(RpAtomic*)
