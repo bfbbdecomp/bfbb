2b989e30d3c2826807d8ee44b838682c8a45d775
bluisblu <53455507+bluisblu@users.noreply.github.com>
2025-10-14 20:56:35 -0400

    Match zLOD data (#612)
    Also roughly start some functions

--- src/SB/Game/zLOD.cpp
+++ src/SB/Game/zLOD.cpp
@@ -1,5 +1,6 @@
 #include "zLOD.h"
 
+#include "xModel.h"
 #include <types.h>
 
 #include "xEnt.h"
@@ -8,20 +9,16 @@
 #include "zBase.h"
 #include "zEntDestructObj.h"
 
-extern U32 sTableCount;
-extern zLODTable* sTableList;
-extern U32 sManagerCount;
-extern zLODManager sManagerList[2048];
-
-extern F32 lbl_803CDC44; // 1.0e-4, 0.0001
-extern F32 lbl_803CDC40; // 1.0
-extern F32 lbl_803CDC48; // 4.0
-extern F32 lbl_803CDC4C; // 10.0
+static U32 sTableCount;
+static zLODTable* sTableList;
+static U32 sManagerIndex;
+static U32 sManagerCount;
+static zLODManager sManagerList[2048];
 
 // Float memes
 void AddToLODList(xModelInstance* model)
 {
-    for (int i = 0; i < sManagerCount; i++)
+    for (S32 i = 0; i < sManagerCount; i++)
     {
         if (sManagerList[i].model == model)
         {
@@ -29,7 +26,7 @@ void AddToLODList(xModelInstance* model)
         }
     }
 
-    for (int i = 0; i < sTableCount; i++)
+    for (S32 i = 0; i < sTableCount; i++)
     {
         if (sTableList[i].baseBucket != NULL &&
             (*sTableList[i].baseBucket)->OriginalData == model->Data)
@@ -47,14 +44,14 @@ void AddToLODList(xModelInstance* model)
                                  (model->Mat->right).y * (model->Mat->right).y +
                                  (model->Mat->right).z * (model->Mat->right).z);
                 minst = model;
-                if (distscale < lbl_803CDC44)
+                if (distscale < 0.0001f)
                 {
-                    distscale = lbl_803CDC40;
+                    distscale = 1.0f;
                 }
                 while (minst != NULL)
                 {
                     minst->FadeEnd = xsqrt(distscale * sTableList[i].noRenderDist);
-                    minst->FadeStart = minst->FadeEnd - lbl_803CDC48;
+                    minst->FadeStart = minst->FadeEnd - 4.0f;
                     minst = minst->Next;
                 }
 
@@ -62,8 +59,8 @@ void AddToLODList(xModelInstance* model)
                 sManagerList[sManagerCount].lod = &sTableList[i];
                 sManagerList[sManagerCount].model = model;
                 sManagerList[sManagerCount].adjustNoRenderDist =
-                    (lbl_803CDC4C + xsqrt(sTableList[i].noRenderDist)) *
-                    (lbl_803CDC4C + xsqrt(sTableList[i].noRenderDist));
+                    (10.0f + xsqrt(sTableList[i].noRenderDist)) *
+                    (10.0f + xsqrt(sTableList[i].noRenderDist));
                 sManagerCount++;
                 return;
             }
@@ -96,6 +93,156 @@ xEnt* AddToLODList(xEnt* ent, xScene* scene, void* v)
     return ent;
 }
 
+// WIP
+void zLOD_Setup(void)
+{
+    sTableCount = 0;
+    sTableList = NULL;
+    sManagerCount = 0;
+
+    U32 assetCount = xSTAssetCountByType('LODT');
+    if (assetCount == 0)
+    {
+        return;
+    }
+
+    for (U32 i = 0; i < assetCount; i++)
+    {
+        U32 tmpSize;
+        S32* asset = (S32*)xSTFindAssetByType('LODT', i, &tmpSize);
+        sTableCount += *asset;
+    }
+
+    if (sTableCount == 0)
+    {
+        return;
+    }
+
+    zLODTable* tableCurr = (zLODTable*)xMemAlloc(gActiveHeap, sTableCount * sizeof(zLODTable), 0);
+    sTableList = tableCurr;
+
+    for (U32 i = 0; i < assetCount; i++)
+    {
+        U32 tmpSize;
+        S32* asset = (S32*)xSTFindAssetByType('LODT', i, &tmpSize);
+        memcpy(tableCurr, asset + 1, (*asset) * sizeof(zLODTable));
+        tableCurr += *asset;
+    }
+
+    tableCurr = sTableList;
+    for (U32 i = 0; i < sTableCount; i++, tableCurr++)
+    {
+        tableCurr->noRenderDist *= tableCurr->noRenderDist;
+
+        if (tableCurr->baseBucket)
+        {
+            RpAtomic* model = (RpAtomic*)xSTFindAsset((U32)tableCurr->baseBucket, NULL);
+            tableCurr->baseBucket = model ? xModelBucket_GetBuckets(model) : NULL;
+        }
+
+        for (U32 j = 0; j < 3; j++)
+        {
+            if (tableCurr->lodBucket[j])
+            {
+                RpAtomic* model = (RpAtomic*)xSTFindAsset((U32)tableCurr->lodBucket[j], NULL);
+                tableCurr->lodBucket[j] = model ? xModelBucket_GetBuckets(model) : NULL;
+            }
+            tableCurr->lodDist[j] *= tableCurr->lodDist[j];
+        }
+    }
+
+    sManagerCount = 0;
+    xSceneForAllEnts(globals.sceneCur, AddToLODList, 0);
+}
+
+// WIP
+void zLOD_Update(U32 percent_update)
+{
+    if (sManagerCount == 0)
+    {
+        return;
+    }
+
+    U32 numUpdates = (sManagerCount * percent_update) / 100;
+    if (numUpdates == 0)
+    {
+        numUpdates = 1;
+    }
+
+    for (U32 i = 0; i < numUpdates; i++)
+    {
+        sManagerIndex++;
+        if (sManagerIndex >= sManagerCount)
+            sManagerIndex = 0;
+
+        zLODManager* mgr = &sManagerList[sManagerIndex];
+        xModelInstance* model = mgr->model;
+        zLODTable* lod = mgr->lod;
+
+        if (!mgr || !model || !lod)
+        {
+            continue;
+        }
+
+        RwMatrix* mat = model->Mat;
+        F32 distscale = mat->right.x * mat->right.x + mat->up.x * mat->up.x + mat->at.x * mat->at.x;
+        if (distscale < 0.0001f)
+            distscale = 1.0f;
+
+        xVec3* camPos = &globals.camera.mat.pos;
+        F32 camdist2 = 0.0f;
+        if (mat)
+        {
+            F32 dx = camPos->x - mat->pos.x;
+            F32 dy = camPos->y - mat->pos.y;
+            F32 dz = camPos->z - mat->pos.z;
+            camdist2 = (dx * dx + dy * dy + dz * dz) / distscale;
+        }
+
+        if (camdist2 >= mgr->adjustNoRenderDist)
+        {
+            model->PipeFlags |= 0x400;
+
+            if (mgr->numextra)
+            {
+                for (xModelInstance* extra = model->Next; extra; extra = extra->Next)
+                    extra->PipeFlags |= 0x400;
+            }
+        }
+        else
+        {
+            model->PipeFlags &= ~0x400;
+
+            if (lod->baseBucket)
+            {
+                model->Bucket = lod->baseBucket;
+                model->Data = (*lod->baseBucket)->Data;
+            }
+
+            S32 lodIndex;
+            for (lodIndex = 0; lodIndex < 3 && lod->lodBucket[lodIndex]; lodIndex++)
+            {
+                if (lod->lodDist[lodIndex] < camdist2)
+                {
+                    model->Bucket = lod->lodBucket[lodIndex];
+                    model->Data = (*lod->lodBucket[lodIndex])->Data;
+                }
+            }
+
+            if (mgr->numextra)
+            {
+                for (xModelInstance* extra = model->Next; extra; extra = extra->Next)
+                {
+                    if (lodIndex == 0)
+                        extra->PipeFlags &= ~0x400;
+                    else
+                        extra->PipeFlags |= 0x400;
+                }
+            }
+        }
+    }
+}
+
 zLODTable* zLOD_Get(xEnt* ent)
 {
     if (!ent->model)
@@ -103,7 +250,7 @@ zLODTable* zLOD_Get(xEnt* ent)
         return 0;
     }
 
-    for (int i = 0; i < sTableCount; i++)
+    for (S32 i = 0; i < sTableCount; i++)
     {
         if (sTableList[i].baseBucket != NULL)
         {
@@ -114,4 +261,88 @@ zLODTable* zLOD_Get(xEnt* ent)
         }
     }
     return 0;
+}
+
+// WIP
+void zLOD_UseCustomTable(xEnt* ent, zLODTable* lod)
+{
+    xModelInstance* model = ent->model;
+    if (model == NULL || sManagerCount == 0)
+    {
+        return;
+    }
+
+    for (U32 i = 0; i < sManagerCount; i++)
+    {
+        zLODManager* mgr = &sManagerList[i];
+
+        if (mgr->model == model)
+        {
+            mgr->lod = lod;
+
+            F32 dist = xsqrt(lod->noRenderDist) + 10.0f;
+            mgr->adjustNoRenderDist = dist * dist;
+
+            RwMatrix* mat = model->Mat;
+            F32 distscale =
+                mat->right.x * mat->right.x + mat->up.x * mat->up.x + mat->at.x * mat->at.x;
+            if (distscale < 0.0001f)
+            {
+                distscale = 1.0f;
+            }
+
+            xVec3* camPos = &globals.camera.mat.pos;
+            F32 camdist2 = 0.0f;
+            if (mat)
+            {
+                camdist2 = ((camPos->x - mat->pos.x) * (camPos->x - mat->pos.x) +
+                            (camPos->y - mat->pos.y) * (camPos->y - mat->pos.y) +
+                            (camPos->z - mat->pos.z) * (camPos->z - mat->pos.z)) /
+                           distscale;
+            }
+
+            if (camdist2 < mgr->adjustNoRenderDist)
+            {
+                model->PipeFlags &= ~0x400;
+
+                if (lod->baseBucket)
+                {
+                    model->Bucket = lod->baseBucket;
+                    model->Data = (*lod->baseBucket)->Data;
+                }
+
+                S32 lodIndex;
+                for (lodIndex = 0;
+                     lodIndex < 3 && lod->lodBucket[lodIndex] && lod->lodDist[lodIndex] < camdist2;
+                     lodIndex++)
+                {
+                    model->Bucket = lod->lodBucket[lodIndex];
+                    model->Data = (*lod->lodBucket[lodIndex])->Data;
+                }
+
+                if (mgr->numextra)
+                {
+                    for (xModelInstance* m = model->Next; m; m = m->Next)
+                    {
+                        if (lodIndex == 0)
+                            m->PipeFlags &= ~0x400;
+                        else
+                            m->PipeFlags |= 0x400;
+                    }
+                }
+            }
+            else
+            {
+                model->PipeFlags |= 0x400;
+
+                if (mgr->numextra)
+                {
+                    for (xModelInstance* m = model->Next; m; m = m->Next)
+                        m->PipeFlags |= 0x400;
+                }
+            }
+
+            return;
+        }
+    }
 }
