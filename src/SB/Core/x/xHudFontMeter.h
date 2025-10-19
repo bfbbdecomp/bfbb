40332308f55c8aeb307e6701dc8e145aee0416b6
Josh Sanchez <joshuaesanch@gmail.com>
2025-10-18 19:03:52 -0500

    xHudFontMeter: Add font_meter_asset struct from DWARF

--- src/SB/Core/x/xHudFontMeter.h
+++ src/SB/Core/x/xHudFontMeter.h
@@ -18,6 +18,15 @@ namespace xhud
         color32u c;
         color32u drop_c;
     };
+    
+    struct font_meter_asset : meter_asset
+    {
+        font_context font;        // offset 0x4C, size 0x24
+        U8 counter_mode;       // offset 0x70, size 0x1
+        U8 pad1;               // offset 0x71, size 0x1
+        U8 pad2;               // offset 0x72, size 0x1
+        U8 pad3;               // offset 0x73, size 0x1
+    };
 
     struct font_meter_widget : meter_widget
     {
@@ -30,6 +39,11 @@ namespace xhud
         xfont xf;
 
         static void load(xBase& data, xDynAsset& asset, size_t);
+
+        font_meter_asset* get_asset()
+        {
+            return (font_meter_asset*)a;
+        }
     };
 } // namespace xhud
 
