c9ee6fd3360be9a45ce3c6473ddcce79426a2448
Joshua Sanchez <joshuaesanch@gmail.com>
2025-11-16 00:48:53 -0600

    xLaserBolt: Fixes to Headers and Initial Matches (#644)
    * Add -sym on c_flags to xLaserBolt.cpp in configure.py
    
    * iParMgr: Properly expose gRenderBuffer global var
    
    * xLaserBolt: Header updates using objdiff tooling
    
    * xLaserBolt: Initial pass of function matches

--- src/SB/Core/x/xLaserBolt.cpp
+++ src/SB/Core/x/xLaserBolt.cpp
@@ -2,14 +2,23 @@
 #include "xString.h"
 #include "xstransvc.h"
 #include "iParMgr.h"
+#include "zEvent.h"
+#include "zBase.h"
+#include "zNPCTypeCommon.h"
+#include "zEntDestructObj.h"
 
 #include <types.h>
 
-extern tagiRenderInput gRenderBuffer;
-extern F32 lbl_803CF7A4; // 0.0f
-extern F32 lbl_803CF7A8; // 255f
+void xLaserBoltEmitter::init(u32 max_bolts, const char* texture_name)
+{
+    this->bolt_raster = NULL;
+    memset(fxsize, 0, sizeof(fxsize));
+    this->bolts.init(max_bolts);
+    debug_init(texture_name);
+}
 
-void xLaserBoltEmitter::set_texture(char* name)
+
+void xLaserBoltEmitter::set_texture(const char* name)
 {
     set_texture(xStrHash(name));
 }
@@ -36,23 +45,30 @@ void xLaserBoltEmitter::set_texture(RwRaster* raster)
     this->bolt_raster = raster;
 }
 
+void xLaserBoltEmitter::reset()
+{
+}
+
 void xLaserBoltEmitter::refresh_config()
 {
     F32 alpha;
     if (this->cfg.kill_dist <= this->cfg.fade_dist)
     {
-        alpha = lbl_803CF7A4;
+        alpha = 0.0f;
     }
     else
     {
-        alpha = lbl_803CF7A8 / (this->cfg.kill_dist - this->cfg.fade_dist);
+        alpha = 255.0f / (this->cfg.kill_dist - this->cfg.fade_dist);
     }
     this->ialpha = alpha;
 }
 
 void xLaserBoltEmitter::attach_effects(fx_when_enum when, effect_data* fx, size_t fxsize)
 {
-    // TODO!!!
+    this->fx[when] = fx;
+    this->fxsize[when] = fxsize;
+    reset_fx(when);
+    debug_refresh_effects(when);
 }
 
 RxObjSpace3DVertex* xLaserBoltEmitter::get_vert_buffer(S32& dat)
@@ -61,9 +77,31 @@ RxObjSpace3DVertex* xLaserBoltEmitter::get_vert_buffer(S32& dat)
     return gRenderBuffer.m_vertex;
 }
 
+void xLaserBoltEmitter::apply_damage(xLaserBoltEmitter::bolt& b)
+{
+    if (b.hit_ent->baseType == eBaseTypeNPC)
+    {
+        zNPCCommon* npc = (zNPCCommon*)b.hit_ent;
+        npc->Damage(DMGTYP_PROJECTILE, NULL, &b.dir);
+    }
+    else if (b.hit_ent->baseType == eBaseTypeDestructObj)
+    {
+        zEntDestructObj_Hit((zEntDestructObj*)b.hit_ent, 0x40000);
+    }
+    else
+    {
+        zEntEvent(b.hit_ent, eEventHit, cfg.damage, 0.0f, 0.0f, 0.0f);
+    }
+}
+
 // WIP.
 void xLaserBoltEmitter::reset_fx(fx_when_enum when)
 {
-    U32* sizePtr = &this->fxsize[when];
-    effect_data** effect = &this->fx[when];
+    for (U32 i = 0; i < sizeof(fx); i++) 
+    {
+        U32* sizePtr = &this->fxsize[when];
+        effect_data** effect = &this->fx[when];
+
+        
+    }
 }
