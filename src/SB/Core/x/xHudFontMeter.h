17e3761a0db43aef8df42ac6e499badd33b19ffd
daft7 <83897243+daft7@users.noreply.github.com>
2025-11-18 18:29:09 -0600

    xHudFontMeter Progress (#648)
    * xHudFontMeter overhead
    
    * Method for converting color32u to iColor_tag
    
    * Generally okay matches for xHudFontMeter
    
    * Seeing if it builds with new commented out
    
    * Other build fix (thanks Sway)
    
    * Revert, this was fine
    
    * My redefinition of the load method was breaking things. Reverted and method commented to be a stub.
    
    * Square tweaks

--- src/SB/Core/x/xHudFontMeter.h
+++ src/SB/Core/x/xHudFontMeter.h
@@ -18,27 +18,36 @@ namespace xhud
         color32u c;
         color32u drop_c;
     };
-    
+
     struct font_meter_asset : meter_asset
     {
-        font_context font;        // offset 0x4C, size 0x24
-        U8 counter_mode;       // offset 0x70, size 0x1
-        U8 pad1;               // offset 0x71, size 0x1
-        U8 pad2;               // offset 0x72, size 0x1
-        U8 pad3;               // offset 0x73, size 0x1
+        font_context font; // offset 0x4C, size 0x24
+        U8 counter_mode; // offset 0x70, size 0x1
+        U8 pad1; // offset 0x71, size 0x1
+        U8 pad2; // offset 0x72, size 0x1
+        U8 pad3; // offset 0x73, size 0x1
+
+        char* type_name();
     };
 
     struct font_meter_widget : meter_widget
     {
-        font_context font;
-        font_context start_font;
-        S32 precision;
-        xVec2 offset;
-        char buffer[12];
-        S32 last_value;
-        xfont xf;
+        font_context font; // 0xC4
+        font_context start_font; // 0xE8
+        S32 precision; // 0x10C
+        xVec2 offset; // 0x110
+        char buffer[12]; // 0x118
+        S32 last_value; // 0x124
+        xfont xf; // 0x128
 
         static void load(xBase& data, xDynAsset& asset, size_t);
+        font_meter_widget(xhud::font_meter_asset& init);
+        void destruct();
+        void destroy();
+        U32 type();
+        U8 is(U32 id);
+        void update(F32 dt);
+        void render();
 
         font_meter_asset* get_asset()
         {
