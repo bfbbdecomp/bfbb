10d8bac6a116cf97c2a7179f1b7a2224146d7a75
bluisblu <53455507+bluisblu@users.noreply.github.com>
2025-11-22 23:44:59 -0500

    iMorph (#659)

--- src/SB/Core/gc/iMorph.h
+++ src/SB/Core/gc/iMorph.h
@@ -6,7 +6,19 @@
 #include <rwcore.h>
 #include <rpworld.h>
 
+struct DirtyMorph // total size: 0x20
+{
+    U32 count;
+    F32 scale;
+    S16 weight[4]; // offset 0x8, size 0x8
+    S16* v_array[4]; // offset 0x10, size 0x10
+};
+
 void iMorphOptimize(RpAtomic* model, S32 normals);
-void iMorphRender(RpAtomic* model, RwMatrix* mat, S16** v_array, S16* weight, U32 normals, F32 scale);
+void iMorphRender(RpAtomic* model, RwMatrix* mat, S16** v_array, S16* weight, U32 normals,
+                  F32 scale);
+void FastS16unpack(F32* dest, S16* v, S32 count, F32 scale);
+void FastS16weight2(F32* dest, S16** v_array, S16* weight, S32 count, F32 scale);
+void FastS16weight4(F32* dest, S16** v_array, S16* weight, S32 count, F32 scale);
 
 #endif
