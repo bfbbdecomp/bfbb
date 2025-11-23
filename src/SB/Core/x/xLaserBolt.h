c441c19fbd21e7621b9e01aa8aee03566568b9f7
Josh Sanchez <joshuaesanch@gmail.com>
2025-11-22 21:27:49 -0600

    xLaserBolt: Header updates and matches

--- src/SB/Core/x/xLaserBolt.h
+++ src/SB/Core/x/xLaserBolt.h
@@ -7,6 +7,7 @@
 #include "xMath2.h"
 #include "xMath3.h"
 #include "xParEmitter.h"
+#include "xVec3.h"
 
 #include <rwcore.h>
 #include <types.h>
@@ -121,7 +122,11 @@ struct xLaserBoltEmitter
     RxObjSpace3DVertex* get_vert_buffer(S32& dat);
     void apply_damage(bolt& b);
     void reset_fx(fx_when_enum when);
-
+    void update_fx(bolt& b, F32 prev_dist, F32 dt);
+    void emit_particle(effect_data& effect, bolt& b, F32 from_dist, F32 to_dist, F32 dt);
+    void emit_decal(effect_data& effect, bolt& b, F32 from_dist, F32 to_dist, F32 dt);
+    void emit_decal_dist(effect_data& effect, bolt& b, F32 from_dist, F32 to_dist, F32 dt);
+;
     U32 visible() const;
 
     void debug_init(const char* texture_name)
@@ -129,7 +134,77 @@ struct xLaserBoltEmitter
 
     }
 
+    void emit_fx(effect_data& effect, bolt& b, F32 from_dist, F32 to_dist, F32 dt)
+    {
+        switch (effect.type)
+        {
+        case FX_TYPE_PARTICLE:
+            emit_particle(effect, b, from_dist, to_dist, dt);
+            break;
+        case FX_TYPE_DECAL:
+            emit_decal(effect, b, from_dist, to_dist, dt);
+            break;
+        case FX_TYPE_DECAL_DIST:
+            emit_decal_dist(effect, b, from_dist, to_dist, dt);
+            break;
+        case FX_TYPE_CALLBACK:
+            effect.callback.fp(b, effect.callback.context);
+            break;
+        }
+    }
+
+    void perturb_dir(xVec3& dir, F32 rand_angle)
+    {
+        xVec3 temp = { 0.0f, 0.0f, 0.0f };
+        xMat3x3 mat;
+
+        temp.x = (xurand() - 0.5f) * rand_angle;
+        temp.y = (xurand() - 0.5f) * rand_angle;
+        temp.z = (xurand() - 0.5f) * rand_angle;
+
+        xMat3x3Euler(&mat, &temp);
+        xMat3x3LMulVec(&dir, &mat, &dir);
+    }
+
+    void update(bolt& b, F32 dt)
+    {
+        b.prev_dist = b.dist;
+        b.dist += cfg.vel * dt;
+    }
+
+    void debug_update(F32 dt)
+    {
+
+    }
+
+    void flush_verts(RxObjSpace3DVertex* verts, S32 num_verts)
+    {
+        RwIm3DTransform(verts, num_verts, NULL, 0x19);
+        RwIm3DRenderPrimitive(rwPRIMTYPETRILIST);
+        RwIm3DEnd();
+    }
+
+    void debug_render()
+    {
+
+    }
+
     void debug_refresh_effects(fx_when_enum when)
+    {
+
+    }
+
+    void log_collide_statics(bool)
+    {
+
+    }
+
+    void log_collide_dynamics(bool)
+    {
+
+    }
+
+    void set_bolt_verts(RxObjSpace3DVertex* vert, const xVec3& pointA, const xVec3& pointB, U8, const xVec3&)
     {
 
     }
