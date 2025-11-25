847259870174f57f7786ff6538a1a087136f5f19
Josh Sanchez <joshuaesanch@gmail.com>
2025-11-24 19:34:01 -0600

    zNPCSupport: Add NPCC_MakeArbPlane to header

--- src/SB/Game/zNPCSupport.h
+++ src/SB/Game/zNPCSupport.h
@@ -145,6 +145,7 @@ void Firework_Timestep(F32 dt);
 void Firework_Release(Firework*);
 RwTexture* NPCC_FindRWTexture(U32 hashid);
 RwTexture* NPCC_FindRWTexture(const char* txtrname);
+void NPCC_MakeArbPlane(const xVec3* dir_norm, xVec3* at, xVec3* rt);
 RwRaster* NPCC_FindRWRaster(const char* txtrname);
 RwRaster* NPCC_FindRWRaster(RwTexture* txtr);
 void zNPC_SNDInit();
