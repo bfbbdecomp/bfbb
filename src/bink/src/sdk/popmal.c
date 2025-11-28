34ac0a1bc05b22af8ba766884903b79b158fc5c2
Colin Miller <therealnucka@gmail.com>
2025-11-28 01:26:40 -0500

    fix pt6

--- src/bink/src/sdk/popmal.c
+++ src/bink/src/sdk/popmal.c
@@ -1,22 +1,22 @@
-#include "popmal.h"
+// #include "popmal.h"
 
-U32 pushtot = NULL;
-U32 pushcur = NULL;
-U32 pushptr = NULL;
-U32 pushamt = NULL;
-U32 cursize = 32;
+// U32 pushtot = NULL;
+// U32 pushcur = NULL;
+// U32 pushptr = NULL;
+// U32 pushamt = NULL;
+// U32 cursize = 32;
 
-// TODO:
-// figure out what arg 1 should be. pretty confident arg 2 is correct
-void pushmalloc(BINKIO* io, HBINK bnk)
-{
-}
+// // TODO:
+// // figure out what arg 1 should be. pretty confident arg 2 is correct
+// void pushmalloc(BINKIO* io, HBINK bnk)
+// {
+// }
 
-U32 popmalloctotal()
-{
-    return pushtot;
-}
+// U32 popmalloctotal()
+// {
+//     return pushtot;
+// }
 
-U32 popmalloc(HBINK bnk)
-{
-}
+// U32 popmalloc(HBINK bnk)
+// {
+// }
