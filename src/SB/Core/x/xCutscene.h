b14ea359c354af2569acd768b26d5bd6c0eef813
Josh Sanchez <joshuaesanch@gmail.com>
2025-12-02 21:12:36 -0600

    xCutscene: Update header with additional constant def and xCutsceneAnimHdr struct

--- src/SB/Core/x/xCutscene.h
+++ src/SB/Core/x/xCutscene.h
@@ -3,6 +3,16 @@
 
 #include "xFile.h"
 #include "rpworld.h"
+#include "xCamera.h"
+
+#define XCUTSCENEDATA_TYPE_CAMERA 3
+#define XCUTSCENEDATA_TYPE_SOUND 5
+#define XCUTSCENEDATA_TYPE_6 6
+
+struct xCutsceneAnimHdr {
+    U32 RootIndex;
+    F32 Translate[3];
+};
 
 struct xCutsceneInfo
 {
@@ -33,9 +43,6 @@ struct xCutsceneData
         void* DataPtr;
     };
 };
-
-#define XCUTSCENEDATA_TYPE_SOUND 5
-#define XCUTSCENEDATA_TYPE_6 6
 
 struct xCutsceneBreak
 {
@@ -120,6 +127,7 @@ xCutscene* xCutscene_Create(U32 id);
 S32 xCutscene_Destroy(xCutscene* csn);
 S32 xCutscene_LoadStart(xCutscene* csn);
 void xCutscene_SetSpeed(xCutscene* csn, F32 speed);
+void xCutscene_SetCamera(xCutscene* csn, xCamera* cam);
 F32 xCutsceneConvertBreak(float time, xCutsceneBreak* breaklist, U32 breakcount, int idx);
 S32 xCutscene_Update(xCutscene* csn, F32 dt);
 void CutsceneShadowRender(CutsceneShadowModel* smod);
