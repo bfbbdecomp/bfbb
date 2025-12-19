f03d57429d13c60e810a9c3e55c06d2712fff7fe
Josh Sanchez <joshuaesanch@gmail.com>
2025-12-18 23:06:36 -0600

    zLasso: Clean up fizzical static functions

--- src/SB/Game/zLasso.cpp
+++ src/SB/Game/zLasso.cpp
@@ -155,22 +155,22 @@ void zLasso_ResetTimer(zLasso* lasso, F32 interpTime)
     xVec3Add(&lasso->stCenter, &lasso->crCenter, &lasso->anchor);
 }
 
-void fizzicalRadius(zLasso* lasso, f32 arg1, xVec3* arg2)
+static void fizzicalRadius(zLasso* lasso, F32 dt, xVec3* newPoint)
 {
-    lasso->crRadius = -((2.0f * (0.75f - lasso->crSlack) * arg1) - lasso->crRadius);
+    lasso->crRadius = -((2.0f * (0.75f - lasso->crSlack) * dt) - lasso->crRadius);
     if (lasso->crRadius < 0.0f)
     {
         lasso->crRadius = 0.0f;
     }
 }
 
-void fizzicalCenter(zLasso* lasso, f32 arg1, xVec3* arg2)
+static void fizzicalCenter(zLasso* lasso, F32 dt, xVec3* newPoint)
 {
     xVec3AddScaled(&lasso->crCenter, &lasso->lastRefs[lasso->reindex[4]], -0.2f);
-    xVec3AddScaled(&lasso->crCenter, arg2, 0.2f);
+    xVec3AddScaled(&lasso->crCenter, newPoint, 0.2f);
 }
 
-void fizzicalNormal(zLasso* lasso, f32 arg1, xVec3* arg2)
+static void fizzicalNormal(zLasso* lasso, F32 dt, xVec3* newPoint)
 {
     xVec3 sp8;
     f32 temp_f31;
@@ -181,39 +181,38 @@ void fizzicalNormal(zLasso* lasso, f32 arg1, xVec3* arg2)
     xVec3Normalize(&lasso->crNormal, &lasso->crNormal);
 }
 
-void fizzicalHonda(zLasso* lasso, f32 arg1, xVec3* arg2)
+static void fizzicalHonda(zLasso* lasso, F32 dt, xVec3* newPoint)
 {
-    xVec3* temp_r3;
-
-    xVec3SMul(&lasso->honda, arg2, -1.0f);
+    xVec3SMul(&lasso->honda, newPoint, -1.0f);
     xVec3AddScaled(&lasso->honda, &lasso->lastRefs[lasso->reindex[0]], 0.571429f);
     xVec3AddScaled(&lasso->honda, &lasso->lastRefs[lasso->reindex[1]], 0.285714f);
     xVec3AddScaled(&lasso->honda, &lasso->lastRefs[lasso->reindex[2]], 0.142857f);
     xVec3AddScaled(&lasso->honda, &lasso->crNormal, -xVec3Dot(&lasso->crNormal, &lasso->honda));
-    temp_r3 = &lasso->honda;
-    xVec3Normalize(temp_r3, temp_r3);
+    xVec3Normalize(&lasso->honda, &lasso->honda);
     xVec3SMulBy(&lasso->honda, lasso->crRadius);
-    if (((s32)negativeHondaX != 0) && (lasso->honda.x > 0.0f))
+    
+    if ((negativeHondaX != 0) && (lasso->honda.x > 0.0f))
     {
-        xSndPlay3D(xStrHash(&"rope\0sound_rope_windup"[5]), 0.77f, 0.0f, 0U, 0x10000U,
-                   &lasso->anchor, 100.0f, (sound_category)0, 0.0f);
+        xSndPlay3D(xStrHash("sound_rope_windup"), 0.77f, 0.0f, 0U, 0x10000U, &lasso->anchor, 100.0f,
+                   SND_CAT_GAME, 0.0f);
         negativeHondaX = 0;
     }
+    
     if (lasso->honda.x < 0.0f)
     {
         negativeHondaX = 1;
     }
+    
     xVec3AddTo(&lasso->honda, &lasso->crCenter);
 }
 
-void nonfizzicalHonda(zLasso* lasso, f32 arg1, xVec3* arg2)
+static void nonfizzicalHonda(zLasso* lasso, F32 dt, xVec3* newPoint)
 {
-    f32 temp_f1;
-
-    xVec3Sub(&lasso->honda, arg2, &lasso->crCenter);
+    xVec3Sub(&lasso->honda, newPoint, &lasso->crCenter);
     xVec3AddScaled(&lasso->honda, &lasso->crNormal, -xVec3Dot(&lasso->honda, &lasso->crNormal));
-    temp_f1 = xVec3Length2(&lasso->honda);
-    if (temp_f1 < 0.00001f)
+
+    F32 hondaLength = xVec3Length2(&lasso->honda);
+    if (hondaLength < 0.00001f)
     {
         lasso->honda.x = lasso->crNormal.y;
         lasso->honda.y = -lasso->crNormal.x;
@@ -222,22 +221,21 @@ void nonfizzicalHonda(zLasso* lasso, f32 arg1, xVec3* arg2)
         xVec3AddTo(&lasso->honda, &lasso->crCenter);
         return;
     }
-    xVec3SMulBy(&lasso->honda, lasso->crRadius / xsqrt(temp_f1));
+
+    xVec3SMulBy(&lasso->honda, lasso->crRadius / xsqrt(hondaLength));
     xVec3AddTo(&lasso->honda, &lasso->crCenter);
 }
 
-void fizzicalSlack(zLasso* lasso, f32 arg1, xVec3* arg2)
+static void fizzicalSlack(zLasso* lasso, F32 dt, xVec3* newPoint)
 {
-    f32 temp_f1;
-
-    lasso->crSlack += (2.0f * (lasso->lastDist - lasso->currDist)) - (0.6f * arg1);
-    temp_f1 = lasso->crSlack;
-    if (temp_f1 < 0.0f)
+    lasso->crSlack += (2.0f * (lasso->lastDist - lasso->currDist)) - (0.6f * dt);
+    if (lasso->crSlack < 0.0f)
     {
         lasso->crSlack = 0.0f;
         return;
     }
-    if (temp_f1 > 1.0f)
+
+    if (lasso->crSlack > 1.0f)
     {
         lasso->crSlack = 1.0f;
     }
