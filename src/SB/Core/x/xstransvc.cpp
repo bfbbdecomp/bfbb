93d8c07e206bb2222f7354f2f41879ff6402b5ce
Zachary Canann <zacharycanann@gmail.com>
2026-03-18 20:05:34 -0700

    Improve xstransvc scene unlock matching

--- src/SB/Core/x/xstransvc.cpp
+++ src/SB/Core/x/xstransvc.cpp
@@ -438,14 +438,15 @@ char* xST_xAssetID_HIPFullPath(U32 aid, U32* sceneID)
 static S32 XST_PreLoadScene(st_STRAN_SCENE* sdata, const char* name)
 {
     S32 buf = 0;
-    st_PACKER_READ_DATA* spkg =
-        g_pkrf->Init(sdata->userdata, (char*)name, 0x2e, &buf, g_typeHandlers);
-    sdata->spkg = spkg;
+    st_PACKER_READ_FUNCS* pkrf = g_pkrf;
+
+    sdata->spkg = pkrf->Init(sdata->userdata, (char*)name, 0x2e, &buf, g_typeHandlers);
     if (sdata->spkg != NULL)
     {
         return buf;
     }
-    return NULL;
+
+    return 0;
 }
 
 static char* XST_translate_sid(U32 sid, char* extension)
@@ -500,10 +501,12 @@ static void XST_unlock(st_STRAN_SCENE* sdata)
     if (sdata != NULL)
     {
         U32 lock = 1 << sdata->lockid;
+        st_STRAN_DATA* stran = &g_xstdata;
+        U32 loadlock = stran->loadlock;
 
-        if (g_xstdata.loadlock & lock)
+        if (loadlock & lock)
         {
-            g_xstdata.loadlock &= ~lock;
+            stran->loadlock = loadlock & ~lock;
             memset(sdata, 0, sizeof(st_STRAN_SCENE));
         }
     }
