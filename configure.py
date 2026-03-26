2bda80ea481d5b9a872e4c5d44d5224ecc8d15eb
Joey Ballentine <34788790+joeyballentine@users.noreply.github.com>
2026-03-25 20:14:06 -0500

    Update the objdiff-cli version that gets auto-downloaded by ninja (#734)

--- configure.py
+++ configure.py
@@ -159,7 +159,7 @@ if not config.non_matching:
 config.binutils_tag = "2.42-1"
 config.compilers_tag = "20250812"
 config.dtk_tag = "v1.7.0"
-config.objdiff_tag = "v3.4.1"
+config.objdiff_tag = "v3.7.1"
 config.sjiswrap_tag = "v1.2.2"
 config.wibo_tag = "1.0.0-beta.5"
 
