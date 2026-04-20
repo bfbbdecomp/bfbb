247a7ba9df7f32d6b125b0875122a31cdda0ef8e
Zachary Canann <zacharycanann@gmail.com>
2026-04-19 21:50:46 -0700

    Improve MSL buffer_io donor import

--- src/PowerPC_EABI_Support/src/MSL_C/MSL_Common/buffer_io.c
+++ src/PowerPC_EABI_Support/src/MSL_C/MSL_Common/buffer_io.c
@@ -3,51 +3,168 @@
 #endif
 
 #include "PowerPC_EABI_Support/MSL_C/MSL_Common/ansi_files.h"
+#include "PowerPC_EABI_Support/MSL_C/MSL_Common/alloc.h"
+#include "PowerPC_EABI_Support/MSL_C/MSL_Common/file_io.h"
+#include "PowerPC_EABI_Support/MSL_C/MSL_Common/critical_regions.h"
 #include "types.h"
 
-void __convert_from_newlines(char *buffer, size_t *length)
+#ifndef _IONBF
+#define _IONBF 0
+#endif
+
+#ifndef _IOLBF
+#define _IOLBF 1
+#endif
+
+#ifndef _IOFBF
+#define _IOFBF 2
+#endif
+
+enum __io_results { __no_io_error, __io_error, __io_EOF };
+
+void* malloc(size_t size);
+
+inline void __convert_from_newlines(char* buffer, size_t* length) {}
+
+void __prep_buffer(FILE* file)
 {
-	return;
+    file->mBufferPtr = file->mBuffer;
+    file->mBufferLength = file->mBufferSize;
+    file->mBufferLength -= file->mPosition & file->mBufferAlignment;
+    file->mBufferPosition = file->mPosition;
 }
 
-void __convert_to_newlines()
+int __load_buffer(FILE* file, size_t* bytes_loaded, int mode)
 {
-	// UNUSED
+    int ioresult;
+    char* buffer_start;
+
+    __prep_buffer(file);
+
+    if (mode == 1) {
+        file->mBufferLength = file->mBufferSize;
+    }
+
+    ioresult = file->readFunc(file->mHandle, file->mBuffer, &file->mBufferLength, file->ref_con);
+
+    if (ioresult == __io_EOF) {
+        file->mBufferLength = 0;
+    }
+
+    if (bytes_loaded != NULL) {
+        *bytes_loaded = file->mBufferLength;
+    }
+
+    if (ioresult != __no_io_error) {
+        return ioresult;
+    }
+
+    file->mPosition += file->mBufferLength;
+
+    if (!file->mMode.binary_io) {
+        int i;
+
+        buffer_start = file->mBuffer;
+        for (i = file->mBufferLength; i != 0; i--) {
+            char c = *buffer_start;
+            buffer_start++;
+            if (c == '\n') {
+                file->mPosition++;
+            }
+        }
+    }
+
+    return __no_io_error;
 }
 
-void __prep_buffer(FILE *file)
+int __flush_buffer(FILE* file, size_t* bytes_flushed)
 {
-	file->mBufferPtr = file->mBuffer;
-	file->mBufferLength = file->mBufferSize;
-	file->mBufferLength = file->mBufferLength - (file->mPosition & file->mBufferAlignment);
-	file->mBufferPosition = file->mPosition;
-	return;
+    size_t buffer_len;
+    int ioresult;
+
+    buffer_len = file->mBufferPtr - file->mBuffer;
+
+    if (buffer_len) {
+        file->mBufferLength = buffer_len;
+
+        if (!file->mMode.binary_io) {
+            __convert_from_newlines(file->mBuffer, &file->mBufferLength);
+        }
+
+        ioresult = file->writeFunc(file->mHandle, file->mBuffer, &file->mBufferLength, file->ref_con);
+
+        if (bytes_flushed) {
+            *bytes_flushed = file->mBufferLength;
+        }
+
+        if (ioresult) {
+            return ioresult;
+        }
+
+        file->mPosition += file->mBufferLength;
+    }
+
+    __prep_buffer(file);
+
+    return __no_io_error;
 }
 
-int __flush_buffer(FILE *file, size_t *length)
+int setvbuf(FILE* file, char* buffer, int mode, size_t size)
 {
-	size_t bufferLen;
-	int writeCode;
-	char binmode;
+    unsigned char* file_bytes;
+    unsigned short mode_bits;
+    int io_mode;
 
-	bufferLen = file->mBufferPtr - file->mBuffer;
-	if (bufferLen) {
-		file->mBufferLength = bufferLen;
+    file_bytes = (unsigned char*)file;
+    mode_bits = *(unsigned short*)(file_bytes + 4);
+    io_mode = (mode_bits >> 6) & 7;
 
-		if (file->mMode.binary_io == 0)	{
-			__convert_from_newlines(file->mBuffer, &file->mBufferLength);
-		}
+    if (mode == _IONBF) {
+        fflush(file);
+    }
 
-		writeCode = file->writeFunc(file->mHandle, file->mBuffer, &file->mBufferLength, file->ref_con);
-		if (length) {
-			*length = file->mBufferLength;
-		}
-		if (writeCode) {
-			return writeCode;
-		}
-		file->mPosition += file->mBufferLength;
-	}
+    if (file->mState.io_state != __neutral || io_mode == 0) {
+        return -1;
+    }
 
-	__prep_buffer(file);
-	return 0;
+    if (mode != _IONBF && mode != _IOLBF && mode != _IOFBF) {
+        return -1;
+    }
+
+    if (file->mBuffer != NULL && file->mState.free_buffer != 0) {
+        free(file->mBuffer);
+    }
+
+    __begin_critical_region(2);
+
+    file->mMode.buffer_mode = mode;
+    file->mState.free_buffer = 0;
+    file->mBuffer = (char*)&file->mCharBuffer;
+    file->mBufferPtr = (char*)&file->mCharBuffer;
+    file->mBufferSize = 1;
+    file->mBufferLength = 0;
+    file->mBufferAlignment = 0;
+
+    if (mode == _IONBF || size < 1) {
+        *file->mBufferPtr = 0;
+        __end_critical_region(2);
+        return 0;
+    }
+
+    if (buffer == NULL) {
+        buffer = (char*)malloc(size);
+        if (buffer == NULL) {
+            __end_critical_region(2);
+            return -1;
+        }
+        file->mState.free_buffer = 1;
+    }
+
+    file->mBuffer = buffer;
+    file->mBufferPtr = file->mBuffer;
+    file->mBufferSize = size;
+    file->mBufferAlignment = 0;
+
+    __end_critical_region(2);
+    return 0;
 }
