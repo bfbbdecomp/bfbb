c32ce13b9173efc1dfe0ecc00b93e82ecce9f109
Zachary Canann <zacharycanann@gmail.com>
2026-04-19 22:21:46 -0700

    Improve MSL extras strcmpi match

--- src/PowerPC_EABI_Support/src/MSL_C/MSL_Common/extras.c
+++ src/PowerPC_EABI_Support/src/MSL_C/MSL_Common/extras.c
@@ -150,9 +150,24 @@ void _strncasecmp(void)
 	// UNUSED FUNCTION
 }
 
-void strcmpi(void)
+int strcmpi(const char* str1, const char* str2)
 {
-	// UNUSED FUNCTION
+	char a_var;
+	char b_var;
+
+	do {
+		b_var = _tolower(*str1++);
+		a_var = _tolower(*str2++);
+
+		if (b_var < a_var) {
+			return -1;
+		}
+		if (b_var > a_var) {
+			return 1;
+		}
+	} while (b_var != 0);
+
+	return 0;
 }
 
 void _strcmpi(void)
