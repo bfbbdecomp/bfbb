a7522e690122df4ee17f48d2c94b7def22b2f4e0
bluisblu <53455507+bluisblu@users.noreply.github.com>
2026-07-01 10:28:34 -0400

    iSystem 99%

--- src/SB/Core/gc/iSystem.h
+++ src/SB/Core/gc/iSystem.h
@@ -1,6 +1,7 @@
 #ifndef ISYSTEM_H
 #define ISYSTEM_H
 
+#include "rwplcore.h"
 #include <types.h>
 #include <dolphin.h>
 
@@ -8,6 +9,24 @@ extern "C" {
 void* malloc(U32 __size);
 void free(void* __ptr);
 }
+
+#define DLFILE_BUF_SIZE 0x2800
+
+struct dlFile // fabricated based on skyFile dwarf
+{
+    U8 readBuffer[DLFILE_BUF_SIZE]; // 0x00
+    DVDFileInfo fileInfo; // 0x2800
+    S32 POS; // 0x283C
+    U32 SOF; // 0x2840
+    U32 bufferPos; // 0x2844
+    S32 accessMode; // 0x2848
+};
+
+struct dlFSUnkGlobals // entirely fabricated
+{
+    RwFileFunctions fileFuncs; // 0x00
+    RwFreeList* unk_2C; // 0x2C
+};
 
 void iVSync();
 
